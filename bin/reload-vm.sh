14da94550fe5f9cd9733726d74955b333375e815
Markus Oehme <veelai.1764@googlemail.com>
2015-10-19 11:30:13 +0200

    auto-build: Note a FIXME

--- bin/reload-vm.sh
+++ bin/reload-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Reloading backends"
 BACKENDS="session core cde event"
-sudo sv restart $BACKENDS
+sudo sv restart $BACKENDS # FIXME fails
 echo "Reloading frontend"
 touch /cdedb2/wsgi/cdedb.wsgi
 echo "Done"
