de804d89289547c4188cbcb05b7bccb5f45afd15
Michael Thies <mail@mhthies.de>
2016-12-03 15:11:36 +0100

    UI/assembly: Add touch and keyboard functionality for interactive preferential voting on show_ballot

--- static/cdedb_voting.js
+++ static/cdedb_voting.js
@@ -28,6 +28,23 @@
     /**
      * Object representing state and methods of the fancy interactive preferential voting form. The jQuery plugin
      * to instantiate this object on the form follows underneath.
+     *
+     * General behaviour:
+     * The jQuery plugin is called on the div.prefvote_container and creates an instance of the following object. The
+     * init() function is called and creates a span.prefvote_candidate for each candidate in the candidates parameter.
+     * These DOM objects are stored as jQuery wrappers in candidate_list.
+     *
+     * On opening the jsform tab, the event handler readPreferenceList() should be called. It creates some
+     * div.prefvote_stage and div.prefvote_spacer, also considering if a bar option is used (bar_moniker != null).
+     *
+     * Candidates can be moved to stages and spacers using Drag'n'Drop or clicking ('activating') a candidate and then
+     * clicking the destination div. Drag'n'Drop is handled by storing the candidate's moniker in event.dataTransfer;
+     * 'activating' is done by adding a class to the span.prefvote_candidate and div.prefvote_container.
+     *
+     * Moving a candidate to a spacer is done by splitSpacer() and results in creating a new stage and a new spacer
+     * underneath the destination spacer and moving the candidate to the new stage. Moving a candidate to a (existing or
+     * new) stage is done by moveCandidate() and does three actions: moving the candidate, deleting the source stage if
+     * empty, calling updatePreferenceList() to create the text based preference list and update the voting form.
      */
     var PrefVote = function($container, candidates, bar_moniker, bar_name, $input_preferencelist) {
         /** Associative list of candidate jQuery DOM elements indexed by their moniker */
@@ -68,8 +85,7 @@
         }
         /**
          * To be used as ondrop on .prefvote_spacer
-         * Removes the .dragover class, creates a new stage and spacer underneath this element and calls
-         * moveCandidate() with the new stage es destination.
+         * Removes the .dragover class and uses splitSpacer() to create new Stage and move condidate into.
          */
         function spacer_drop(e) {
             $(this).removeClass('dragover');
@@ -78,23 +94,79 @@
             $candidate = candidate_list[data];
             if (!$candidate)
                 return;
-            
+            splitSpacer($(this), $candidate);
+        }
+        /**
+         * To be used as onclick on .prefvote_stage
+         * If container is active: moves active candidate(s) to stage and disactivates container
+         */
+        function stage_click(e) {
+            if ($container.hasClass('active')) {
+                var $active_candidates = $container.find('.prefvote_candidate.active');
+                moveCandidate($active_candidates,$(this));
+                $active_candidates.removeClass('active');
+                $container.removeClass('active')
+            }
+        }
+        /**
+         * To be used as onclick on .prefvote_spacer
+         * If container is active: splits spacer (moving active candidate to new stage) and disactivates container
+         */
+        function spacer_click(e) {
+            if ($container.hasClass('active')) {
+                var $active_candidates = $container.find('.prefvote_candidate.active');
+                splitSpacer($(this),$active_candidates);
+                $active_candidates.removeClass('active');
+                $container.removeClass('active')
+            }
+        }
+        /**
+         * To be used as onclick on .prefvote_candidate
+         * If candidate is active: disactivates container and candidate
+         * Else: activates the candidate, the container and disactivates all other candidates
+         */
+        function candidate_click(e) {
+            if ($(this).hasClass('active')) {
+                $container.removeClass('active');
+                $(this).removeClass('active');
+            } else {
+                $container.find('.prefvote_candidate').removeClass('active');
+                $(this).addClass('active');
+                $container.addClass('active');
+            }
+            e.stopPropagation();
+        }
+        /**
+         * Returns an event handler function for keyboard events that calls the given callback if the keyCode represents
+         * a press of ENTER or SPACE function.
+         */
+        function getKeyboardHandler(callback) {
+            return function(e) {
+                if (e.keyCode == 13 || e.keyCode == 32)
+                    callback.call(this,e);
+            };
+        }
+        
+        /**
+         * Split $spacer, introduce new stage and move $candidate into it (using moveCandidate()).
+         * To be called by spacer_drop() and spacer_click()
+         */
+        function splitSpacer($spacer, $candidate) {
             $new_stage = createStage();
             $new_spacer = createSpacer();
-            if ($(this).hasClass('positive')) {
+            if ($spacer.hasClass('positive')) {
                 $new_stage.addClass('positive');
                 $new_spacer.addClass('positive');
-            } else if ($(this).hasClass('negative')) {
+            } else if ($spacer.hasClass('negative')) {
                 $new_stage.addClass('negative');
                 $new_spacer.addClass('negative');
             }
             
-            $new_stage.insertAfter($(this));
+            $new_stage.insertAfter($spacer);
             $new_spacer.insertAfter($new_stage);
             
             moveCandidate($candidate, $new_stage);
         }
-        
         /**
          * To be used implicitly by drop functions.
          * Moves the $candidate to the $destination stage and deletes the source stage if empty and not neutral.
@@ -131,20 +203,24 @@
         
         /** Create a new .prefvote_spacer box and return jQuery reference. */
         function createSpacer() {
-            var $sp = $('<div></div>', {'class': 'prefvote_spacer'});
+            var $sp = $('<div></div>', {'class': 'prefvote_spacer', 'tabindex': '0'});
             $sp.on('dragover',allowDrop);
             $sp.on('drop',spacer_drop);
             $sp.on('dragenter',dragenter);
             $sp.on('dragleave',dragleave);
+            $sp.on('click',spacer_click);
+            $sp.on('keydown',getKeyboardHandler(spacer_click));
             return $sp;
         }
         /** Create a new .prefvote_stage box and return jQuery reference. */
         function createStage() {
-            var $st = $('<div></div>', {'class': 'prefvote_stage'});
+            var $st = $('<div></div>', {'class': 'prefvote_stage', 'tabindex': '0'});
             $st.on('dragover',allowDrop);
             $st.on('drop',stage_drop);
             $st.on('dragenter',dragenter);
             $st.on('dragleave',dragleave);
+            $st.on('click',stage_click);
+            $st.on('keydown',getKeyboardHandler(stage_click));
             return $st;
         }
         
@@ -159,12 +235,16 @@
                 var $cand = $('<span></span>', {'class': 'prefvote_candidate',
                                                 'draggable': 'true',
                                                 'id': 'vote-cand_' + candidates[i][1].id,
-                                                'data-moniker': moniker});
+                                                'data-moniker': moniker,
+                                                'tabindex': '0'});
                 $cand.text(candidates[i][1].description);
                 candidate_list[moniker] = $cand;
                 $cand.on('dragstart',function(e) {
                     e.originalEvent.dataTransfer.setData('text', $(this).attr('data-moniker'));
+                    $container.find('.prefvote_candidate').removeClass('active');
                 });
+                $cand.click(candidate_click);
+                $cand.on('keydown',getKeyboardHandler(candidate_click));
             }
         }
         
@@ -267,6 +347,12 @@
 
     /**
      * jQuery plugin for the fancy interactive preferential voting.
+     *
+     * parameters:
+     * candidates: List of all candidates in form: [ [id, {'id', 'moniker', 'description'}] ]
+     * bar_moniker: Moniker of bar option, null if bar is not used
+     * bar_name: Label of bar option / neutral stage box
+     * $input_preferencelist: jQuery object of text only voting form input field
      */
     $.fn.cdedbPrefVote = function(candidates, bar_moniker, bar_name, $input_preferencelist) {
         if ($(this).data('cdedbPrefVote'))
