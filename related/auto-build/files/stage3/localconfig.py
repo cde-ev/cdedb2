c1c22af506206745de29c5833a981caf2cd899a5
Markus Oehme <veelai.1764@googlemail.com>
2017-12-31 15:31:24 +0100

    auto-build: Add more logfiles to localconfig
    Otherwise they will be put into a temp directory.

--- related/auto-build/files/stage3/localconfig.py
+++ related/auto-build/files/stage3/localconfig.py
@@ -10,11 +10,19 @@ SYSLOG_LOG_LEVEL = logging.INFO
 CONSOLE_LOG_LEVEL = logging.INFO
 
 GLOBAL_LOG = "/log/cdedb.log"
+TIMING_LOG = "/log/cdedb-timing.log"
 FRONTEND_LOG = "/log/cdedb-frontend.log"
+CORE_FRONTEND_LOG = "/log/cdedb-frontend-core.log"
+CDE_FRONTEND_LOG = "/log/cdedb-frontend-cde.log"
+EVENT_FRONTEND_LOG = "/log/cdedb-frontend-event.log"
+ML_FRONTEND_LOG = "/log/cdedb-frontend-ml.log"
+ASSEMBLY_FRONTEND_LOG = "/log/cdedb-frontend-assembly.log"
+BACKEND_LOG = "/log/cdedb-backend.log"
 CORE_BACKEND_LOG = "/log/cdedb-backend-core.log"
 SESSION_BACKEND_LOG = "/log/cdedb-backend-session.log"
 CDE_BACKEND_LOG = "/log/cdedb-backend-cde.log"
 EVENT_BACKEND_LOG = "/log/cdedb-backend-event.log"
+PAST_EVENT_BACKEND_LOG = "/log/cdedb-backend-past-event.log"
 ML_BACKEND_LOG = "/log/cdedb-backend-ml.log"
 ASSEMBLY_BACKEND_LOG = "/log/cdedb-backend-assembly.log"
 
