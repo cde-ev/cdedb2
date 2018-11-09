4fa191883255043dd9c065f425ca0d34838ecc19
Michael Thies <mail@mhthies.de>
2018-11-09 14:49:30 +0100

    tools: Fix reset-ldap-schema.sh
    * Don't require env variable LDAP_PASSWORDFILE
    * Correct wildcard usage for schema config file delition
    * Call slaptest with absolute path, as /usr/sbin is not in $PATH for
      non-root-users on Debian

--- bin/reset-ldap-schema.sh
+++ bin/reset-ldap-schema.sh
@@ -2,21 +2,20 @@
 
 echo "This is basically impossible, so use this at your own risk."
 echo "We now chicken out. The code below is mainly for documentation purposes."
-exit 0
 
 #
 # First delete personas
 #
 
-echo 'ou=personas,dc=cde-ev,dc=de' | ldapdelete -c -r -x -D `cat .ldap_rootdn` -y ${LDAP_PASSWORDFILE} || true
-echo 'ou=personas-test,dc=cde-ev,dc=de' | ldapdelete -c -r -x -D `cat .ldap_rootdn` -y ${LDAP_PASSWORDFILE} || true
+echo 'ou=personas,dc=cde-ev,dc=de' | ldapdelete -c -r -x -D `cat .ldap_rootdn` -y .ldap_rootpw || true
+echo 'ou=personas-test,dc=cde-ev,dc=de' | ldapdelete -c -r -x -D `cat .ldap_rootdn` -y .ldap_rootpw || true
 
 #
 # Second delete schema
 #
 
 sudo systemctl stop slapd
-sudo rm "/etc/ldap/slapd.d/cn=config/cn=schema/cn=*cdepersona.ldif"
+sudo bash -c 'rm "/etc/ldap/slapd.d/cn=config/cn=schema/cn="*"cdepersona.ldif"'
 sudo systemctl start slapd
 
 #
@@ -26,7 +25,7 @@ sudo systemctl start slapd
 
 sudo ldapmodify -Y EXTERNAL -H ldapi:/// -f /cdedb2/related/auto-build/files/stage3/init.ldif
 TMPDIR=`mktemp -d`
-slaptest -f /cdedb2/related/auto-build/files/stage3/temp_ldap.conf -F $TMPDIR
+/usr/sbin/slaptest -f /cdedb2/related/auto-build/files/stage3/temp_ldap.conf -F $TMPDIR
 # stupid LDAP needs manual fixing
 sed -n -i -e '/structuralObjectClass: olcSchemaConfig/q;p' "$TMPDIR/cn=config/cn=schema/cn="*"cdepersona.ldif"
 sed -i -e 's/^dn: cn={[0-9]\+}cdepersona/dn: cn=cdepersona,cn=schema,cn=config/' "$TMPDIR/cn=config/cn=schema/cn="*"cdepersona.ldif"
