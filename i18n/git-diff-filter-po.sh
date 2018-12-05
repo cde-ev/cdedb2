fb1d738fc797354d501e82cc14dc900c1007c48b
Michael Thies <mail@mhthies.de>
2018-12-05 11:16:06 +0100

    Move po-diff-handler and improve comment
    This requires anyone, who already set up this script as textconv for a
    diff driver, to adapt his .git/config to the new path of the script.

--- i18n/git-diff-filter-po.sh
+++ i18n/git-diff-filter-po.sh
@@ -1,19 +1,20 @@
-# This is a script to handle diffs of .po files.
+#!/bin/bash
+#
+# This is a script to filter .po files for diffing.
 #
 # It ignores all lines starting with '#:' to ignore changes to line numbers
 # where translatable strings are used.
 #
 # To set this up, add the following to your .git/config file:
 #
-# [diff "podiff"]
-#   textconv = i18n/po-comments-filter
+#   [diff "podiff"]
+#     textconv = i18n/git-diff-filter-po.sh
 #
 # Then add the following line to your .git/info/attributes file:
 #
-# *.po diff=podiff
+#   *.po diff=podiff
+#   *.pot diff=podiff
 #
-# taken from:
-#
-# https://gist.github.com/stephenharris/3c3792568494b2a7cf48
+# taken from: https://gist.github.com/stephenharris/3c3792568494b2a7cf48
 
 grep -vE '^#:|POT-Creation-Date' $1
