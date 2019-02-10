af14758eab288305504b49da6f9c5addf7894d3e
Markus Oehme <veelai.1764@googlemail.com>
2019-02-10 11:35:39 +0100

    fix

--- test/test_backend_session.py
+++ test/test_backend_session.py
@@ -7,12 +7,12 @@ class TestSessionBackend(BackendTest):
 
     def test_sessionlookup(self):
         user = self.session.lookupsession("random key", "127.0.0.0")
-        self.assertIsNone(user['persona_id'])
-        self.assertEqual(False, user['is_active'])
+        self.assertIsNone(user.persona_id)
+        self.assertEqual({"anonymous"}, user.roles)
         key = self.login(USER_DICT["anton"])
         user = self.session.lookupsession(key, "127.0.0.0")
         self.assertIsInstance(user, User)
-        self.assertTrue(user.persona_id)
+        self.assertEqual(USER_DICT["anton"]['persona_id'], user.persona_id)
 
     def test_ip_mismatch(self):
         key = self.login(USER_DICT["anton"], ip="1.2.3.4")
