5313f72aec6aa225968d21ce3b3bdad113fc650c
Maarten Grothus <maarten@grothus-gebing.de>
2019-02-10 20:14:39 +0100

    doc: Change guzzle project name zu CdEDBv2
    Project name has been "Project name" before.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -103,7 +103,7 @@ extensions.append("guzzle_sphinx_theme")
 # Guzzle theme options (see theme.conf for more information)
 html_theme_options = {
     # Set the name of the project to appear in the sidebar
-    "project_nav_name": "Project Name",
+    "project_nav_name": "CdEDBv2",
 }
 ## end added for guzzle
 
