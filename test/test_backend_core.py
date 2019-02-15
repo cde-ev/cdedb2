68b92a98096a04419631c36e8a5e66f2e227cc6e
Tobias Udtke <tobias.udtke@gmail.com>
2019-02-15 18:02:21 +0100

    test: adjust test to changes in change_notes language.

--- test/test_backend_core.py
+++ test/test_backend_core.py
@@ -167,7 +167,7 @@ class TestCoreBackend(BackendTest):
                 'birth_name': None,
                 'birthday': None,
                 'bub_search': None,
-                'change_note': 'Account erstellt.',
+                'change_note': 'Persona creation.',
                 'change_status': 2,
                 'country': None,
                 'country2': None,
