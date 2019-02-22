8be9098f994a736165df4fa696e0a09e25e50a2d
Maarten Grothus <maarten@grothus-gebing.de>
2019-02-22 16:39:23 +0100

    zxcvbn: Add proper umlaut support
    The frequency lists contains now words with umlauts as it
    should, while counting their transliteration separately as
    well.
    
    To make this work, quotation marks, ndashs and bullets have
    been removed from the string.
    
    This is adding a test for proper umlaut recognition as well.

--- test/test_frontend_core.py
+++ test/test_frontend_core.py
@@ -223,7 +223,16 @@ class TestCoreFrontend(FrontendTest):
         self.assertPresence('Passwort ist zu schwach.', div="notifications")
         self.assertPresence('Füge ein oder zwei weitere Wörter hinzu. Unübliche Wörter sind besser.')
         self.assertPresence('Großschreibung hilft nicht wirklich.')
-        # Password four: User-specific passwords
+        # Password four: German umlauts
+        new_password = 'überwährend'
+        f['old_password'] = user['password']
+        f['new_password'] = new_password
+        f['new_password2'] = new_password
+        self.submit(f, check_notification=False)
+        self.assertNonPresence('Passwort geändert.')
+        self.assertPresence('Passwort ist zu schwach.', div="notifications")
+        self.assertPresence('Füge ein oder zwei weitere Wörter hinzu. Unübliche Wörter sind besser.')
+        # Password five: User-specific passwords
         new_password = (user['given_names'].replace('-', ' ').split()[0] +
                         user['family_name'].replace('-', ' ').split()[0])
         f = self.response.forms['passwordchangeform']
