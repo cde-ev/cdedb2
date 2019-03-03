4aab6693fcc5fb6c31ee8dfe2d2c2bdde335a5d9
Tobias Udtke <tobias.udtke@gmail.com>
2019-03-03 15:10:38 +0100

    frontend: allow <br> through bleach.

--- cdedb/frontend/common.py
+++ cdedb/frontend/common.py
@@ -479,7 +479,7 @@ def get_bleach_cleaner():
         # customizations
         'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'colgroup', 'col', 'tr', 'th',
         'thead', 'table', 'tbody', 'td', 'hr', 'p', 'span', 'div', 'pre', 'tt',
-        'sup']
+        'sup', 'br']
     ATTRIBUTES = {
         'a': ['href', 'title'],
         'abbr': ['title'],
