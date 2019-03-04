d2596aa4baee2e53dd831af53e9df46b06b7c82c
Tobias Udtke <tobias.udtke@gmail.com>
2019-03-04 12:14:27 +0100

    bin: add small python script to extract the translation strings from PO-file.
    This should work regardless of being executed in the vm or from outside.
    The default path of the generated file is not tracked by git.

---
+++ bin/extract_msgstr.py
@@ -0,0 +1,15 @@
+import re
+import pathlib
+
+main_dir = pathlib.Path.resolve(pathlib.Path(__file__).parent).parent
+infile = str(main_dir / "i18n/de/LC_MESSAGES/cdedb.po")
+outfile = str(main_dir / "i18n/de/LC_MESSAGES/cdedb.spellcheck")
+
+
+with open(infile, mode="r") as infile:
+    text = infile.read()
+    text = re.sub(r'(#.*\n)*msgid (".*?"\n)+msgstr ', "", text)
+    text = re.sub(r'"\n"', "", text)
+
+with open(outfile, mode="w") as outfile:
+    outfile.write(text)
