89da97c6e4fa1f9e7faa2d904a8e40e93dd41fbb
Markus Oehme <veelai.1764@googlemail.com>
2019-03-05 22:54:31 +0100

    core-frontend: Fix another TypeError.
    The variable data should consistently be a list of dicts.

--- cdedb/frontend/core.py
+++ cdedb/frontend/core.py
@@ -637,11 +637,11 @@ class CoreFrontend(AbstractFrontend):
         if {"core_admin", "admin"} & rs.user.roles:
             anid, errs = validate.check_cdedbid(phrase, "phrase")
             if not errs:
-                data = self.coreproxy.get_persona(rs, anid)
+                data = self.coreproxy.get_personas(rs, (anid,))
             else:
                 anid, errs = validate.check_id(phrase, "phrase")
                 if not errs:
-                    data = self.coreproxy.get_persona(rs, anid)
+                    data = self.coreproxy.get_personas(rs, (anid,))
 
         # Don't query, if search phrase is too short
         if not data and len(phrase) < self.conf.NUM_PREVIEW_CHARS:
