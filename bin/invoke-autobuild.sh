ff257b5cda8c061aa7269a1bf5c759afd8aaabce
Markus Oehme <veelai.1764@googlemail.com>
2019-03-15 22:34:56 +0100

    auto-build: Upgrade locking.
    We now use an actual locking solution instead of the shoehorned thing I
    previously invented.

--- bin/invoke-autobuild.sh
+++ bin/invoke-autobuild.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 BINDIR=/home/cdedb/cdedb2/bin/
+LOCKFILE=/home/cdedb/cdedb2-autobuild.lock
 LOGFILE=$(mktemp)
 MAILTO=cdedb@lists.cde-ev.de
 
-COUNT=`ps aux | grep invoke-autobuild.sh | wc -l`
-if [ $COUNT -gt 4 ]; then
-    echo "Already running $COUNT processes." | mail -s "cdedb2: auto-build abort" $MAILTO
-    exit 42
-fi
-
-$BINDIR/cdedb-autobuild-stage3.sh &> $LOGFILE
+flock -n -E -42 $LOCKFILE $BINDIR/cdedb-autobuild-stage3.sh &> $LOGFILE
 RETVAL=$?
 
 if [[ $RETVAL -eq 0 ]]; then
-	rm -f $LOGFILE
-	exit 0
+    # autobuild is up to date
+    rm -f $LOGFILE
+    exit 0
 fi;
 if [[ $RETVAL -eq 1 ]]; then
-	cat $LOGFILE | mail -s "cdedb2-auto-build: neue Version" $MAILTO
-	rm -f $LOGFILE
-	exit 0
+    cat $LOGFILE | mail -s "cdedb2-auto-build: neue Version" $MAILTO
+    rm -f $LOGFILE
+    exit 0
+fi;
+if [[ $RETVAL -eq -42 ]]; then
+    mail -s "cdedb2-auto-build: abort" $MAILTO
+    rm -f $LOGFILE
+    exit 0
 fi;
 
 cat $LOGFILE | mail -s "cdedb2: auto-build failure" $MAILTO
