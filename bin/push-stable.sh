b4a118532c5951aacabff911f22b7cc12c9926a2
Markus Oehme <veelai.1764@googlemail.com>
2019-04-22 10:46:48 +0200

    scripts: Add dry-run mode to stable push script.

--- bin/push-stable.sh
+++ bin/push-stable.sh
@@ -6,6 +6,17 @@ function notice_lines () {
     COUNT=$(($COUNT + $1))
 }
 
+if [ $# -gt 0 ]
+then
+    function push_stable () {
+        echo "Omitted push."
+    }
+else
+    function push_stable () {
+        git push origin stable
+    }
+fi
+
 for rev in $(git rev-list origin/stable..stable); do
     notice_lines $(git show -s $rev | grep -i '^\W*Deploy:' | sed -e "s/^\W*/${rev:0:8} /" | wc -l)
     git show -s $rev | grep -i '^\W*Deploy:' | sed -e "s/^\W*/${rev:0:8} /"
@@ -16,12 +27,12 @@ then
     echo ""
     select yn in "Push" "Abort"; do
         case $yn in
-            Push ) git push origin stable
+            Push ) push_stable
                    break;;
             Abort ) exit;;
         esac
     done
 
 else
-    git push origin stable
+    push_stable
 fi
