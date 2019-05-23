ee3f3e753029e749ee97d6c02c14a476c97389eb
Markus Oehme <veelai.1764@googlemail.com>
2019-05-23 21:38:22 +0200

    cron: Improve usage of werkzeug Map.bind().

--- cdedb/frontend/cron.py
+++ cdedb/frontend/cron.py
@@ -72,7 +72,8 @@ class CronFrontend(BaseApp):
             "encode_notification": self.encode_notification,
             "decode_notification": self.decode_notification,
         }
-        urls = self.urlmap.bind("db.cde-ev.de/db", url_scheme='https')
+        urls = self.urlmap.bind("db.cde-ev.de", script_name="/db/",
+                                url_scheme="https")
         rs = RequestState(
             None, user, None, None, [], urls, {},
             [], {}, lang,
