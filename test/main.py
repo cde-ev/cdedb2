49ccde527f8ffdee8900f81bcd9df63155c7e0f0
Tobias Udtke <tobias.udtke@gmail.com>
2020-03-12 19:08:10 +0100

    test: be more flexible in providing tests to run with `bin/check.sh`.
    You can now give partial desciptions, which may result in multiple test files being run:
    
    * i.e. `bin/check.sh assembly` will run the tests from both `test_backend_assembly.py`
    	as well as `test_frontend_assembly.py`.
    * `bin/check.sh frontend` will run all frontend tests etc.
    * `bin/check.sh frontend_assembly frontend_cde` will run frontend tests for cde ans assembly realm.

--- test/main.py
+++ test/main.py
@@ -5,13 +5,11 @@ import sys
 from test.common import MyTextTestResult
 
 if __name__ == "__main__":
-    pattern = 'test*.py'
-    exact_test = None
-    if len(sys.argv) > 1 and sys.argv[1].endswith('.py'):
-        pattern = sys.argv[1]
     loader = unittest.TestLoader()
-    tests = loader.discover('./test/', pattern=pattern)
     unittest.installHandler()
     testRunner = unittest.runner.TextTestRunner(
         verbosity=2, resultclass=MyTextTestResult)
-    testRunner.run(tests)
+    suite = unittest.TestSuite()
+    for arg in sys.argv[1:]:
+        suite.addTests(loader.discover('./test/', pattern='*{}*.py'.format(arg)))
+    testRunner.run(suite)
