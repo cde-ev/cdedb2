bbc18495e0b8ff1e1ee0e1d9d5292e07fbefdb92
Tobias Udtke <tobias.udtke@gmail.com>
2020-03-14 01:05:39 +0100

    parse: fix error handling when encountering a line that is too short.

--- cdedb/frontend/cde.py
+++ cdedb/frontend/cde.py
@@ -871,7 +871,7 @@ class CdEFrontend(AbstractUserFrontend):
 
         for i, line in enumerate(reversed(list(reader))):
             if not len(line) == 23:
-                p = ("statement",
+                p = ("statement_file",
                      ValueError(n_("Line %(lineno)s does not have "
                                    "the correct number of "
                                    "columns."),
@@ -885,6 +885,8 @@ class CdEFrontend(AbstractUserFrontend):
             t.inspect(rs, self.coreproxy.get_persona)
 
             transactions.append(t)
+        if rs.has_validation_errors():
+            return self.parse_statement_form(rs)
 
         data, params = self.organize_transaction_data(
             rs, transactions, start, end, timestamp)
