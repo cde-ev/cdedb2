38179fb42ba631d3f3fa449ef08f2c722a1c6a39
Tobias Udtke <tobias.udtke@gmail.com>
2020-04-08 00:58:05 +0200

    event: increase EVENT_EXPORT_VERSION

--- cdedb/common.py
+++ cdedb/common.py
@@ -1698,7 +1698,7 @@ def roles_to_db_role(roles):
 
 #: Version tag, so we know that we don't run out of sync with exported event
 #: data. This has to be incremented whenever the event schema changes.
-CDEDB_EXPORT_EVENT_VERSION = 9
+CDEDB_EXPORT_EVENT_VERSION = 10
 
 #: Default number of course choices of new event course tracks
 DEFAULT_NUM_COURSE_CHOICES = 3
