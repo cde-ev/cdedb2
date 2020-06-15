e680b5dd3f499f2b9bba2d5609600ec169d45aaf
Tobias Udtke <tobias.udtke@gmail.com>
2020-06-15 20:03:12 +0200

    in: add a dryrun mode and some comments to the attachment migration script.

--- bin/migrate_attachments_to_versioning.py
+++ bin/migrate_attachments_to_versioning.py
@@ -9,12 +9,18 @@ from cdedb.common import unwrap, get_hash, now
 
 # Configuration
 
+# While this is True, make sure to not do any actual changes.
+dry_run = True
+
 rs = setup(persona_id=-1, dbuser="cdb",
            dbpassword="987654321098765432109876543210")()
 
 assembly = make_backend("assembly", proxy=False)
 
+
+# Get to work
 with Atomizer(rs):
+    # First create the new table.
     query = \
     """CREATE TABLE assembly.attachment_versions (
     id              bigserial PRIMARY KEY,
@@ -34,14 +40,18 @@ GRANT INSERT, DELETE, UPDATE on assembly.attachment_versions TO cdb_admin;
 GRANT SELECT, UPDATE on assembly.attachment_versions_id_seq TO cdb_admin;"""
     assembly.query_exec(rs, query, tuple())
 
+    # Second retrieve the data for all current attachments.
     query = """SELECT * FROM assembly.attachments"""
     data = assembly.query_all(rs, query, tuple())
 
-    ctime_query = ("SELECT ctime FROM assembly.log WHERE code = %s"
-                   " AND additional_info = %s ORDER BY ctime DESC LIMIT 1")
+    # Third handle the current attachments.
+    new_files = []
     if not data:
         print("No attachments found")
     for e in data:
+        # Fourth try to find the original upload time from the log.
+        ctime_query = ("SELECT ctime FROM assembly.log WHERE code = %s"
+                       " AND additional_info = %s ORDER BY ctime DESC LIMIT 1")
         ctime = assembly.query_one(
             rs, ctime_query, (const.AssemblyLogCodes.attachment_added,
                               e['title']))
@@ -49,12 +59,24 @@ GRANT SELECT, UPDATE on assembly.attachment_versions_id_seq TO cdb_admin;"""
             ctime = now()
         else:
             ctime = unwrap(ctime)
+
+        # Fifth try to find the attachment file.
         path = assembly.attachment_base_path / str(e['id'])
+        new_path = assembly.attachment_base_path / (str(e['id']) + "_v1")
         if path.exists():
             with open(path, 'rb') as f:
                 file_hash = get_hash(f.read())
-            path.copy(assembly.attachment_base_path / (str(e['id']) + "_v1"))
-            print(f"Renamed {e['filename']}.")
+            if new_path.exists():
+                print(f"Destination file for {e['filename']} ({new_path})"
+                      f" already exists.")
+            else:
+                new_files.append(new_path)
+                if dry_run:
+                    path.copy(new_path)
+                    print(f"Found {e['filename']}.")
+                else:
+                    path.rename(new_path)
+                    print(f"Renamed {e['filename']}.")
         else:
             file_hash = ""
             print(f"File for attachment {e['id']} ({e['title']}) not found.'")
@@ -74,3 +96,7 @@ GRANT SELECT, UPDATE on assembly.attachment_versions_id_seq TO cdb_admin;"""
     query = ("ALTER TABLE assembly.attachments"
              " DROP COLUMN title, DROP COLUMN filename")
     assembly.query_exec(rs, query, tuple())
+    if dry_run:
+        for p in new_files:
+            p.unlink()
+        raise ValueError("Aborting dryrun without commiting changes.")
