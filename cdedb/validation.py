6730d9c1fd55d09b4b7b210dd8eaa30c6021da2a
Tobias Udtke <tobias.udtke@gmail.com>
2020-07-16 17:49:35 +0200

    code-style: make `cdedb/ml_type_aux.py` mypy compliant.

--- cdedb/validation.py
+++ cdedb/validation.py
@@ -3733,12 +3733,12 @@ def _mailinglist(val, argname=None, *, creation=False, _convert=True,
                          _ignore_warnings=_ignore_warnings)
     if errs:
         return val, errs
-    mandatory_validation_fields = (('moderators', '[id]'),)
-    optional_validation_fields = (('whitelist', '[email]'),)
+    mandatory_validation_fields = [('moderators', '[id]'),]
+    optional_validation_fields = [('whitelist', '[email]'),]
     if "ml_type" in val:
         atype = ml_type.get_type(val["ml_type"])
-        mandatory_validation_fields += atype.mandatory_validation_fields
-        optional_validation_fields += atype.optional_validation_fields
+        mandatory_validation_fields.extend(atype.mandatory_validation_fields)
+        optional_validation_fields.extend(atype.optional_validation_fields)
     mandatory_fields = dict(_MAILINGLIST_COMMON_FIELDS())
     optional_fields = dict(_MAILINGLIST_OPTIONAL_FIELDS())
     iterable_fields = []
