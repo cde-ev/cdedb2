e2f58805e552ce36165dc70ecd71adc3b530e73a
Nils Kattenbeck <nilskemail+cde@gmail.com>
2020-10-03 14:03:43 +0200

    Revert moving extensions and collations to different file

--- cdedb/database/cdedb-db.sql
+++ cdedb/database/cdedb-db.sql
@@ -5,3 +5,6 @@ DROP DATABASE IF EXISTS :cdb_database_name;
 CREATE DATABASE :cdb_database_name WITH OWNER = cdb TEMPLATE = template0 ENCODING = 'UTF8';
 
 ALTER DATABASE :cdb_database_name SET datestyle TO 'ISO, YMD';
+
+CREATE EXTENSION pg_trgm;
+CREATE COLLATION "de-u-kn-true" (provider = icu, locale = 'de-u-kn-true');
