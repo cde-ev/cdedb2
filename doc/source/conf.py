3320738b0bd817f3b1eb7bb0ad0cdf4f77f72ea2
Lars Esser <mail@lars-esser.de>
2020-10-13 10:08:50 +0200

    doc: introduce custom css for twitching the doc theme
    this reduces the space between paragraphs followed by lists
    as recommended by Johannes in
     <https://tracker.cde-ev.de/gitea/cdedb/cdedb2/issues/1267#issue-1412

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -128,7 +128,12 @@ html_favicon = "../../static/favicon.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["../../static"]
+
+
+# These paths are either relative to html_static_path or fully qualified
+# paths (eg. https://...)
+html_css_files = ['doc.css']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
