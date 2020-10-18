04df9789c13d58355b8d893585421dfadcd44f88
Lars Esser <mail@lars-esser.de>
2020-10-18 20:05:04 +0200

    ml: add link to documentation for not-privileged moderators

--- cdedb/frontend/common.py
+++ cdedb/frontend/common.py
@@ -2188,9 +2188,12 @@ def mailinglist_guard(argname: str = "mailinglist_id",
                         "moderators."))
                 if not obj.mlproxy.may_manage(rs, mailinglist_id=arg,
                                               privileged=True):
+                    link = doclink(rs, label=n_("Privileged Moderator"),
+                                   topic="Handbuch_Moderator",
+                                   anchor="privilegierte-moderatoren")
                     rs.notify("info", n_(
-                        "You have only restricted moderator access and may not "
-                        "change subscriptions."))
+                        "You have not %(link)s access and may not change "
+                        "subscriptions."), {'link': link})
             else:
                 if not obj.mlproxy.is_relevant_admin(rs, **{argname: arg}):
                     raise werkzeug.exceptions.Forbidden(rs.gettext(
