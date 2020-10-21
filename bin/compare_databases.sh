c213ab2a4e5cdca970da356c3ec467a2e312f447
Tobias Udtke <tobias.udtke@gmail.com>
2020-10-21 18:58:32 +0200

    bin: make `compare_databases.sh` not destroy local state.

--- bin/compare_databases.sh
+++ bin/compare_databases.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# Takes the base commit as the first and the target commit as the second argument.
+# This does not modify the `cdb` database, but the `cdb_text` database.
+
 export TESTPREPARATION=manual
 cd /cdedb2
 
 # old version
-echo "Checkout $2"
-git checkout $2 &> /dev/null
+echo "Checkout $1"
+git checkout $1 &> /dev/null
 git pull &> /dev/null
 ls cdedb/database/evolutions > /tmp/oldevolutions.txt
 make -B test/ancillary_files/sample_data.sql &> /dev/null
-make sample-data &> /dev/null
+make sql-test &> /dev/null
 
 # new version
-echo "Checkout $3"
-git checkout $3 &> /dev/null
+echo "Checkout $2"
+git checkout $2 &> /dev/null
 git pull &> /dev/null
 ls cdedb/database/evolutions | sort > /tmp/newevolutions.txt
 echo "Compiling list of evolutions to apply."
@@ -25,18 +28,18 @@ echo ""
 echo "" > /tmp/output-evolution.txt
 for evolution in $(cat /tmp/todoevolutions.txt); do
     echo "Apply evolution $evolution" | tee -a /tmp/output-evolution.txt
-    sudo -u postgres psql -U postgres -d $1 \
+    sudo -u postgres psql -U postgres -d cdb_test \
          -f cdedb/database/evolutions/$evolution
 done
 echo "Creating database description."
-sudo -u postgres psql -U postgres -d $1 \
+sudo -u postgres psql -U postgres -d cdb_test \
      -f bin/describe_database.sql > /tmp/old-description.txt
 bin/normalize_database_description.py /tmp/old-description.txt
 echo "Resetting database."
 make -B test/ancillary_files/sample_data.sql &> /dev/null
-make sample-data &> /dev/null
+make sql-test &> /dev/null
 echo "Creating database description again."
-sudo -u postgres psql -U postgres -d $1 \
+sudo -u postgres psql -U postgres -d cdb_test \
      -f bin/describe_database.sql > /tmp/new-description.txt
 bin/normalize_database_description.py /tmp/new-description.txt
 
@@ -48,4 +51,4 @@ echo ""
 echo "EVOLUTIONS:"
 cat /tmp/output-evolution.txt
 echo ""
-echo "OLD: $2 NEW: $3"
+echo "OLD: $1 NEW: $2"
