5f44c5e8817335a85c73df238f1bbfab564d2279
Markus Oehme <markus@jonglieren-jena.de>
2020-10-24 11:11:11 +0200

    script: Transitional unification.
    The functionality will be differentiated in the next step.

--- bin/evolution-trial.sh
+++ bin/evolution-trial.sh
@@ -1,54 +1,78 @@
 #!/bin/bash
 
-# Takes the base commit as the first and the target commit as the second argument.
-# This does not modify the `cdb` database, but the `cdb_text` database.
-
 export TESTPREPARATION=manual
 cd /cdedb2
-
-# old version
-echo "Checkout $1"
-git checkout $1 &> /dev/null
 git pull &> /dev/null
-ls cdedb/database/evolutions > /tmp/oldevolutions.txt
-make -B test/ancillary_files/sample_data.sql &> /dev/null
-make sql-test &> /dev/null
 
-# new version
+# old revision
 echo "Checkout $2"
 git checkout $2 &> /dev/null
 git pull &> /dev/null
+ls cdedb/database/evolutions > /tmp/oldevolutions.txt
+make -B test/ancillary_files/sample_data.sql &> /dev/null
+make sample-data &> /dev/null
+make sample-data-test &> /dev/null
+
+# new revision
+echo "Checkout $3"
+git checkout $3 &> /dev/null
+git pull &> /dev/null
+make -B test/ancillary_files/sample_data.sql &> /dev/null
 ls cdedb/database/evolutions | sort > /tmp/newevolutions.txt
-echo "Compiling list of evolutions to apply."
 grep /tmp/newevolutions.txt -v -f /tmp/oldevolutions.txt \
      > /tmp/todoevolutions.txt
-echo ""
-cat /tmp/todoevolutions.txt
-echo ""
 echo "" > /tmp/output-evolution.txt
 for evolution in $(cat /tmp/todoevolutions.txt); do
     echo "Apply evolution $evolution" | tee -a /tmp/output-evolution.txt
     sudo -u postgres psql -U postgres -d cdb_test \
-         -f cdedb/database/evolutions/$evolution
+         -f cdedb/database/evolutions/$evolution \
+         2>&1 | tee -a /tmp/output-evolution.txt
 done
-echo "Creating database description."
+# evolved db
 sudo -u postgres psql -U postgres -d cdb_test \
-     -f bin/describe_database.sql > /tmp/old-description.txt
-bin/normalize_database_description.py /tmp/old-description.txt
-echo "Resetting database."
-make -B test/ancillary_files/sample_data.sql &> /dev/null
-make sql-test &> /dev/null
-echo "Creating database description again."
+     -c "SELECT (table_schema || '.' || table_name) AS full_name
+         FROM information_schema.tables
+         WHERE table_schema != 'information_schema'
+               AND table_schema != 'pg_catalog'
+         ORDER BY table_schema, table_name" \
+    | sort > /tmp/alltables.txt
+for table in $(cat /tmp/alltables.txt)
+do
+    sudo -u postgres psql -U postgres -d cdb_test -c "\d $table" \
+        | sort > /tmp/spec-evolved-$table.txt
+done;
+# new db
+make i18n-compile
+make sample-data-test-shallow
 sudo -u postgres psql -U postgres -d cdb_test \
-     -f bin/describe_database.sql > /tmp/new-description.txt
-bin/normalize_database_description.py /tmp/new-description.txt
+     -c "SELECT (table_schema || '.' || table_name) AS full_name
+         FROM information_schema.tables
+         WHERE table_schema != 'information_schema'
+               AND table_schema != 'pg_catalog'
+         ORDER BY table_schema, table_name" \
+    | sort > /tmp/alltables.txt
+for table in $(cat /tmp/alltables.txt)
+do
+    sudo -u postgres psql -U postgres -d cdb_test -c "\d $table" \
+        | sort > /tmp/spec-new-$table.txt
+done;
+
 
 # perform check
+./bin/check.sh 2> >(tee -a /tmp/output-check.txt >&2)
 echo ""
-echo "DATABASE COMPARISON (this should be empty):"
-comm -3 <(sort /tmp/old-description.txt) <(sort /tmp/new-description.txt)
+echo "DATABASE COMPARISON:"
+for table in $(cat /tmp/alltables.txt)
+do
+    echo "Comparing spec for $table"
+    diff -u /tmp/spec-evolved-$table.txt /tmp/spec-new-$table.txt
+done;
+echo ""
+echo "CONDENSED REPORT:"
+grep -E '^(ERROR|FAIL):' /tmp/output-check.txt
 echo ""
 echo "EVOLUTIONS:"
 cat /tmp/output-evolution.txt
 echo ""
-echo "OLD: $1 NEW: $2"
+echo "OLD: $2 NEW: $3"
+sudo shutdown -h now
