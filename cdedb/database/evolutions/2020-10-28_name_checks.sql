c718efc105a53a9b981e75b6cb4d59b1dc759cd1
Maarten Grothus <mail@mgrothus.de>
2020-10-28 20:03:27 +0100

    database: Use explicitly named checks
    Prior, we used automatically named checks, which gives a hard time
    when dropping or modifying checks.
    
    Furthermore, the personas_realm_gender check is modified, such
    that it is also applied if a persona is archived.
    
    Closes #1457

---
+++ cdedb/database/evolutions/2020-10-28_name_checks.sql
@@ -0,0 +1,20 @@
+BEGIN;
+    ALTER TABLE core.personas DROP CONSTRAINT personas_check;
+    ALTER TABLE core.personas DROP CONSTRAINT personas_check1;
+    ALTER TABLE core.personas DROP CONSTRAINT personas_check2;
+    ALTER TABLE core.personas DROP CONSTRAINT personas_check3;
+    ALTER TABLE core.personas DROP CONSTRAINT personas_check4;
+    ALTER TABLE core.personas DROP CONSTRAINT personas_check5;
+    ALTER TABLE core.personas ADD CONSTRAINT personas_realm_gender
+        CHECK((NOT is_cde_realm AND NOT is_event_realm) OR gender IS NOT NULL);
+    ALTER TABLE core.personas ADD CONSTRAINT personas_cde_balance
+        CHECK(NOT is_cde_realm OR balance IS NOT NULL);
+    ALTER TABLE core.personas ADD CONSTRAINT personas_cde_consent
+        CHECK(NOT is_cde_realm OR decided_search IS NOT NULL);
+    ALTER TABLE core.personas ADD CONSTRAINT personas_cde_trial
+        CHECK(NOT is_cde_realm OR trial_member IS NOT NULL);
+    ALTER TABLE core.personas ADD CONSTRAINT personas_cde_bub
+        CHECK(NOT is_cde_realm OR bub_search IS NOT NULL);
+    ALTER TABLE core.personas ADD CONSTRAINT personas_cde_expuls
+        CHECK(NOT is_cde_realm OR paper_expuls IS NOT NULL);
+COMMIT;
