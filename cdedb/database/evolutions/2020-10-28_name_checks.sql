8230808b064d8ea4f8d97c1de1029e7622c1cc80
Maarten Grothus <mail@mgrothus.de>
2020-11-01 16:30:53 +0100

    database: Use transaction properly for name checks evolution

--- cdedb/database/evolutions/2020-10-28_name_checks.sql
+++ cdedb/database/evolutions/2020-10-28_name_checks.sql
@@ -1,9 +1,15 @@
 BEGIN;
+    -- DROP existing, unnamed checks
     ALTER TABLE core.personas DROP CONSTRAINT personas_check;
     ALTER TABLE core.personas DROP CONSTRAINT personas_check1;
     ALTER TABLE core.personas DROP CONSTRAINT personas_check2;
     ALTER TABLE core.personas DROP CONSTRAINT personas_check3;
     ALTER TABLE core.personas DROP CONSTRAINT personas_check4;
+    -- DROP checks that might exist if people have evolved their database
+    -- manually in weird ways
+    ALTER TABLE core.personas DROP CONSTRAINT IF EXISTS personas_check5;
+    ALTER TABLE core.personas DROP CONSTRAINT IF EXISTS personas_check6;
+    -- CREATE named, partially improved checks
     ALTER TABLE core.personas ADD CONSTRAINT personas_realm_gender
         CHECK((NOT is_cde_realm AND NOT is_event_realm) OR gender IS NOT NULL);
     ALTER TABLE core.personas ADD CONSTRAINT personas_cde_balance
@@ -17,6 +23,3 @@ BEGIN;
     ALTER TABLE core.personas ADD CONSTRAINT personas_cde_expuls
         CHECK(NOT is_cde_realm OR paper_expuls IS NOT NULL);
 COMMIT;
-
-ALTER TABLE core.personas DROP CONSTRAINT IF EXISTS personas_check5;
-ALTER TABLE core.personas DROP CONSTRAINT IF EXISTS personas_check6;
