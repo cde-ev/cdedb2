c3425926b5bf1bac61f6275ba3e2b0d8fbe48d83
Tobias Udtke <tobias.udtke@gmail.com>
2020-11-02 23:12:32 +0100

    test: remove _percentage validation test.

--- test/test_validation.py
+++ test/test_validation.py
@@ -564,18 +564,3 @@ class TestValidation(unittest.TestCase):
         self.assertEqual(msg, msg.encode('utf-8').decode('utf-8-sig'))
         self.assertEqual("\ufeff" + msg, msg.encode('utf-8-sig').decode('utf-8'))
         self.assertEqual(msg, msg.encode('utf-8-sig').decode('utf-8-sig'))
-
-    def test_percentage(self):
-        self.do_validator_test("_percentage", (
-            ("23", 23, None, False),
-            (11.0, 11, None, False),
-            (19, 19, None, True),
-            (0.97, None, ValueError, False),
-            (23.11, None, ValueError, False),
-            (1997, 1997, None, True),
-            ("abc", None, ValueError, False),
-            (1, 1, None, True),
-            (1.0, 1, None, False),
-            ("1", 1, None, False),
-            ("1.0", None, ValueError, False),
-        ))
