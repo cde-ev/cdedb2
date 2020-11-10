e9d9e850f91cead0f58c8ac14992007c595ad7db
Tobias Udtke <tobias.udtke@gmail.com>
2020-11-10 19:30:08 +0100

    database: make sure not invalid entries are entered during `shortname` initialisation.

--- cdedb/database/evolutions/2020-11-10_assembly_mailinglists.sql
+++ cdedb/database/evolutions/2020-11-10_assembly_mailinglists.sql
@@ -1,6 +1,6 @@
 BEGIN;
     ALTER TABLE assembly.assemblies ADD COLUMN shortname VARCHAR;
-    UPDATE assembly.assemblies SET shortname = title;
+    UPDATE assembly.assemblies SET shortname = 'placeholder';
     ALTER TABLE assembly.assemblies ALTER COLUMN shortname SET NOT NULL;
     ALTER TABLE assembly.assemblies RENAME COLUMN mail_address TO presider_address;
 COMMIT;
