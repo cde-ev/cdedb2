394e6e694bbed1a71a820fe71c9fbe31714799b1
Lars Esser <mail@lars-esser.de>
2020-12-15 19:07:01 +0100

    core/vcard: add tests

--- test/test_frontend_privacy.py
+++ test/test_frontend_privacy.py
@@ -27,7 +27,8 @@ class TestPrivacyFrontend(FrontendTest):
         "Zweitadresse": 'address-information',
         "Fachgebiet": 'additional', "Schule, Uni, …": 'additional',
         "Jahrgang, Matrikel, …": 'additional', "Interessen": 'additional',
-        "Sonstiges": 'additional', "Verg. Veranstaltungen": 'past-events'
+        "Sonstiges": 'additional', "Verg. Veranstaltungen": 'past-events',
+        "VCard": 'vcard'
     }
 
     ALL_FIELDS = set(FIELD_TO_DIV.keys())
@@ -83,7 +84,7 @@ class TestPrivacyFrontend(FrontendTest):
             "Geburtsname", "Geburtsdatum", "Telefon", "Mobiltelefon", "WWW",
             "Adresse", "Zweitadresse", "Fachgebiet", "Schule, Uni, …",
             "Jahrgang, Matrikel, …", "Interessen", "Sonstiges",
-            "Verg. Veranstaltungen"
+            "Verg. Veranstaltungen", "VCard"
         }
         for field in expected:
             self.assertPresence(field, div=self.FIELD_TO_DIV[field])
@@ -151,7 +152,7 @@ class TestPrivacyFrontend(FrontendTest):
         expected = {
             "Account aktiv", "Bereiche", "Admin-Privilegien", "Admin-Notizen",
             "Gedruckter exPuls", "Guthaben", "Mitgliedschaft", "Geburtsname",
-            "Geschlecht", "Geburtsdatum"
+            "Geschlecht", "Geburtsdatum", "VCard"
         }
         for field in expected:
             self.assertPresence(field, div=self.FIELD_TO_DIV[field])
