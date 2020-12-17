126b7ce17579574feac911f1e96759697cd20e1d
Lars Esser <mail@lars-esser.de>
2020-12-17 14:56:28 +0100

    core/vcard: simply use the default initial values of the QR code class

--- cdedb/frontend/core.py
+++ cdedb/frontend/core.py
@@ -374,8 +374,7 @@ class CoreFrontend(AbstractFrontend):
 
         vcard = self._create_vcard(rs, persona_id)
 
-        qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H,
-                           box_size=5, border=1)
+        qr = qrcode.QRCode()
         qr.add_data(vcard)
         qr.make(fit=True)
         qr_image = qr.make_image(qrcode_svg.SvgPathFillImage)
