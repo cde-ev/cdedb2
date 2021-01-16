87370489524468510dfa43213b59be1212c7b138
Tobias Udtke <tobias.udtke@gmail.com>
2021-01-16 17:15:38 +0100

    database: fix persona_constraint to allow purging.
    Deploy: apply the `2021-01-16_fix_persona_constraint.sql` evolution.

--- cdedb/database/cdedb-tables.sql
+++ cdedb/database/cdedb-tables.sql
@@ -145,7 +145,7 @@ CREATE TABLE core.personas (
         free_form               varchar,
         balance                 numeric(8, 2) DEFAULT NULL,
         CONSTRAINT personas_cde_balance
-            CHECK(NOT is_cde_realm OR balance IS NOT NULL),
+            CHECK(NOT is_cde_realm OR balance IS NOT NULL OR is_purged),
         -- True if user decided (positive or negative) on searchability
         decided_search          boolean DEFAULT FALSE,
         CONSTRAINT personas_cde_consent
