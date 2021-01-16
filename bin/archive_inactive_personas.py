1d7a190fdb09796767bedfaf39dad256a1362bb1
Tobias Udtke <tobias.udtke@gmail.com>
2021-01-16 17:30:18 +0100

    bin: add first draft of script for archiving very old accounts.

---
+++ bin/archive_inactive_personas.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from cdedb.script import make_backend, setup, Script
+
+# Configuration
+
+# The admin id will need to be replaces before use.
+executing_admin_id = 1
+rs = setup(persona_id=executing_admin_id, dbuser="cdb_admin",
+           dbpassword="9876543210abcdefghijklmnopqrst")
+
+DRY_RUN = True
+
+# Prepare stuff
+
+core = make_backend("core")
+
+# Execution
+
+with Script(rs(), dry_run=DRY_RUN):
+    persona_id = core.next_persona(rs(), persona_id=-1, is_member=False)
+    while persona_id:
+        is_archivable = core.is_persona_automatically_archivable(rs(), persona_id)
+        print(persona_id, is_archivable)
+        if is_archivable:
+            note = "Automatically archived after prolonged inactivity."
+            print(f"Archiving user {persona_id}...")
+            code = core.archive_persona(rs(), persona_id, note)
+            if code:
+                print("Success!")
+            else:
+                print("Error!")
+
+        persona_id = core.next_persona(rs(), persona_id=persona_id, is_member=False)
