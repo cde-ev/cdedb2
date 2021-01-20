fa6981616134dcfb7bf1b34f08388d6288aecded
Tobias Udtke <tobias.udtke@gmail.com>
2021-01-20 17:42:44 +0100

    bin: make mypy happy about archival script. Fix next_persona usage.

--- bin/archive_inactive_personas.py
+++ bin/archive_inactive_personas.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 from cdedb.common import ArchiveError
-from cdedb.script import make_backend, setup, Script
+from cdedb.script import make_backend, setup, Script, CoreBackend
 
 # Configuration
 
 # The admin id will need to be replaces before use.
 executing_admin_id = -1
 rs = setup(persona_id=executing_admin_id, dbuser="cdb_admin",
-           dbpassword="9876543210abcdefghijklmnopqrst")
+           dbpassword="9876543210abcdefghijklmnopqrst")()
 
 DRY_RUN = True
 
 # Prepare stuff
 
-core = make_backend("core")
+core: CoreBackend = make_backend("core")
 
 successful = set()
 archive_error = set()
@@ -21,19 +21,20 @@ other_error = set()
 
 # Execution
 
-with Script(rs(), dry_run=DRY_RUN):
-    persona_id = core.next_persona(rs(), persona_id=-1, is_member=False)
+with Script(rs, dry_run=DRY_RUN):
+    persona_id = core.next_persona(
+        rs, persona_id=-1, is_member=False, is_archived=False)
     while persona_id:
-        is_archivable = core.is_persona_automatically_archivable(rs(), persona_id)
+        is_archivable = core.is_persona_automatically_archivable(rs, persona_id)
         print(persona_id, is_archivable)
         if is_archivable:
             note = "Automatically archived after prolonged inactivity."
             print(f"Archiving user {persona_id}...")
             try:
-                code = core.archive_persona(rs(), persona_id, note)
-            except ArchiveError as e:
-                print(f"Error: {e}")
-                archive_error.add((persona_id, e))
+                code = core.archive_persona(rs, persona_id, note)
+            except ArchiveError as ae:
+                print(f"Error: {ae}")
+                archive_error.add((persona_id, ae))
             else:
                 if code:
                     successful.add(persona_id)
@@ -42,7 +43,8 @@ with Script(rs(), dry_run=DRY_RUN):
                     other_error.add(persona_id)
                     print("Error!")
 
-        persona_id = core.next_persona(rs(), persona_id=persona_id, is_member=False)
+        persona_id = core.next_persona(
+            rs, persona_id=persona_id, is_member=False, is_archived=False)
 
     print(f"Successfully archived {len(successful)} accounts.")
     print(f"{len(archive_error)} archivals failed with an archive error.")
