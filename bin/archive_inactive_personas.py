83c4528c186c416fcaa5a8d0fe2362e1c87b8f90
Tobias Udtke <tobias.udtke@gmail.com>
2021-01-20 23:43:04 +0100

    bin: only archive those without any sessions via script.

--- bin/archive_inactive_personas.py
+++ bin/archive_inactive_personas.py
@@ -26,8 +26,9 @@ with Script(rs, dry_run=DRY_RUN):
         rs, persona_id=-1, is_member=False, is_archived=False)
     while persona_id:
         is_archivable = core.is_persona_automatically_archivable(rs, persona_id)
-        print(persona_id, is_archivable)
-        if is_archivable:
+        last_session = core.get_persona_latest_session(rs, persona_id)
+        print(persona_id, is_archivable, last_session)
+        if is_archivable and last_session is None:
             note = "Automatically archived after prolonged inactivity."
             print(f"Archiving user {persona_id}...")
             try:
