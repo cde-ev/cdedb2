fb930ea6eb22db5c2b0d046246fe54024bad41d2
Johannes Jung <jungj+gitea@posteo.de>
2021-02-19 10:36:47 +0100

    tests: test_offline: suppress all output from creating the vm
    `make reload` printed something to stderr before, which was not DEVNULLed

--- tests/test_offline.py
+++ tests/test_offline.py
@@ -31,7 +31,8 @@ class TestOffline(FrontendTest):
             subprocess.run(
                 ['bin/make_offline_vm.py', '--test', '--no-extra-packages',
                  'tests/ancillary_files/event_export.json'],
-                cwd=base, check=True, stdout=subprocess.DEVNULL)
+                cwd=base, check=True, stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL)
             # Reset web test app for changed configuration
             try:
                 del sys.modules['cdedb.localconfig']
