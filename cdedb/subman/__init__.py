f7c96f387a15ee1e33a66792ddcc334f54eb0f11
Lars Esser <mail@lars-esser.de>
2021-03-02 18:25:01 +0100

    subman: make imports directly from subman possible

---
+++ cdedb/subman/__init__.py
@@ -0,0 +1,3 @@
+from .exceptions import SubscriptionError, SubscriptionInfo
+from .machine import SubscriptionStates, SubscriptionPolicy, SubscriptionLogCodes
+from .subman import apply_action, is_obsolete
