6017d086945dcaf4596690a9b2140deb077732d3
Tobias Udtke <tobias.udtke@gmail.com>
2021-03-02 18:45:08 +0100

    subman: add `__all__` to `__init__.py`

--- cdedb/subman/__init__.py
+++ cdedb/subman/__init__.py
@@ -1,3 +1,6 @@
 from .exceptions import SubscriptionError, SubscriptionInfo
 from .machine import SubscriptionStates, SubscriptionPolicy, SubscriptionLogCodes
 from .subman import apply_action, is_obsolete
+
+__all__ = ['SubscriptionError', 'SubscriptionInfo', 'SubscriptionStates',
+           'SubscriptionPolicy', 'SubscriptionLogCodes', 'apply_action', 'is_obsolete']
