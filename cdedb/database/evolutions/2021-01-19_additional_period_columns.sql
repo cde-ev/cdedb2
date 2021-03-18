0ec09c15ac24aca83c0bf50c943ec573cc263c6f
Markus Oehme <markus@jonglieren-jena.de>
2021-03-18 15:48:14 +0100

    sql: Fix evolution to use the intended type.

--- cdedb/database/evolutions/2021-01-19_additional_period_columns.sql
+++ cdedb/database/evolutions/2021-01-19_additional_period_columns.sql
@@ -3,6 +3,6 @@ BEGIN;
     ALTER TABLE cde.org_period ADD COLUMN archival_notification_done timestamp WITH TIME ZONE DEFAULT NULL;
     ALTER TABLE cde.org_period ADD COLUMN archival_notification_count integer NOT NULL DEFAULT 0;
     ALTER TABLE cde.org_period ADD COLUMN archival_state integer REFERENCES core.personas(id);
-    ALTER TABLE cde.org_period ADD COLUMN archival_done time WITH TIME ZONE DEFAULT NULL;
+    ALTER TABLE cde.org_period ADD COLUMN archival_done timestamp WITH TIME ZONE DEFAULT NULL;
     ALTER TABLE cde.org_period ADD COLUMN archival_count integer NOT NULL DEFAULT 0;
 COMMIT;
