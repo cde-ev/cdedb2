8f9a2c71b45bcfbb871b3a007b007cf8ba9b4e7b
Tobias Udtke <tobias.udtke@gmail.com>
2021-04-21 19:54:54 +0200

    mypy: simplify a type hint.

--- cdedb/backend/assembly.py
+++ cdedb/backend/assembly.py
@@ -1874,7 +1874,7 @@ class AssemblyBackend(AbstractBackend):
             if attachment['num_versions'] <= 1:
                 raise ValueError(n_("Cannot remove the last remaining version "
                                     "of an attachment."))
-            deletor = {
+            deletor: Dict[str, Union[int, datetime.datetime, None]] = {
                 'attachment_id': attachment_id,
                 'version': version,
                 'dtime': now(),
@@ -1887,8 +1887,7 @@ class AssemblyBackend(AbstractBackend):
             setters = ", ".join(f"{k} = %s" for k in keys)
             query = (f"UPDATE assembly.attachment_versions SET {setters}"
                      f" WHERE attachment_id = %s AND version = %s")
-            params: Union[Tuple[Union[int, datetime.datetime, None]]]
-            params = tuple(deletor[k] for k in keys) + (attachment_id, version)  # type: ignore
+            params = tuple(deletor[k] for k in keys) + (attachment_id, version)
             ret = self.query_exec(rs, query, params)
 
             if ret:
