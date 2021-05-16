1e18a9ada70a30176baaf380a336bc141b2a153c
Johannes Jung <jungj+gitea@posteo.de>
2021-05-16 19:58:55 +0200

    test: suppress uber verbose output from shallow make target
    closes #1969

--- tests/ancillary_files/clean_data.sql
+++ tests/ancillary_files/clean_data.sql
@@ -1,6 +1,8 @@
 DO $$
     DECLARE onetable varchar;
     BEGIN
+        -- suppress uber verbose output - sadly this is not possible via stdout/err distinction in Makefile
+        SET client_min_messages TO warning;
         FOR onetable IN
             SELECT table_schema || '.' || table_name AS atable FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema')
         LOOP
