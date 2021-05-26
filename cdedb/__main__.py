cf13fea60c3a73f57c52cdb970521dd6a161bf48
Nils Kattenbeck <nilskemail+cde@gmail.com>
2021-05-26 13:00:40 +0200

    misc: move import and change port

--- cdedb/__main__.py
+++ cdedb/__main__.py
@@ -1,17 +1,18 @@
 import os
 from pathlib import Path
 
+from werkzeug.serving import run_simple
+
 from cdedb.frontend.application import Application
 
 static_directory = Path(__file__).parent.parent / "static"
 
 if __name__ == "__main__":
-    from werkzeug.serving import run_simple
     os.environ["INTERACTIVE_DEBUGGER"] = "1"
     application = Application()
     run_simple(
         "0.0.0.0",
-        8000,
+        5000,
         application,
         use_debugger=True,
         use_evalex=True,
