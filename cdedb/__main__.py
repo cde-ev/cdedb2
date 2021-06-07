be800c3ce1e94915682fdf3b340304069ba11165
Nils Kattenbeck <nilskemail+cde@gmail.com>
2021-06-07 16:12:21 +0200

    Add configpath

--- cdedb/__main__.py
+++ cdedb/__main__.py
@@ -6,10 +6,11 @@ from werkzeug.serving import run_simple
 from cdedb.frontend.application import Application
 
 static_directory = Path(__file__).parent.parent / "static"
+configpath = Path("/etc/cdedb-application-config.py")
 
 if __name__ == "__main__":
     os.environ["INTERACTIVE_DEBUGGER"] = "1"
-    application = Application()
+    application = Application(configpath if configpath.is_file() else None)
     run_simple(
         "0.0.0.0",
         5000,
