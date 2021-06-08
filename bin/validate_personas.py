8cea093062fa388218ce3feab22865699378de80
Maarten Grothus <mail@mgrothus.de>
2021-06-09 01:41:07 +0200

    test: Improve test script
    This now tries to be smart about allowed data.
    Furthermore, this adds a unit test version of
    the bin script.

--- bin/validate_personas.py
+++ bin/validate_personas.py
@@ -2,7 +2,6 @@
 from typing import Optional
 
 from cdedb.script import make_backend, setup, Script, CoreBackend
-from cdedb.validation import ValidationSummary
 import cdedb.validationtypes as vtypes
 from cdedb.backend.common import affirm_validation_typed as affirm
 
@@ -28,11 +27,41 @@ with Script(rs, dry_run=False):
         if not persona_id:
             break
 
-        persona = core.get_total_persona(rs, persona_id)
-
+        # Validate ml data
+        persona = core.get_ml_user(rs, persona_id)
         try:
             affirm(vtypes.Persona, persona, _ignore_warnings=True)
         except Exception as e:
             print(f"Error for persona {persona_id}:")
             print(e.__class__)
             print(e)
+            print("--------------------------------------------------------------------------------")
+            continue
+
+        # Validate event data if applicable
+        if not persona['is_event_realm']:
+            continue
+
+        persona = core.get_event_user(rs, persona_id)
+        try:
+            affirm(vtypes.Persona, persona, _ignore_warnings=True)
+        except Exception as e:
+            print(f"Error for persona {persona_id}:")
+            print(e.__class__)
+            print(e)
+            print("--------------------------------------------------------------------------------")
+            continue
+
+        # Validate cde/total data if applicable
+        if not persona['is_cde_realm']:
+            continue
+
+        persona = core.get_total_persona(rs, persona_id)
+        try:
+            affirm(vtypes.Persona, persona, _ignore_warnings=True)
+        except Exception as e:
+            print(f"Error for persona {persona_id}:")
+            print(e.__class__)
+            print(e)
+            print("--------------------------------------------------------------------------------")
+            continue
