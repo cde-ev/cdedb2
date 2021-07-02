5d264a7dc50bfb24eec88e6d9ec251a1a6ec6826
Tobias Udtke <tobias.udtke@gmail.com>
2021-07-02 20:18:02 +0200

    ml: add "accept all", "reject all" and "discard all" buttons for mailinglist moderation.

--- cdedb/frontend/ml.py
+++ cdedb/frontend/ml.py
@@ -3,7 +3,9 @@
 """Services for the ml realm."""
 
 import urllib.error
+from typing import Collection, Tuple
 
+import mailmanclient
 from werkzeug import Response
 
 import cdedb.database.constants as const
@@ -22,6 +24,71 @@ class MlFrontend(MailmanMixin, MlBaseFrontend):
         held = self.get_mailman().get_held_messages(rs.ambience['mailinglist'])
         return self.render(rs, "message_moderation", {'held': held})
 
+    moderate_action_logcodes = {
+        "whitelist": const.MlLogCodes.moderate_accept,
+        "accept": const.MlLogCodes.moderate_accept,
+        "reject": const.MlLogCodes.moderate_reject,
+        "discard": const.MlLogCodes.moderate_discard,
+    }
+
+    def moderate_single_message(
+        self, rs: RequestState, mmlist: mailmanclient.MailingList, request_id: int,
+        action: str
+    ) -> Tuple[int, str]:
+        try:
+            held = mmlist.get_held_message(request_id)
+            change_note = f'{held.sender} / {held.subject}'
+            response = mmlist.moderate_message(request_id, action)
+        except urllib.error.HTTPError:
+            rs.notify("error", n_("Message unavailable."))
+            return 0, ""
+        else:
+            return response.status, change_note
+
+    @access("ml", modi={"POST"})
+    @mailinglist_guard()
+    @REQUESTdata("request_ids", "action")
+    def message_moderation_multi(self, rs: RequestState, mailinglist_id: int,
+                                 request_ids: Collection[int], action: str) -> Response:
+        if action not in self.moderate_action_logcodes or action in {"whitelist"}:
+            rs.add_validation_error(
+                ("action", ValueError(n_("Invalid moderation action."))))
+        if rs.has_validation_errors():
+            return self.message_moderation_form(rs, mailinglist_id)
+        dblist = rs.ambience['mailinglist']
+
+        if (self.conf["CDEDB_OFFLINE_DEPLOYMENT"] or (
+                self.conf["CDEDB_DEV"] and not self.conf["CDEDB_TEST"])):
+            self.logger.info("Skipping mailman request in dev/offline mode.")
+            rs.notify('info', n_("Skipping mailman request in dev/offline mode."))
+        else:
+            mailman = self.get_mailman()
+            mmlist = mailman.get_list_safe(dblist['address'])
+            success = warning = error = 0
+            for request_id in request_ids:
+                status, change_note = self.moderate_single_message(
+                    rs, mmlist, request_id, action)
+                if status // 100 == 2:
+                    success += 1
+                    self.mlproxy.log_moderation(
+                        rs, self.moderate_action_logcodes[action],
+                        mailinglist_id, change_note=change_note)
+                elif status // 100 == 4:
+                    warning += 1
+                else:
+                    error += 1
+            if success:
+                rs.notify("success", n_("%(count)s messages moderated."),
+                          {"count": success})
+            if warning:
+                rs.notify("success", n_("%(count)s messages not moderated."),
+                          {"count": warning})
+            if error:
+                rs.notify("success", n_("%(count)s messages not moderated."),
+                          {"count": error})
+
+        return self.redirect(rs, "ml/message_moderation")
+
     @access("ml", modi={"POST"})
     @mailinglist_guard()
     @REQUESTdata("request_id", "action", "sender")
@@ -31,18 +98,11 @@ class MlFrontend(MailmanMixin, MlBaseFrontend):
 
         Valid actions are: whitelist, accept, reject, discard
         """
-        logcode = {
-            "whitelist": const.MlLogCodes.moderate_accept,
-            "accept": const.MlLogCodes.moderate_accept,
-            "reject": const.MlLogCodes.moderate_reject,
-            "discard": const.MlLogCodes.moderate_discard,
-        }.get(action)
-        if logcode is None:
+        if action not in self.moderate_action_logcodes:
             rs.add_validation_error(
                 ("action", ValueError(n_("Invalid moderation action."))))
         if rs.has_validation_errors():
             return self.message_moderation_form(rs, mailinglist_id)
-        assert logcode is not None
         dblist = rs.ambience['mailinglist']
 
         # Add to whitelist if requested
@@ -60,19 +120,15 @@ class MlFrontend(MailmanMixin, MlBaseFrontend):
             if mmlist is None:
                 rs.notify("error", n_("List unavailable."))
             else:
-                try:
-                    held = mmlist.get_held_message(request_id)
-                    change_note = f'{held.sender} / {held.subject}'
-                    response = mmlist.moderate_message(request_id, action)
-                except urllib.error.HTTPError:
-                    rs.notify("error", n_("Message unavailable."))
+                status, change_note = self.moderate_single_message(
+                    rs, mmlist, request_id, action)
+                if status // 100 == 2:
+                    rs.notify("success", n_("Message moderated."))
+                    self.mlproxy.log_moderation(
+                        rs, self.moderate_action_logcodes[action],
+                        mailinglist_id, change_note=change_note)
+                elif status // 100 == 4:
+                    rs.notify("warning", n_("Message not moderated."))
                 else:
-                    if response.status // 100 == 2:
-                        rs.notify("success", n_("Message moderated."))
-                        self.mlproxy.log_moderation(
-                            rs, logcode, mailinglist_id, change_note=change_note)
-                    elif response.status // 100 == 4:
-                        rs.notify("warning", n_("Message not moderated."))
-                    else:
-                        rs.notify("error", n_("Message not moderated."))
+                    rs.notify("error", n_("Message not moderated."))
         return self.redirect(rs, "ml/message_moderation")
