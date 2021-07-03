b314bf8ff14aca06334fa34e9a6fd7d1b4d72ee6
Lars Esser <mail@lars-esser.de>
2021-07-03 13:58:47 +0200

    sql/ldap: add ldap_agents
    LDAP agents (Directory System Agents) are used for external services
    to bind against the LDAP

--- cdedb/database/cdedb-tables.sql
+++ cdedb/database/cdedb-tables.sql
@@ -1287,6 +1287,15 @@ CREATE TABLE ldap_organizations (
 );
 GRANT ALL ON ldap_organizations TO cdb_admin;
 
+-- corresponds to Directory System Agents
+DROP TABLE IF EXISTS ldap_agents;
+CREATE TABLE ldap_agents (
+	id serial PRIMARY KEY,
+	cn varchar NOT NULL,
+    password_hash varchar NOT NULL
+);
+GRANT ALL ON ldap_agents TO cdb_admin;
+
 DROP TABLE IF EXISTS ldap_attr_mappings;
 CREATE TABLE ldap_attr_mappings (
 	id bigserial PRIMARY KEY,
@@ -1309,7 +1318,10 @@ CREATE VIEW ldap_entries (id, dn, oc_map_id, parent, keyval) AS
             2^32 AS id_offset,
 
             2 AS persona_oc_id,
-            10 AS persona_parent
+            4 AS organizational_role_oc_id,
+
+            10 AS persona_parent,
+            12 AS organizational_role_parent
     )
     -- organizations
     (
@@ -1321,17 +1333,28 @@ CREATE VIEW ldap_entries (id, dn, oc_map_id, parent, keyval) AS
            id AS keyval
         FROM ldap_organizations
     )
-    -- personas
+    -- Directory System Agents
     UNION (
         SELECT
            id + const.id_offset,
+           'cn=' || cn || ',ou=dsa,dc=cde-ev,dc=de' AS dn,
+           const.organizational_role_oc_id AS oc_map_id,
+           const.organizational_role_parent AS parent,
+           id as keyval
+        FROM ldap_agents, const
+    )
+    -- personas
+    UNION (
+        SELECT
+           id + 2*const.id_offset,
            -- the DB-ID is really static
            'cn=' || id || ',ou=users,dc=cde-ev,dc=de' AS dn,
            const.persona_oc_id AS oc_map_id,
            const.persona_parent AS parent,
            id as keyval
         FROM core.personas, const
-);
+    )
+;
 GRANT ALL ON ldap_entries TO cdb_admin;
 
 CREATE VIEW ldap_entry_objclasses (entry_id, oc_name) AS
