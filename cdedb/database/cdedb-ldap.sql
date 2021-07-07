c5df9fe4d25a4f5b03aeb1846716e324a4ca404d
Lars Esser <mail@lars-esser.de>
2021-07-08 00:07:50 +0200

    database/ldap: add event-orgas group

--- cdedb/database/cdedb-ldap.sql
+++ cdedb/database/cdedb-ldap.sql
@@ -52,6 +52,10 @@ CREATE FUNCTION node_ml_moderators_group_id()
   RETURNS int LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
 $$ SELECT 16; $$;
 
+CREATE FUNCTION node_event_orgas_group_id()
+  RETURNS int LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
+$$ SELECT 17; $$;
+
 CREATE FUNCTION node_static_group_id()
   RETURNS int LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
 $$ SELECT 19; $$;
@@ -141,6 +145,10 @@ CREATE FUNCTION make_ml_moderators_entity_id(mailinglist_id INT)
   RETURNS bigint LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
 $$ SELECT CAST (6 * 2^32 + $1 AS BIGINT); $$ ;
 
+CREATE FUNCTION make_event_orgas_entity_id(event_id INT)
+  RETURNS bigint LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
+$$ SELECT CAST (7 * 2^32 + $1 AS BIGINT); $$ ;
+
 ---
 --- create dn
 --- Some dn's are used at multiple places. To ensure consistency, we define a
@@ -206,7 +214,8 @@ INSERT INTO ldap_organizations (id, dn, oc_map_id, parent, display_name, additio
     -- Additional organizational units holding group of groups
         (node_static_group_id(), 'ou=status,ou=groups,dc=cde-ev,dc=de', oc_organizationalUnit_id(), make_organization_entity_id(node_groups_id()), 'Status', NULL),
         (node_ml_subscribers_group_id(), 'ou=ml-subscribers,ou=groups,dc=cde-ev,dc=de', oc_organizationalUnit_id(), make_organization_entity_id(node_groups_id()), 'Mailinglists Subscribers', NULL),
-        (node_ml_moderators_group_id(), 'ou=ml-moderators,ou=groups,dc=cde-ev,dc=de', oc_organizationalUnit_id(), make_organization_entity_id(node_groups_id()), 'Mailinglists Moderators', NULL);
+        (node_ml_moderators_group_id(), 'ou=ml-moderators,ou=groups,dc=cde-ev,dc=de', oc_organizationalUnit_id(), make_organization_entity_id(node_groups_id()), 'Mailinglists Moderators', NULL),
+        (node_event_orgas_group_id(), 'ou=event-orgas,ou=groups,dc=cde-ev,dc=de', oc_organizationalUnit_id(), make_organization_entity_id(node_groups_id()), 'Event Orgas', NULL);
 
 -- ldap Directory System Agents
 DROP TABLE IF EXISTS ldap_agents;
@@ -267,6 +276,14 @@ CREATE VIEW ldap_groups (id, cn, description) AS
            make_mailinglist_cn(address) AS cn,
            title || ' <' || address || '>' AS description
         FROM ml.mailinglists
+    )
+    -- event orgas
+    UNION (
+        SELECT
+           make_event_orgas_entity_id(id),
+           CAST (id as VARCHAR) AS cn,
+           title || ' (' || shortname || ')' AS description
+        FROM event.events
     )
 ;
 GRANT ALL ON ldap_groups TO cdb_admin;
@@ -405,6 +422,13 @@ CREATE VIEW ldap_group_members (group_id, member_dn) AS
            make_ml_moderators_entity_id(mailinglist_id) AS group_id,
            make_persona_dn(persona_id) AS member_dn
         FROM ml.moderators
+    )
+    -- event orgas
+    UNION (
+        SELECT
+           make_event_orgas_entity_id(event_id) AS group_id,
+           make_persona_dn(persona_id) AS member_dn
+        FROM event.orgas
     )
 ;
 GRANT ALL ON ldap_group_members TO cdb_admin;
@@ -554,6 +578,16 @@ CREATE VIEW ldap_entries (id, dn, oc_map_id, parent, keyval) AS
                make_organization_entity_id(node_ml_moderators_group_id()) AS parent,
                make_ml_moderators_entity_id(id) as keyval
             FROM ml.mailinglists
+        )
+        -- event orgas
+        UNION (
+            SELECT
+               make_event_orgas_entity_id(id),
+               'cn=' || id || ',ou=event-orgas,ou=groups,dc=cde-ev,dc=de' AS dn,
+               oc_groupOfUniqueNames_id() AS oc_map_id,
+               make_organization_entity_id(node_event_orgas_group_id()) AS parent,
+               make_event_orgas_entity_id(id) as keyval
+            FROM event.events
         )
 ;
 GRANT ALL ON ldap_entries TO cdb_admin;
