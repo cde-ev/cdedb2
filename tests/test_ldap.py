8f8d1a6628c2c84f31f380680c3105f997d4490f
Lars Esser <mail@lars-esser.de>
2021-07-07 20:22:47 +0200

    tests/ldap: add helper function for single_result_search

--- tests/test_ldap.py
+++ tests/test_ldap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Set
+from typing import Dict, List, Set
 
 import ldap3   # type: ignore
 from ldap3.abstract.entry import Entry  # type: ignore
@@ -19,6 +19,17 @@ class TestLDAP(BasicTest):
         super().setUpClass()
         cls.server = ldap3.Server(
             cls.conf['LDAP_HOST'], port=cls.conf['LDAP_PORT'], get_info=ldap3.ALL)
+
+    def single_result_search(self, search_filter: str, attributes: List[str],
+                             expectation: Dict[str, List[str]], *,
+                             user: str = test_dsa_dn, password: str = test_dsa_pw,
+                             search_base: str = root_dn):
+        with ldap3.Connection(self.server, user=user, password=password) as conn:
+            conn.search(search_base=search_base, search_filter=search_filter,
+                        attributes=attributes)
+            self.assertEqual(len(conn.entries), 1)
+            result = conn.entries[0].entry_attributes_as_dict
+            self.assertEqual(result, expectation)
 
     def test_anonymous_bind(self) -> None:
         conn = ldap3.Connection(self.server)
@@ -63,16 +74,8 @@ class TestLDAP(BasicTest):
             'objectClass': ['organization'],
             'o': ['CdE e.V.']
         }
-
-        # First, test search by uid
         search_filter = "(objectClass=organization)"
-        with ldap3.Connection(self.server, user=self.test_dsa_dn,
-                              password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter,
-                        attributes=attributes)
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
 
     def test_organizational_unit_entity(self) -> None:
         """Check if all attributes of an organizational unit are correctly present."""
@@ -82,22 +85,13 @@ class TestLDAP(BasicTest):
             'objectClass': ['organizationalUnit'],
             'o': ['Users']
         }
-
-
-        # First, test search by uid
         search_filter = (
             "(&"
                 "(objectClass=organizationalUnit)"
                 f"(o={organizational_unit_o})"
             ")"
         )
-        with ldap3.Connection(self.server, user=self.test_dsa_dn,
-                              password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter,
-                        attributes=attributes)
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
 
     def test_user_entity(self) -> None:
         """Check if all attributes of an user are correctly present."""
@@ -114,18 +108,13 @@ class TestLDAP(BasicTest):
             'userPassword': [b'{CRYPT}$6$rounds=60000$uvCUTc5OULJF/kT5$CNYWFoGXgEwhrZ0nXmbw0jlWvqi/S6TDc1KJdzZzekFANha68XkgFFsw92Me8a2cVcK3TwSxsRPb91TLHF/si/'],
             'objectClass': ['inetOrgPerson'],
         }
-
         search_filter = (
             "(&"
             "(objectClass=inetOrgPerson)"
             f"(uid={user_id})"
             ")"
         )
-        with ldap3.Connection(self.server, user=self.test_dsa_dn, password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter, attributes=attributes)
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
 
     def test_group_entity(self) -> None:
         """Check if all attributes of groups are correctly present."""
@@ -141,20 +130,13 @@ class TestLDAP(BasicTest):
             ],
             'objectClass': ['groupOfUniqueNames']
         }
-
-        # First, test search by uid
         search_filter = (
             "(&"
             "(objectClass=groupOfUniqueNames)"
             f"(cn={group_cn})"
             ")"
         )
-        with ldap3.Connection(self.server, user=self.test_dsa_dn,
-                              password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter, attributes=attributes)
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
 
     def test_dsa_entity(self) -> None:
         """Check if all attributes of dsas are correctly present."""
@@ -165,29 +147,16 @@ class TestLDAP(BasicTest):
             'objectClass': ['organizationalRole', 'simpleSecurityObject'],
             'userPassword': [b'{CRYPT}$6$cde$n3UPrRR3mIYr21BnAeSgx3vfVp.mTChOUzN1nUxv8T12mLqUOWnyIvxpd9awmOSFuBI5R5IVmK5kBQ0dBgoIb1'],
         }
-
-        # First, test search by uid
         search_filter = (
             "(&"
             "(objectClass=organizationalRole)"
             f"(cn={dsa_cn})"
             ")"
         )
-        with ldap3.Connection(self.server, user=self.test_dsa_dn, password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter, attributes=attributes)
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
 
     def test_search_groups_of_user(self) -> None:
         user_id = 10
-        search_filter = (
-            "(&"
-                "(objectClass=groupOfUniqueNames)"
-                f"(uniqueMember=uid={user_id},ou=users,{self.root_dn})"
-            ")"
-        )
-
         expectation = {
             'cn=is_active,ou=groups,dc=cde-ev,dc=de',
             'cn=is_ml_realm,ou=groups,dc=cde-ev,dc=de',
@@ -199,9 +168,14 @@ class TestLDAP(BasicTest):
             'cn=moderatoren@lists.cde-ev.de,ou=mailinglists,ou=groups,dc=cde-ev,dc=de',
             'cn=witz@lists.cde-ev.de,ou=mailinglists,ou=groups,dc=cde-ev,dc=de'
         }
+        search_filter = (
+            "(&"
+                "(objectClass=groupOfUniqueNames)"
+                f"(uniqueMember=uid={user_id},ou=users,{self.root_dn})"
+            ")"
+        )
         with ldap3.Connection(self.server, user=self.test_dsa_dn, password=self.test_dsa_pw) as conn:
             conn.search(search_base=self.root_dn, search_filter=search_filter)
-            # one entry has class Entry
             result_names: Set[str] = {entry.entry_dn for entry in conn.entries}
             self.assertEqual(result_names, expectation)
 
@@ -216,17 +190,11 @@ class TestLDAP(BasicTest):
             f"(cn={group_cn})"
             ")"
         )
-        # second, also request some attributes of the users groups
         expectation = {
             'cn': ['42@lists.cde-ev.de'],
             'objectClass': ['groupOfUniqueNames']
         }
-        with ldap3.Connection(self.server, user=self.test_dsa_dn, password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter, attributes=attributes)
-            # convert each entry to a dict[attribte, List[value]]
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
 
     def test_search_user_attributes(self) -> None:
         """Search a user by given attributes and return some of its attributes."""
@@ -248,11 +216,7 @@ class TestLDAP(BasicTest):
             f"(uid={user_id})"
             ")"
         )
-        with ldap3.Connection(self.server, user=self.test_dsa_dn, password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter, attributes=attributes)
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
 
         # Second, test search by email
         search_filter = (
@@ -261,8 +225,4 @@ class TestLDAP(BasicTest):
             f"(mail={user_mail})"
             ")"
         )
-        with ldap3.Connection(self.server, user=self.test_dsa_dn, password=self.test_dsa_pw) as conn:
-            conn.search(search_base=self.root_dn, search_filter=search_filter, attributes=attributes)
-            self.assertEqual(len(conn.entries), 1)
-            result = conn.entries[0].entry_attributes_as_dict
-            self.assertEqual(result, expectation)
+        self.single_result_search(search_filter, attributes, expectation)
