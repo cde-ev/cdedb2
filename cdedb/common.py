ce0559e2720c62ba937599327b578c863ac163c5
Michael Thies <mail@mhthies.de>
2021-08-15 14:11:23 +0200

    common: Refactor asciificator() and add inverse_diacritic_patterns()

--- cdedb/common.py
+++ cdedb/common.py
@@ -1343,6 +1343,37 @@ def n_(x: str) -> str:
     return x
 
 
+UMLAUT_MAP = {
+    "ä": "ae", "æ": "ae",
+    "Ä": "AE", "Æ": "AE",
+    "ö": "oe", "ø": "oe", "œ": "oe",
+    "Ö": "Oe", "Ø": "Oe", "Œ": "Oe",
+    "ü": "ue",
+    "Ü": "Ue",
+    "ß": "ss",
+    "à": "a", "á": "a", "â": "a", "ã": "a", "å": "a", "ą": "a",
+    "À": "A", "Á": "A", "Â": "A", "Ã": "A", "Å": "A", "Ą": "A",
+    "ç": "c", "č": "c", "ć": "c",
+    "Ç": "C", "Č": "C", "Ć": "C",
+    "è": "e", "é": "e", "ê": "e", "ë": "e", "ę": "e",
+    "È": "E", "É": "E", "Ê": "E", "Ë": "E", "Ę": "E",
+    "ì": "i", "í": "i", "î": "i", "ï": "i",
+    "Ì": "I", "Í": "I", "Î": "I", "Ï": "I",
+    "ł": "l",
+    "Ł": "L",
+    "ñ": "n", "ń": "n",
+    "Ñ": "N", "Ń": "N",
+    "ò": "o", "ó": "o", "ô": "o", "õ": "o", "ő": "o",
+    "Ò": "O", "Ó": "O", "Ô": "O", "Õ": "O", "Ő": "O",
+    "ù": "u", "ú": "u", "û": "u", "ű": "u",
+    "Ù": "U", "Ú": "U", "Û": "U", "Ű": "U",
+    "ý": "y", "ÿ": "y",
+    "Ý": "Y", "Ÿ": "Y",
+    "ź": "z",
+    "Ź": "Z",
+}
+
+
 def asciificator(s: str) -> str:
     """Pacify a string.
 
@@ -1350,39 +1381,10 @@ def asciificator(s: str) -> str:
     be used if your use case does not tolerate any fancy characters
     (like SEPA files).
     """
-    umlaut_map = {
-        "ä": "ae", "æ": "ae",
-        "Ä": "AE", "Æ": "AE",
-        "ö": "oe", "ø": "oe", "œ": "oe",
-        "Ö": "Oe", "Ø": "Oe", "Œ": "Oe",
-        "ü": "ue",
-        "Ü": "Ue",
-        "ß": "ss",
-        "à": "a", "á": "a", "â": "a", "ã": "a", "å": "a", "ą": "a",
-        "À": "A", "Á": "A", "Â": "A", "Ã": "A", "Å": "A", "Ą": "A",
-        "ç": "c", "č": "c", "ć": "c",
-        "Ç": "C", "Č": "C", "Ć": "C",
-        "è": "e", "é": "e", "ê": "e", "ë": "e", "ę": "e",
-        "È": "E", "É": "E", "Ê": "E", "Ë": "E", "Ę": "E",
-        "ì": "i", "í": "i", "î": "i", "ï": "i",
-        "Ì": "I", "Í": "I", "Î": "I", "Ï": "I",
-        "ł": "l",
-        "Ł": "L",
-        "ñ": "n", "ń": "n",
-        "Ñ": "N", "Ń": "N",
-        "ò": "o", "ó": "o", "ô": "o", "õ": "o", "ő": "o",
-        "Ò": "O", "Ó": "O", "Ô": "O", "Õ": "O", "Ő": "O",
-        "ù": "u", "ú": "u", "û": "u", "ű": "u",
-        "Ù": "U", "Ú": "U", "Û": "U", "Ű": "U",
-        "ý": "y", "ÿ": "y",
-        "Ý": "Y", "Ÿ": "Y",
-        "ź": "z",
-        "Ź": "Z",
-    }
     ret = ""
     for char in s:
-        if char in umlaut_map:
-            ret += umlaut_map[char]
+        if char in UMLAUT_MAP:
+            ret += UMLAUT_MAP[char]
         elif char in (  # pylint: disable=superfluous-parens
             string.ascii_letters + string.digits + " /-?:().,+"
         ):
@@ -1441,6 +1443,27 @@ def diacritic_patterns(s: str, two_way_replace: bool = False) -> str:
         for _, regex in umlaut_map:
             s = re.sub(regex, regex, s, flags=re.IGNORECASE)
     return s
+
+
+def inverse_diacritic_patterns(s: str) -> str:
+    """
+    Replace diacritic letters in a search pattern with a regex that
+    matches either the diacritic letter or its ASCII representation.
+
+    This function does kind of the opposite thing than
+    :func:`diacritic_patterns`: Instead of enhancing a search expression such
+    that also searches for similiar words with diacritics, it takes a word with
+    diacritic characters and enhances it to a search expression that will find
+    the word even when written without the diacritics.
+    """
+    ret = ""
+    for char in s:
+        if char in UMLAUT_MAP:
+            repl = UMLAUT_MAP[char]
+            ret += f"({char}|{repl})" if len(char) > 1 else f"[{char}{repl}]"
+        else:
+            ret += char
+    return ret
 
 
 _tdelta = datetime.timedelta
