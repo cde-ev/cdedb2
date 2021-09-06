e757d751440ea4ca9369058fac52aee5d446e8f4
Markus Walter <markus@jonglieren-jena.de>
2021-09-06 17:46:38 +0200

    parse: Move output format constant to more general place.

--- cdedb/common.py
+++ cdedb/common.py
@@ -2219,3 +2219,7 @@ EPSILON = 10 ** (-6)  #:
 #: Timestamp which lies in the future. Make a constant so we do not have to
 #: hardcode the value otherwere
 FUTURE_TIMESTAMP = datetime.datetime(9996, 1, 1, 0, 0, 0, tzinfo=pytz.utc)
+
+#: Specification for the output date format of money transfers.
+#: Note how this differs from the input in that we use 4 digit years.
+PARSE_OUTPUT_DATEFORMAT = "%d.%m.%Y"
