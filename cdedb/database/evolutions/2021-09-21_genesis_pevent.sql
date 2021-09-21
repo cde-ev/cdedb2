dab447ba1bde3700ec592aed4a8f6028392e3052
Tobias Udtke <tobias.udtke@gmail.com>
2021-09-22 00:17:46 +0200

    database: fix evolution typo.

--- cdedb/database/evolutions/2021-09-21_genesis_pevent.sql
+++ cdedb/database/evolutions/2021-09-21_genesis_pevent.sql
@@ -1,3 +1,3 @@
 BEGIN;
-    ALTER TABLE core.gensis_cases ADD COLUMN pevent_id integer REFERENCES past_event.events(id) DEFAULT NULL;
+    ALTER TABLE core.genesis_cases ADD COLUMN pevent_id integer REFERENCES past_event.events(id) DEFAULT NULL;
 COMMIT;
