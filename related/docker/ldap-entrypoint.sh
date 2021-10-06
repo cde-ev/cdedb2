d1b4772c7e1567eec7a76edd5ea45941c60ff60c
Lars Esser <mail@lars-esser.de>
2021-10-06 15:29:54 +0200

    ldap/docker: use the new python scripts to create the ldap templates
    this can be improved further by replacing the search+replace hacks
    with sensible config option during template rendering.
    
    This needs a way to apply the add-duas.sql script in docker...

--- related/docker/ldap-entrypoint.sh
+++ related/docker/ldap-entrypoint.sh
@@ -9,17 +9,22 @@ if [ ! -e /var/lib/ldap/container_already_initalized ]; then
     # consequently we give the app container time to create the sample-data
     sleep 20
 
+    # TODO run the add-duas.sql script produced by create-ldap.py
+
     # remove pre-installed mdb. This uses the same olcSuffix and blocks our sql database
     rm /etc/ldap/slapd.d/cn=config/olcDatabase=\{1\}mdb.ldif
 
+    # TODO replace the following hacks with a proper config during rendering the templates.
     # Replace localhost with cdb as that is where the db is accesible by default.
-    sed -i "s/localhost/${DATABASE_HOST:-cdb}/" /etc/odbc.ini /app/sql-ldap.ldif
+    sed -i "s/localhost/${DATABASE_HOST:-cdb}/" /etc/odbc.ini /app/config-ldap.ldif
+    sed -i "s/localhost/${DATABASE_HOST:-cdb}/" /etc/odbc.ini /app/cdedb-ldap.ldif
 
     # This is required for testing where the database name differs.
     if [ ! -z "${DATABASE_NAME}" ]; then
         sed -i -r "s/\[cdb\]/[${DATABASE_NAME}]/" /etc/odbc.ini
         sed -i -r "s/(Database\s*=\s*)cdb/\1${DATABASE_NAME}/" /etc/odbc.ini
-        sed -i -r "s/(olcDbName:\s*)cdb/\1${DATABASE_NAME}/" /app/sql-ldap.ldif
+        sed -i -r "s/(olcDbName:\s*)cdb/\1${DATABASE_NAME}/" /app/config-ldap.ldif
+        sed -i -r "s/(olcDbName:\s*)cdb/\1${DATABASE_NAME}/" /app/cdedb-ldap.ldif
     fi
 
     # Start slapd in the background (in foreground mode with -d 0 to prevent forking)
@@ -28,7 +33,8 @@ if [ ! -e /var/lib/ldap/container_already_initalized ]; then
     # Wait for slapd to come up.
     sleep 5
     # Run ldapmodify to initialize the ldap-sql backend and config.
-    ldapmodify -Y EXTERNAL -H ldapi:// -f /app/sql-ldap.ldif
+    ldapmodify -Y EXTERNAL -H ldapi:// -f /app/config-ldap.ldif
+    ldapmodify -Y EXTERNAL -H ldapi:// -f /app/cdedb-ldap.ldif
     # Stop the slapd process we started first.
     kill -INT "$(cat /run/slapd/slapd.pid)"
 
