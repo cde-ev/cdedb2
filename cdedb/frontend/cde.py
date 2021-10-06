54ba2872428e7396564a2712185cad91be685efa
Tobias Udtke <tobias.udtke@gmail.com>
2021-10-06 04:33:08 +0200

    cde: add new mixins to final CdEFrontend.

--- cdedb/frontend/cde.py
+++ cdedb/frontend/cde.py
@@ -5,7 +5,10 @@
 from cdedb.frontend.cde_base import CdEBaseFrontend
 from cdedb.frontend.cde_lastschrift import CdELastschriftMixin
 from cdedb.frontend.cde_past_event import CdEPastEventMixin
+from cdedb.frontend.cde_parse import CdEParseMixin
+from cdedb.frontend.cde_semester import CdESemesterMixin
 
 
-class CdEFrontend(CdELastschriftMixin, CdEPastEventMixin, CdEBaseFrontend):
+class CdEFrontend(CdELastschriftMixin, CdEPastEventMixin, CdEParseMixin,
+                  CdESemesterMixin, CdEBaseFrontend):
     pass
