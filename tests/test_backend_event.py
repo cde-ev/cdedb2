4ceb1cf644f470a7f5684d7f7490731dd7130624
Tobias Udtke <tobias.udtke@gmail.com>
2021-10-20 15:57:41 +0200

    tests: adjust readonly values in tests.

--- tests/test_backend_event.py
+++ tests/test_backend_event.py
@@ -1483,7 +1483,7 @@ class TestEventBackend(BackendTest):
                      'default_value': None,
                      'info': None,
                      'pos': 0,
-                     'readonly': None,
+                     'readonly': False,
                      'input_size': None,
                      'title': 'Ich bin unter 13 Jahre alt.',
                      'kind': const.QuestionnaireUsages.registration,
@@ -1620,7 +1620,7 @@ class TestEventBackend(BackendTest):
                     'default_value': None,
                     'info': "Du kannst freiwillig etwas mehr bezahlen um zukünftige"
                             " Akademien zu unterstützen.",
-                    'readonly': None,
+                    'readonly': False,
                     'input_size': None,
                     'title': "Ich möchte den Solidaritätszuschlag bezahlen.",
                 },
