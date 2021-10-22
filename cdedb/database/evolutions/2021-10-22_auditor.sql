82955a0d7d18f36c6f2586a0ca6689084343cfa5
Tobias Udtke <tobias.udtke@gmail.com>
2021-10-22 03:53:09 +0200

    database: fix evolution syntax

--- cdedb/database/evolutions/2021-10-22_auditor.sql
+++ cdedb/database/evolutions/2021-10-22_auditor.sql
@@ -1,12 +1,15 @@
 BEGIN;
     -- Create new `is_auditor` columns.
     ALTER TABLE core.personas ADD COLUMN is_auditor boolean NOT NULL DEFAULT FALSE;
-    GRANT SELECT is_auditor ON core.personas TO cdb_anonymous;
+    GRANT SELECT (is_auditor) ON core.personas TO cdb_anonymous;
     ALTER TABLE core.personas ADD CONSTRAINT personas_auditor CHECK (NOT is_auditor OR is_cde_realm);
     ALTER TABLE core.changelog ADD COLUMN is_auditor boolean DEFAULT FALSE;
     ALTER TABLE core.changelog ALTER COLUMN is_auditor DROP DEFAULT;
     ALTER TABLE core.privilege_changes ADD COLUMN is_auditor boolean DEFAULT NULL;
 
+    REVOKE SELECT ON core.log FROM cdb_admin;
+    REVOKE SELECT ON cde.log FROM cdb_admin;
+    REVOKE SELECT ON past_event.log FROM cdb_admin;
     GRANT SELECT ON core.log TO cdb_member;
     GRANT SELECT ON cde.log TO cdb_member;
     GRANT SELECT ON past_event.log TO cdb_member;
