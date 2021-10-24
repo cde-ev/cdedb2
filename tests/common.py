2b278bc8ed109d79bfba7acdcdc00c8efc94e125
Tobias Udtke <tobias.udtke@gmail.com>
2021-10-24 19:34:16 +0200

    tests: fix `get_sample_data` conversions for empty values`.

--- tests/common.py
+++ tests/common.py
@@ -291,12 +291,12 @@ class BasicTest(unittest.TestCase):
             for k in keys:
                 r[k] = copy.deepcopy(_SAMPLE_DATA[table][anid][k])
                 if table == 'core.personas':
-                    if k == 'balance':
+                    if k == 'balance' and r[k]:
                         r[k] = decimal.Decimal(r[k])
-                    if k == 'birthday':
+                    if k == 'birthday' and r[k]:
                         r[k] = datetime.date.fromisoformat(r[k])
                 if k in {'ctime', 'atime', 'vote_begin', 'vote_end',
-                         'vote_extension_end', 'signup_end'}:
+                         'vote_extension_end', 'signup_end'} and r[k]:
                     r[k] = parse_datetime(r[k])
             ret[anid] = r
         return ret
