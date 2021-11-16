f17148260da85c898426ddc2018c4fe05dbad4db
Maarten Grothus <mail@mgrothus.de>
2021-11-16 20:04:00 +0100

    test: Fix tests

--- tests/test_frontend_cde.py
+++ tests/test_frontend_cde.py
@@ -882,9 +882,9 @@ class TestCdEFrontend(FrontendTest):
         self.assertPresence("Dr. Zelda Zeruda-Hime von und zu",
                             div='personal-information')
         self.assertPresence("05.06.1987", div='personal-information')
-        self.assertPresence("030 456790", div='contact-telephone',
+        self.assertPresence("+49 30 456790", div='contact-telephone',
                             exact=True)
-        self.assertPresence("0160 20479204", div='contact-mobile', exact=True)
+        self.assertPresence("+49 160 20479204", div='contact-mobile', exact=True)
         self.assertPresence("12345 Lynna", div='address')
         self.assertPresence("Ligusterweg 4", div='address2')
         self.assertPresence("CdE-Mitglied (Probemitgliedschaft)",
