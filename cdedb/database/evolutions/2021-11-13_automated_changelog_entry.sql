0901777886b801d0609fe587bd76ed32fdc6c4d3
Tobias Udtke <tobias.udtke@gmail.com>
2021-11-16 15:23:29 +0100

    database: make changelog.automated_change not nullable in evolution.

--- cdedb/database/evolutions/2021-11-13_automated_changelog_entry.sql
+++ cdedb/database/evolutions/2021-11-13_automated_changelog_entry.sql
@@ -1,3 +1,3 @@
 BEGIN;
-    ALTER TABLE core.changelog ADD COLUMN automated_change boolean DEFAULT FALSE;
+    ALTER TABLE core.changelog ADD COLUMN automated_change boolean NOT NULL DEFAULT FALSE;
 COMMIT;
