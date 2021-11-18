a10c8c8093b0ae50f0692be29195f4cd7eff847f
Tobias Udtke <tobias.udtke@gmail.com>
2021-11-18 19:25:12 +0100

    i18n: add log code translations.

--- cdedb/i18n_additional.py
+++ cdedb/i18n_additional.py
@@ -113,6 +113,11 @@ I18N_STRINGS = (
     n_("EventLogCodes.minor_form_removed"),
     n_("EventLogCodes.query_stored"),
     n_("EventLogCodes.query_deleted"),
+    n_("EventLogCodes.part_group_created"),
+    n_("EventLogCodes.part_group_changed"),
+    n_("EventLogCodes.part_group_deleted"),
+    n_("EventLogCodes.part_group_link_created"),
+    n_("EventLogCodes.part_group_link_deleted"),
 
     n_("FinanceLogCodes.new_member"),
     n_("FinanceLogCodes.gain_membership"),
