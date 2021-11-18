7b111d29a106b8bfa8ccfd94efdb7752666c5582
Tobias Udtke <tobias.udtke@gmail.com>
2021-11-18 16:34:50 +0100

    database: restrict update privileges for part groups.

--- cdedb/database/cdedb-tables.sql
+++ cdedb/database/cdedb-tables.sql
@@ -726,7 +726,8 @@ CREATE TABLE event.part_groups (
         constraint_type         integer NOT NULL
 );
 CREATE INDEX idx_event_part_groups_event_id ON event.part_groups(event_id);
-GRANT INSERT, SELECT, UPDATE, DELETE ON event.part_groups TO cdb_persona;
+GRANT INSERT, SELECT, DELETE ON event.part_groups TO cdb_persona;
+GRANT UPDATE(title, shortname, notes) ON event.part_groups TO cdb_persona;
 GRANT SELECT, UPDATE ON event.part_groups_id_seq TO cdb_persona;
 GRANT SELECT ON event.part_groups TO cdb_anonymous;
 
@@ -736,7 +737,7 @@ CREATE TABLE event.part_group_parts (
         part_id                 integer REFERENCES event.event_parts(id)
 );
 CREATE UNIQUE INDEX idx_part_group_parts_constraint ON event.part_group_parts(part_group_id, part_id);
-GRANT INSERT, SELECT, UPDATE, DELETE ON event.part_group_parts TO cdb_persona;
+GRANT INSERT, SELECT, DELETE ON event.part_group_parts TO cdb_persona;
 GRANT SELECT, UPDATE ON event.part_group_parts TO cdb_persona;
 GRANT SELECT ON event.part_group_parts TO cdb_anonymous;
 
--- cdedb/database/evolutions/2021-10-30_event_part_groups.sql
+++ cdedb/database/evolutions/2021-10-30_event_part_groups.sql
@@ -8,7 +8,8 @@ BEGIN;
             constraint_type         integer NOT NULL
     );
     CREATE INDEX idx_event_part_groups_event_id ON event.part_groups(event_id);
-    GRANT INSERT, SELECT, UPDATE, DELETE ON event.part_groups TO cdb_persona;
+    GRANT INSERT, SELECT, DELETE ON event.part_groups TO cdb_persona;
+    GRANT UPDATE(title, shortname, notes) ON event.part_groups TO cdb_persona;
     GRANT SELECT, UPDATE ON event.part_groups_id_seq TO cdb_persona;
     GRANT SELECT ON event.part_groups TO cdb_anonymous;
 
@@ -18,7 +19,7 @@ BEGIN;
             part_id                 integer REFERENCES event.event_parts(id)
     );
     CREATE UNIQUE INDEX idx_part_group_parts_constraint ON event.part_group_parts(part_group_id, part_id);
-    GRANT INSERT, SELECT, UPDATE, DELETE ON event.part_group_parts TO cdb_persona;
+    GRANT INSERT, SELECT, DELETE ON event.part_group_parts TO cdb_persona;
     GRANT SELECT, UPDATE ON event.part_group_parts TO cdb_persona;
     GRANT SELECT ON event.part_group_parts TO cdb_anonymous;
 COMMIT;
