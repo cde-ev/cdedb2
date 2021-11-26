9e74be52e5a0072458d3fc3a620eea271f3d5040
Maarten Grothus <mail@mgrothus.de>
2021-11-26 01:40:02 +0100

    ml: More variety regarding held messages

--- cdedb/frontend/common.py
+++ cdedb/frontend/common.py
@@ -1250,7 +1250,11 @@ class CdEMailmanClient(mailmanclient.Client):
         if self.conf["CDEDB_OFFLINE_DEPLOYMENT"] or self.conf["CDEDB_DEV"]:
             self.logger.info("Skipping mailman query in dev/offline mode.")
             if self.conf["CDEDB_DEV"]:
-                return HELD_MESSAGE_SAMPLE
+                # Some diversity regarding moderation.
+                if dblist['id'] % 2 == 0:
+                    return HELD_MESSAGE_SAMPLE
+                else:
+                    return []
             return None
         else:
             mmlist = self.get_list_safe(dblist['address'])
