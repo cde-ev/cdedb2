93cd6c1a80987151a50258d52a7f960c6dfba84e
Lars Esser <mail@lars-esser.de>
2021-12-13 12:54:53 +0100

    bin/check: adopt script to new testing strategy
    load the tests accordingly to their TestCase class to have more
    controll over which tests are run at which time.
    
    The idea is to add an own function for each invocation scenario
    of the script, like it is currently done for the regular tests.

--- bin/check.py
+++ bin/check.py
@@ -6,6 +6,7 @@ import pathlib
 import subprocess
 import sys
 import unittest
+from unittest import TestLoader, TestSuite
 from types import TracebackType
 from typing import List, Optional, TextIO, Tuple, Type
 
@@ -19,6 +20,33 @@ os.chdir(root)
 from bin.test_runner_helpers import MyTextTestResult, MyTextTestRunner, check_test_setup
 from tests.prepare_tests import prepare_environment
 
+# import all TestCases which should be tested
+import tests.backend_tests as backend_tests
+import tests.frontend_tests as frontend_tests
+from tests.test_common import TestCommon
+from tests.test_config import TestConfig
+from tests.test_database import TestDatabase
+from tests.test_ldap import TestLDAP
+from tests.test_script import TestScript
+from tests.test_session import TestSessionBackend, TestSessionFrontend, TestMultiSessionFrontend
+from tests.test_subman import SubmanTest
+from tests.test_validation import TestValidation
+from tests.test_vote_verification_script import TestVerificationScript
+from tests.test_zzzoffline import TestOffline
+
+
+TEST_CASES = {
+    "regular": [
+        TestCommon, TestConfig, TestDatabase, TestScript,
+        TestSessionBackend, TestSessionFrontend, TestMultiSessionFrontend,
+        SubmanTest,
+        TestValidation, TestVerificationScript, TestOffline
+    ],
+    "ldap": [
+        TestLDAP
+    ]
+}
+
 
 class CdEDBTestLock:
     """
@@ -30,6 +58,7 @@ class CdEDBTestLock:
     # TODO: improve this in #1948
     THREADS = (1, 2, 3, 4)
 
+    configpath: pathlib.Path
     thread_id: Optional[int]
     lockfile: TextIO
 
@@ -55,6 +84,7 @@ class CdEDBTestLock:
                 try:
                     self.thread_id = thread_id
                     self.lockfile = open(self.lockfile_path, 'x')
+                    self.configpath = root / f"tests/config/test_{self.thread_id}.py"
                     return
                 except FileExistsError:
                     continue
@@ -106,6 +136,25 @@ def check_xss(payload: str, thread_id: int = 1, verbose: bool = False,
     return ret.returncode
 
 
+def _load_tests(testpatterns: List[str], test_modules=None, test_cases=None) -> TestSuite:
+    """Load all tests from test_modules and test_cases matching one of testpatterns."""
+    test_loader = TestLoader()
+    test_suite = TestSuite()
+
+    # when no/empty pattern given, specify nothing to run all tests given
+    if testpatterns:
+        test_loader.testNamePatterns = [
+            pattern if "*" in pattern else f"*{pattern}*" for pattern in testpatterns
+        ]
+
+    for test_module in test_modules:
+        test_suite.addTests(test_loader.loadTestsFromModule(test_module))
+    for test_case in test_cases:
+        test_suite.addTests(test_loader.loadTestsFromTestCase(test_case))
+
+    return test_suite
+
+
 def run_tests():
     pass
 
@@ -113,18 +162,17 @@ def run_tests():
 def run_regular_tests(configpath: pathlib.Path, testpatterns: List[str] = None, *,
                       verbose: bool = False) -> int:
     prepare_environment(configpath)
+    os.environ['CDEDB_TEST_CONFIGPATH'] = str(configpath)
 
-    if testpatterns:  # when no/empty pattern given, specify nothing to run full suite
-        unittest.defaultTestLoader.testNamePatterns = [
-            pattern if "*" in pattern else f"*{pattern}*" for pattern in testpatterns
-        ]
-    # TODO exclude ldap tests
-    all_tests = unittest.defaultTestLoader.discover('tests', top_level_dir=str(root))
+    # load all tests which are not meant to be run separately (f.e. the ldap tests)
+    test_cases = TEST_CASES["regular"]
+    test_modules = [backend_tests, frontend_tests]
+    test_suite = _load_tests(testpatterns, test_modules, test_cases)
 
     unittest.installHandler()
     test_runner = MyTextTestRunner(verbosity=(2 if verbose else 1),
                                    resultclass=MyTextTestResult, descriptions=False)
-    ran_tests = test_runner.run(all_tests)
+    ran_tests = test_runner.run(test_suite)
     return 0 if ran_tests.wasSuccessful() else 1
 
 
@@ -132,26 +180,31 @@ def run_xss_tests():
     pass
 
 
-def run_ldap_tests():
-    pass
+def run_ldap_tests(testpatterns: List[str] = None, *, verbose: bool = False) -> int:
+    configpath = root / "tests/config/test_ldap.py"
+    prepare_environment(configpath)
+    os.environ['CDEDB_TEST_CONFIGPATH'] = str(configpath)
+
+    test_suite = _load_tests(testpatterns, test_cases=TEST_CASES["ldap"])
+
+    unittest.installHandler()
+    test_runner = MyTextTestRunner(verbosity=(2 if verbose else 1),
+                                   resultclass=MyTextTestResult, descriptions=False)
+    ran_tests = test_runner.run(test_suite)
+    return 0 if ran_tests.wasSuccessful() else 1
 
 
 if __name__ == '__main__':
-    configpath = root / "tests/config/test_1.py"
-    os.environ['CDEDB_TEST_CONFIGPATH'] = str(configpath)
-    # pattern = "test_dummy"
-    # code = run_regular_tests(configpath, testpatterns=[pattern], verbose=True)
-    code = run_regular_tests(configpath, verbose=True)
-    exit()
-
     # parse arguments
     parser = argparse.ArgumentParser(
         description="Entry point to CdEDB's testing facilities.")
     parser.add_argument('testpatterns', default=[], nargs="*")
 
     test_options = parser.add_argument_group("general options")
+    # TODO is this necessary?
     test_options.add_argument('--manual-preparation', action='store_true',
                               help="don't do test preparation")
+    # TODO is this necessary?
     test_options.add_argument(
         '--thread-id', type=int, choices=(1, 2, 3, 4), metavar="INT",
         help="ID of thread to use for run (optional, if not given, choose free thread"
@@ -183,22 +236,18 @@ if __name__ == '__main__':
 
     # splitup in three parts with similar runtime
     if args.first:
-        args.testpatterns.append('tests.test_frontend_[abcd]*')
+        args.testpatterns.append('tests.frontend_tests.[abcd]*')
     if args.second:
-        args.testpatterns.append('tests.test_frontend_[!abcd]*')
+        args.testpatterns.append('tests.frontend_tests.[!abcd]*')
     if args.third:
+        args.testpatterns.append('tests.backend_tests.*')
         args.testpatterns.append('tests.test_[!f]*')
 
     with CdEDBTestLock(args.thread_id) as Lock:
         assert Lock.thread_id is not None
         print(f"Using thread {Lock.thread_id}", file=sys.stderr)
-        if args.xss_check:
-            return_code = check_xss(
-                args.payload, thread_id=Lock.thread_id, verbose=args.verbose,
-                manual_preparation=args.manual_preparation)
-        else:
-            return_code = run_testsuite(
-                args.testpatterns, thread_id=Lock.thread_id, verbose=args.verbose,
-                manual_preparation=args.manual_preparation)
+        return_code = run_regular_tests(
+            configpath=Lock.configpath, testpatterns=args.testpatterns,
+            verbose=args.verbose)
 
     sys.exit(return_code)
