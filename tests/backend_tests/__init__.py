5a80675cb739e8439568b6e587bb3a6e2ba614f9
Lars Esser <mail@lars-esser.de>
2021-12-13 12:52:18 +0100

    tests/backend_tests&frontend_tests: import all TestCases in modules __init__

---
+++ tests/backend_tests/__init__.py
@@ -0,0 +1,7 @@
+from tests.backend_tests.assembly import TestAssemblyBackend
+from tests.backend_tests.cde import TestCdEBackend
+from tests.backend_tests.common import TestBackendCommon
+from tests.backend_tests.core import TestCoreBackend
+from tests.backend_tests.event import TestEventBackend
+from tests.backend_tests.ml import TestMlBackend
+from tests.backend_tests.past_event import TestPastEventBackend
---
+++ tests/frontend_tests/__init__.py
@@ -0,0 +1,10 @@
+from tests.frontend_tests.application import TestApplication
+from tests.frontend_tests.assembly import TestAssemblyFrontend, TestMultiAssemblyFrontend
+from tests.frontend_tests.cde import TestCdEFrontend
+from tests.frontend_tests.common import TestFrontendCommon
+from tests.frontend_tests.core import TestCoreFrontend
+from tests.frontend_tests.cron import TestCron
+from tests.frontend_tests.event import TestEventFrontend
+from tests.frontend_tests.ml import TestMlFrontend
+from tests.frontend_tests.parse import TestParseFrontend
+from tests.frontend_tests.privacy import TestPrivacyFrontend
