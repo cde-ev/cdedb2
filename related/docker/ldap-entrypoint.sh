f7bfa43887f3dedb573b0ec369a51ba7f62e383c
Lars Esser <mail@lars-esser.de>
2021-12-18 12:00:54 +0100

    ldap/docker: adopt setup to new templating mechanism
    This has no longer any side-dependencies on python, but directly
    uses sed to replace the placeholders in the templates.
    
    The information can be passed-in via environment variables.

--- related/docker/ldap-entrypoint.sh
+++ related/docker/ldap-entrypoint.sh
@@ -15,19 +15,6 @@ if [ ! -e /var/lib/ldap/container_already_initalized ]; then
     # remove pre-installed mdb. This uses the same olcSuffix and blocks our sql database
     rm /etc/ldap/slapd.d/cn=config/olcDatabase=\{1\}mdb.ldif
 
-    # TODO replace the following hacks with a proper config during rendering the templates.
-    # Replace localhost with cdb as that is where the db is accesible by default.
-    sed -i "s/localhost/${DATABASE_HOST:-cdb}/" /etc/odbc.ini /app/config-ldap.ldif
-    sed -i "s/localhost/${DATABASE_HOST:-cdb}/" /etc/odbc.ini /app/cdedb-ldap.ldif
-
-    # This is required for testing where the database name differs.
-    if [ ! -z "${DATABASE_NAME}" ]; then
-        sed -i -r "s/\[cdb\]/[${DATABASE_NAME}]/" /etc/odbc.ini
-        sed -i -r "s/(Database\s*=\s*)cdb/\1${DATABASE_NAME}/" /etc/odbc.ini
-        sed -i -r "s/(olcDbName:\s*)cdb/\1${DATABASE_NAME}/" /app/config-ldap.ldif
-        sed -i -r "s/(olcDbName:\s*)cdb/\1${DATABASE_NAME}/" /app/cdedb-ldap.ldif
-    fi
-
     # Start slapd in the background (in foreground mode with -d 0 to prevent forking)
     # with ldapi:// (unix socket) to allow for simple authentication with ldapmodify.
     slapd -d 0 -h ldapi:// &
