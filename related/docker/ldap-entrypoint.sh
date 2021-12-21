b459fb2dd9e6accb589798d1ebd98bfdd5d37bd9
Lars Esser <mail@lars-esser.de>
2021-12-21 12:51:56 +0100

    docker/ldap: move preparation of files from docker-file to entry-point
    There were some problems accessing the environment variables from
    docker-compose in the docker file. However, in the entrypoints shell
    script everything works.

--- related/docker/ldap-entrypoint.sh
+++ related/docker/ldap-entrypoint.sh
@@ -9,6 +9,19 @@ if [ ! -e /var/lib/ldap/container_already_initalized ]; then
     # consequently we give the app container time to create the sample-data
     sleep 20
 
+    # prepare odbc.ini file to enable database connection for ldap
+    sed -i -r "s/DATABASE_NAME/${DATABASE_NAME}/" /app/odbc.ini
+    sed -i -r "s/DATABASE_HOST/${DATABASE_HOST}/" /app/odbc.ini
+    sed -i -r "s/DATABASE_USER_PASSWORD/${DATABASE_USER_PASSWORD}/" /app/odbc.ini
+
+    # copy the odbc.ini file at the right place
+    cp -f /app/odbc.ini /etc/odbc.ini
+
+    # Prepare the cdedb-ldap.ldif file
+    sed -i -r "s/OLC_DB_HOST/${DATABASE_HOST}/" /app/cdedb-ldap.ldif
+    sed -i -r "s/OLC_DB_NAME/${DATABASE_NAME}/" /app/cdedb-ldap.ldif
+    sed -i -r "s/DATABASE_USER_PASSWORD/${DATABASE_USER_PASSWORD}/" /app/cdedb-ldap.ldif
+
     # the duas must be manually added by invoking bin/ldap_add_duas.py.
     # in dev and test instances, they are included in the sample-data.
 
@@ -22,6 +35,7 @@ if [ ! -e /var/lib/ldap/container_already_initalized ]; then
     sleep 5
     # Run ldapmodify to initialize the ldap-sql backend and config.
     ldapmodify -Y EXTERNAL -H ldapi:// -f /app/config-ldap.ldif
+    # Now apply our custom ldap configuration
     ldapmodify -Y EXTERNAL -H ldapi:// -f /app/cdedb-ldap.ldif
     # Stop the slapd process we started first.
     kill -INT "$(cat /run/slapd/slapd.pid)"
