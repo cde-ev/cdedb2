f2062b221e8a1059504b5d98f6a2a78625197d1c
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-27 19:51:55 +0100

    database: add and improve unique constraints for part groups.

--- cdedb/database/cdedb-tables.sql
+++ cdedb/database/cdedb-tables.sql
@@ -723,22 +723,22 @@ CREATE TABLE event.part_groups (
         title                   varchar NOT NULL,
         shortname               varchar NOT NULL,
         notes                   varchar,
-        constraint_type         integer NOT NULL
+        constraint_type         integer NOT NULL,
+        UNIQUE (event_id, shortname),
+        UNIQUE (event_id, title)
 );
-CREATE INDEX idx_event_part_groups_event_id ON event.part_groups(event_id);
 GRANT INSERT, SELECT, DELETE ON event.part_groups TO cdb_persona;
-GRANT UPDATE(title, shortname, notes) ON event.part_groups TO cdb_persona;
+GRANT UPDATE (title, shortname, notes) ON event.part_groups TO cdb_persona;
 GRANT SELECT, UPDATE ON event.part_groups_id_seq TO cdb_persona;
 GRANT SELECT ON event.part_groups TO cdb_anonymous;
 
 CREATE TABLE event.part_group_parts (
         id                      serial PRIMARY KEY,
         part_group_id           integer REFERENCES event.part_groups(id),
-        part_id                 integer REFERENCES event.event_parts(id)
+        part_id                 integer REFERENCES event.event_parts(id),
+        UNIQUE (part_id, part_group_id)
 );
-CREATE UNIQUE INDEX idx_part_group_parts_constraint ON event.part_group_parts(part_group_id, part_id);
 GRANT INSERT, SELECT, DELETE ON event.part_group_parts TO cdb_persona;
-GRANT SELECT, UPDATE ON event.part_group_parts TO cdb_persona;
 GRANT SELECT, UPDATE ON event.part_group_parts_id_seq TO cdb_persona;
 GRANT SELECT ON event.part_group_parts TO cdb_anonymous;
 
