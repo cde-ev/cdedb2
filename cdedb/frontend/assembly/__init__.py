d55aacffa6a293425aae496a048866d89f9aa09a
Johannes Jung <jungj+gitea@posteo.de>
2022-01-01 15:55:58 +0100

    frontend/create_assembly: fix nasty bug about missing validation error notification
    The "Validation failed." error notification is only added if there are no other
    notifications present at all, no matter what their type is.

--- cdedb/frontend/assembly/__init__.py
+++ cdedb/frontend/assembly/__init__.py
@@ -375,10 +375,14 @@ class AssemblyFrontend(AbstractUserFrontend):
             return self.create_assembly_form(rs)
         assert data is not None
         presider_ml_data = None
+        # we need to wait with info notifications until the last validation error check,
+        # for the "Failed validation" error notification to be shown
+        info_notifications: List[Tuple[str, Dict[str, Any]]] = []
         if create_presider_list:
             if presider_address:
-                rs.notify("info", n_("Given presider address ignored in favor of"
-                                     " newly created mailinglist."))
+                info_notifications.append((n_("Given presider address ignored in favor"
+                                              " of newly created mailinglist."),
+                                           {}))
             presider_ml_data = self._get_mailinglist_setter(data, presider=True)
             presider_address = ml_type.get_full_address(  # type: ignore
                 presider_ml_data)
@@ -386,8 +390,9 @@ class AssemblyFrontend(AbstractUserFrontend):
             data["presider_address"] = presider_address
             if self.mlproxy.verify_existence(rs, presider_address):
                 presider_ml_data = None
-                rs.notify("info", n_("Mailinglist %(address)s already exists."),
-                          {'address': presider_address})
+                info_notifications.append(
+                    (n_("Mailinglist %(address)s already exists."),
+                     {'address': presider_address}))
         else:
             data["presider_address"] = presider_address
         if presider_ids:
@@ -410,6 +415,8 @@ class AssemblyFrontend(AbstractUserFrontend):
             return self.create_assembly_form(rs)
         assert data is not None
         new_id = self.assemblyproxy.create_assembly(rs, data)
+        for msg, params in info_notifications:
+            rs.notify('info', msg, params)
         if presider_ml_data:
             presider_ml_data['assembly_id'] = new_id
             code = self.mlproxy.create_mailinglist(rs, presider_ml_data)
