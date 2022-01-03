94f8e084587fb2ebd16ca24886cae104a6eac0c4
Tobias Udtke <tobias.udtke@gmail.com>
2022-01-03 02:42:22 +0100

    database: expand adjustments of automated changelog entries.

--- cdedb/database/evolutions/2021-11-19-automate_country_codes.sql
+++
@@ -1,3 +0,0 @@
-UPDATE core.changelog SET automated_change = TRUE
-WHERE (change_note = 'Land auf Ländercode umgestellt.'
-       AND ctime >= '2021-03-20 09:42:43' AND ctime < '2021-03-20 09:42:44');
