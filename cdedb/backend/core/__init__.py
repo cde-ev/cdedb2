d4be4d35cc6283d9d3634dce1e454500954e151b
Tobias Udtke <tobias.udtke@gmail.com>
2022-01-05 02:19:30 +0100

    core: fix position of module docstring.

--- cdedb/backend/core/__init__.py
+++ cdedb/backend/core/__init__.py
@@ -1,5 +1,4 @@
-from cdedb.backend.core.base import CoreBaseBackend
-from cdedb.backend.core.genesis import CoreGenesisBackend
+#!/usr/bin/env python3
 
 """The core backend provides services which are common for all users.
 
@@ -7,6 +6,9 @@ The user roles which are most relevant are:
 * `anonymous`: For non-logged-in users (actually logged-in users too).
 * `persona`: For logged-in users regardless of the users realms.
 """
+
+from cdedb.backend.core.base import CoreBaseBackend
+from cdedb.backend.core.genesis import CoreGenesisBackend
 
 
 class CoreBackend(CoreGenesisBackend, CoreBaseBackend):
