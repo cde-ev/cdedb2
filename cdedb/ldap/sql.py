5344be9bdfd161dc35db456c312ba66ee3f65a11
Lars Esser <mail@lars-esser.de>
2022-01-21 21:24:58 +0100

    ldaptor: determine the children from a DN

--- cdedb/ldap/sql.py
+++ cdedb/ldap/sql.py
@@ -1,11 +1,70 @@
+from typing import Optional, Union
+
 import ldaptor.entry
 import ldaptor.entryhelpers
 import ldaptor.interfaces
 import zope.interface
 from ldaptor import attributeset, entry
 from ldaptor.protocols.ldap import distinguishedname, ldaperrors
+from ldaptor.protocols.ldap.distinguishedname import DistinguishedName
 from twisted.internet import defer, error
 from twisted.python import failure
+
+
+def get_duas():
+    query = "SELECT cn FROM ldap.duas"
+    dua_rdns = ...
+    return dua_rdns
+
+
+def get_assembly_presider_groups():
+    pass
+
+
+def get_event_orga_groups():
+    pass
+
+
+def get_ml_moderator_groups():
+    pass
+
+
+def get_ml_subscriber_groups():
+    pass
+
+
+def get_users():
+    pass
+
+
+LDAP_TREE = {
+    "dc=de": {
+        "dc=cde-ev": {
+            "ou=dua": get_duas,
+            "ou=groups": {
+                "ou=assembly-presiders": get_assembly_presider_groups,
+                "ou=event-orgas": get_event_orga_groups,
+                "ou=ml-moderators": get_ml_moderator_groups,
+                "ou=ml-subscribers": get_ml_subscriber_groups,
+                "ou=status": [
+                    "cn=is_active",
+                    "cn=is_assembly_admin",
+                    "cn=is_assembly_realm",
+                    "cn=is_cdelokal_admin",
+                    "cn=is_core_admin",
+                    "cn=is_event_admin",
+                    "cn=is_event_realm",
+                    "cn=is_finance_admin",
+                    "cn=is_member",
+                    "cn=is_ml_admin",
+                    "cn=is_ml_realm",
+                    "cn=is_searchable"
+                ],
+            },
+            "ou=users": get_users,
+        }
+    }
+}
 
 
 # mimik the implementation of LDAPTreeEntry
@@ -22,7 +81,7 @@ class LDAPsqlEntry(
     # TODO maybe we need to implement the IEditableLDAPEntry interface too, since the
     #  default ldapserver does not take this into account properly. However, we can
     #  probably simply rise an exception if those methods are used.
-    def __init__(self, dn, *a, **kw):
+    def __init__(self, dn: Optional[Union[DistinguishedName,str]], *a, **kw):
         # root entry
         if dn is None:
             dn = ""
@@ -44,8 +103,29 @@ class LDAPsqlEntry(
             return self.__class__(self.dn.up())
 
     def _children(self, callback=None):
-        # TODO select direct childrens from database and instantiate them
-        children = list()
+        dn_str = self.dn.getText()
+        subtree = LDAP_TREE
+        # loop backwards through the DN
+        for rdn in dn_str.split(",")[::-1]:
+            # we reached a leaf in the ldap tree
+            if not isinstance(subtree, dict):
+                return None
+            # this can only happen if the dn is not in the ldap tree
+            if rdn not in subtree:
+                return None
+            subtree = subtree[rdn]
+
+        # get the children from the subtree
+        if isinstance(subtree, dict):
+            children_rdns = list(subtree.keys())
+        elif isinstance(subtree, list):
+            children_rdns = list(*subtree)
+        elif callable(subtree):
+            children_rdns = subtree()
+        else:
+            raise RuntimeError("Impossible")
+
+        children = [self.__class__(f"{rdn},{dn_str}") for rdn in children_rdns]
         if callback is None:
             return children
         else:
