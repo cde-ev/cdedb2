576f362391f80d81113d4fac792ac35b4648ca3c
Lars Esser <mail@lars-esser.de>
2022-01-22 13:50:39 +0100

    ldap: implement children and load methods of LDAPsqlEntry

--- cdedb/ldap/sql.py
+++ cdedb/ldap/sql.py
@@ -10,6 +10,13 @@ from ldaptor.protocols.ldap.distinguishedname import DistinguishedName
 from twisted.internet import defer, error
 from twisted.python import failure
 
+from cdedb.common import unwrap
+from cdedb.ldap.tree import LDAP_TREE, Attributes, Children, LDAPTreeLeaf
+
+
+class LDAPTreeNoSuchEntry(Exception):
+    """The ldap tree does not contain such an object."""
+
 
 # mimik the implementation of LDAPTreeEntry
 @zope.interface.implementer(ldaptor.interfaces.IConnectedLDAPEntry)
@@ -30,12 +37,53 @@ class LDAPsqlEntry(
         if dn is None:
             dn = ""
         entry.BaseLDAPEntry.__init__(self, dn, *a, **kw)
+        # TODO where do we catch the error if the dn is invalid?
         if self.dn != "":
+            # this checks also if the given dn is valid for the ldap tree
             self._load()
 
+    @staticmethod
+    def _get_entry(dn: DistinguishedName) -> Tuple[Optional[Children], Optional[Attributes]]:
+        """Get all infos about an entry from the ldap tree.
+
+        This is done by traversing the ldap tree from the root to the entry described
+        by the given dn.
+
+        If no such entry is found, an `LDAPTreeNoSuchEntry` exception is raised.
+        """
+        # TODO maybe make this more performant by querying only those attributes which
+        #  we are asked for?
+        subtree = LDAP_TREE
+        attributes = None
+        # iterate backwards over all rdns of the dn
+        for iteration, rdns in enumerate(reversed(dn.split()), start=1):
+            # all rdn's in our ldap tree consist of only one attribute-value pair
+            if rdns.count() != 1:
+                raise LDAPTreeNoSuchEntry
+            rdn = unwrap(rdns.split())
+            # we reached a leaf of the ldap tree
+            if isinstance(subtree, LDAPTreeLeaf):
+                # the dn contains to many elements and is therefore not valid
+                if iteration != len(dn.split()):
+                    raise LDAPTreeNoSuchEntry
+                return None, subtree.attributes(rdn)
+            # this happens only if the dn is not valid for our tree
+            if rdn.getText() not in subtree:
+                raise LDAPTreeNoSuchEntry
+            # walk down one level in the ldap tree
+            attributes, subtree = subtree[rdn.getText()]
+        if isinstance(subtree, dict):
+            children = subtree.keys()
+        elif isinstance(subtree, LDAPTreeLeaf):
+            children = subtree.entities()
+        else:
+            raise RuntimeError("Impossible")
+        return children, attributes
+
     def _load(self):
-        # TODO load entry's attributes from the database from only knowing its dn
-        attributes = ...
+        _, attributes = self._get_entry(self.dn)
+        if attributes is None:
+            return None
         for k, v in attributes.items():
             self._attributes[k] = attributeset.LDAPAttributeSet(k, v)
 
@@ -47,29 +95,12 @@ class LDAPsqlEntry(
             return self.__class__(self.dn.up())
 
     def _children(self, callback=None):
-        dn_str = self.dn.getText()
-        subtree = LDAP_TREE
-        # loop backwards through the DN
-        for rdn in dn_str.split(",")[::-1]:
-            # we reached a leaf in the ldap tree
-            if not isinstance(subtree, dict):
-                return None
-            # this can only happen if the dn is not in the ldap tree
-            if rdn not in subtree:
-                return None
-            subtree = subtree[rdn]
+        _, children_rdns = self._get_entry(self.dn)
+        if children_rdns is None:
+            return None
 
-        # get the children from the subtree
-        if isinstance(subtree, dict):
-            children_rdns = list(subtree.keys())
-        elif isinstance(subtree, list):
-            children_rdns = list(*subtree)
-        elif callable(subtree):
-            children_rdns = subtree()
-        else:
-            raise RuntimeError("Impossible")
-
-        children = [self.__class__(f"{rdn},{dn_str}") for rdn in children_rdns]
+        # TODO do we really need them to be instantiated, since this can be expensive?
+        children = [self.__class__(f"{rdn},{self.dn.getText()}") for rdn in children_rdns]
         if callback is None:
             return children
         else:
