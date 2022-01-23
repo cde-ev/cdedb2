bc06d84dac9df1c7ba2e12911b372d5483c4cda9
Lars Esser <mail@lars-esser.de>
2022-01-23 14:03:01 +0100

    ldaptor: refactor construction and usage of ldap-tree
    This splits the handling of ldap branch entries and ldap leaf entries
    more clearly. At the same time, this introduces some redundancy
    (we require the keys of the hardcoded dicts to be consistent)
    to avoid the need to use iterative dicts.
    
    This has the great advantage that the attributes of mulitple entries
    can now be retrieved at the same time, which redruces the number of
    database calls to instantiate the children of f.e. the user entry
    from n to 1.

--- cdedb/ldap/sql.py
+++ cdedb/ldap/sql.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import ldaptor.entry
 import ldaptor.entryhelpers
@@ -11,7 +11,7 @@ from twisted.internet import defer, error
 from twisted.python import failure
 
 from cdedb.common import unwrap
-from cdedb.ldap.tree import LDAP_TREE, Attributes, Children, LDAPTreeLeaf
+from cdedb.ldap.tree import LDAP_BRANCHES, LDAP_LEAFS, Attributes
 
 
 class LDAPTreeNoSuchEntry(Exception):
@@ -32,58 +32,49 @@ class LDAPsqlEntry(
     # TODO maybe we need to implement the IEditableLDAPEntry interface too, since the
     #  default ldapserver does not take this into account properly. However, we can
     #  probably simply rise an exception if those methods are used.
-    def __init__(self, dn: Optional[Union[DistinguishedName,str]], *a, **kw):
-        # root entry
-        if dn is None:
-            dn = ""
+    def __init__(self, dn: Union[DistinguishedName, str], attributes: Attributes = None, *a, **kw):
         entry.BaseLDAPEntry.__init__(self, dn, *a, **kw)
+        # root entry
+        if self.dn == "":
+            return
         # TODO where do we catch the error if the dn is invalid?
-        if self.dn != "":
-            # this checks also if the given dn is valid for the ldap tree
-            self._load()
+        self._load(attributes=attributes)
 
     @staticmethod
-    def _get_entry(dn: DistinguishedName) -> Tuple[Optional[Children], Optional[Attributes]]:
-        """Get all infos about an entry from the ldap tree.
+    def _get_entities(dns: List[DistinguishedName]) -> Dict[DistinguishedName, Optional[Attributes]]:
+        """Get all attributes of the given entities."""
+        ret = dict()
+        # get all attributes of non-leaf ldap entries
+        for dn in dns:
+            if dn.getText() in LDAP_BRANCHES:
+                ret[dn] = LDAP_BRANCHES[dn.getText()]
+        # get all attributes of leaf ldap entries
+        parents = set(dn.up() for dn in dns)
+        for parent in parents:
+            if parent.getText() in LDAP_LEAFS:
+                siblings = [dn for dn in dns if dn.split()[1:] == parent.split()]
+                getter = LDAP_LEAFS[parent.getText()]["get_entities"]
+                ret.update(getter(siblings))
+        # TODO what should we do if some entries are not found?
+        if set(dns) > set(ret):
+            missing_dns = set(dns) - set(ret)
+            for dn in missing_dns:
+                ret[dn] = None
+        return ret
 
-        This is done by traversing the ldap tree from the root to the entry described
-        by the given dn.
+    def _get_entity(self, dn: DistinguishedName) -> Optional[Attributes]:
+        return unwrap(self._get_entities([dn]))
 
-        If no such entry is found, an `LDAPTreeNoSuchEntry` exception is raised.
+    def _load(self, attributes: Attributes = None):
+        """Load own attributes.
+
+        This accepts a set of Attributes to be used instead of fetching them from the
+        database, since this reduces the number of queries when instantiating children
+        entries significantly.
         """
-        # TODO maybe make this more performant by querying only those attributes which
-        #  we are asked for?
-        subtree = LDAP_TREE
-        attributes = None
-        # iterate backwards over all rdns of the dn
-        for iteration, rdns in enumerate(reversed(dn.split()), start=1):
-            # all rdn's in our ldap tree consist of only one attribute-value pair
-            if rdns.count() != 1:
-                raise LDAPTreeNoSuchEntry
-            rdn = unwrap(rdns.split())
-            # we reached a leaf of the ldap tree
-            if isinstance(subtree, LDAPTreeLeaf):
-                # the dn contains to many elements and is therefore not valid
-                if iteration != len(dn.split()):
-                    raise LDAPTreeNoSuchEntry
-                return None, subtree.attributes(rdn)
-            # this happens only if the dn is not valid for our tree
-            if rdn.getText() not in subtree:
-                raise LDAPTreeNoSuchEntry
-            # walk down one level in the ldap tree
-            attributes, subtree = subtree[rdn.getText()]
-        if isinstance(subtree, dict):
-            children = subtree.keys()
-        elif isinstance(subtree, LDAPTreeLeaf):
-            children = subtree.entities()
-        else:
-            raise RuntimeError("Impossible")
-        return children, attributes
-
-    def _load(self):
-        _, attributes = self._get_entry(self.dn)
+        attributes = attributes or self._get_entity(self.dn)
         if attributes is None:
-            return None
+            raise LDAPTreeNoSuchEntry
         for k, v in attributes.items():
             self._attributes[k] = attributeset.LDAPAttributeSet(k, v)
 
@@ -94,13 +85,34 @@ class LDAPsqlEntry(
         else:
             return self.__class__(self.dn.up())
 
-    def _children(self, callback=None):
-        _, children_rdns = self._get_entry(self.dn)
-        if children_rdns is None:
-            return None
+    @staticmethod
+    def _get_children(parent_dn: DistinguishedName) -> Optional[List[DistinguishedName]]:
+        """Return the children of the given entry."""
+        ret = list()
+        # get all branch children
+        for dn_str in LDAP_BRANCHES:
+            dn = DistinguishedName(dn_str)
+            # here, we compare if the given dn is the parent of the branch dn
+            if dn.up() == parent_dn:
+                ret.append(dn)
+        # get all leaf children
+        for dn_str in LDAP_LEAFS:
+            dn = DistinguishedName(dn_str)
+            # attention, since LDAP_LEAFS maps already the _parent_ dn to its children
+            if dn == parent_dn:
+                children_rdns = LDAP_LEAFS[dn_str]["list_entities"]()
+                children_dns = [DistinguishedName(listOfRDNs=[rdn, *dn.listOfRDNs])
+                                for rdn in children_rdns]
+                ret.extend(children_dns)
+        return ret or None
 
-        # TODO do we really need them to be instantiated, since this can be expensive?
-        children = [self.__class__(f"{rdn},{self.dn.getText()}") for rdn in children_rdns]
+    def _children(self, callback=None):
+        dns = self._get_children(self.dn)
+        if dns is None:
+            return None
+        attributes = self._get_entities(dns)
+
+        children = [self.__class__(dn, attributes=attributes[dn]) for dn in dns]
         if callback is None:
             return children
         else:
--- cdedb/ldap/tree.py
+++ cdedb/ldap/tree.py
@@ -1,149 +1,104 @@
-from typing import Callable, Dict, List, NewType, Tuple, Union
+from typing import Callable, Dict, List, TypedDict
 
-DN = NewType("DN", str)
-RDN = NewType("RDN", str)
-
+from ldaptor.protocols.ldap.distinguishedname import (
+    DistinguishedName as DN, RelativeDistinguishedName as RDN,
+)
 
 ##############
 # Attributes #
 ##############
 
 Attributes = Dict[str, List[str]]
-AttributesCallback = Callable[[RDN], Attributes]
-
-#
-# domain components
-#
-
-DC_DE: Attributes = {
-    "objectClass": ["dcObject", "top"],
-}
-
-DC_CDE: Attributes = {
-    "objectClass": ["dcObject", "organization"],
-    "o": ["CdE e.V."],
-}
-
-#
-# organizational units
-#
-
-OU_DUA: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Directory User Agents"]
-}
-
-OU_GROUPS: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Groups"]
-}
-
-OU_USERS: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Users"]
-}
-
-OU_STATUS: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Status"]
-}
-
-OU_ASSEMBLY_PRESIDERS: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Assembly Presiders"]
-}
-
-OU_EVENT_ORGAS: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Event Orgas"]
-}
-
-OU_ML_MODERATORS: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Mailinglists Moderators"]
-}
-
-OU_ML_SUBSCRIBERS: Attributes = {
-    "objectClass": ["organizationalUnit"],
-    "o": ["Mailinglists Subscribers"]
-}
-
-#
-# groups
-#
-
-
-def get_status_group_attributes(rdn: RDN) -> Attributes:
-    pass
-
-
-def get_assembly_presider_group_attributes(rdn: RDN) -> Attributes:
-    pass
-
-
-def get_event_orga_group_attributes(rdn: RDN) -> Attributes:
-    pass
-
-
-def get_ml_moderator_group_attributes(rdn: RDN) -> Attributes:
-    pass
-
-
-def get_ml_subscriber_group_attributes(rdn: RDN) -> Attributes:
-    pass
 
 #
 # duas
 #
 
 
-def get_dua_attributes(rdn: RDN) -> Attributes:
+def get_duas(dns: List[DN]) -> Dict[DN, Attributes]:
     pass
+
 
 #
 # users
 #
 
 
-def get_user_attributes(rdn: RDN) -> Attributes:
+def get_users(dns: List[DN]) -> Dict[DN, Attributes]:
     pass
 
 
-############
-# Children #
-############
-
-Children = List[RDN]
-ChildrenCallback = Callable[[], Children]
+#
+# groups
+#
 
 
-def get_duas() -> List[RDN]:
+def get_status_groups(dns: List[DN]) -> Dict[DN, Attributes]:
+    pass
+
+
+def get_assembly_presider_groups(dns: List[DN]) -> Dict[DN, Attributes]:
+    pass
+
+
+def get_event_orga_groups(dns: List[DN]) -> Dict[DN, Attributes]:
+    pass
+
+
+def get_ml_moderator_groups(dns: List[DN]) -> Dict[DN, Attributes]:
+    pass
+
+
+def get_ml_subscriber_groups(dns: List[DN]) -> Dict[DN, Attributes]:
+    pass
+
+
+#################
+# List Entities #
+#################
+
+#
+# duas
+#
+
+
+def list_duas() -> List[RDN]:
     query = "SELECT cn FROM ldap.duas"
     dua_rdns = ...
     return dua_rdns
 
 
-def get_assembly_presider_groups() -> List[RDN]:
+#
+# users
+#
+
+
+def list_users() -> List[RDN]:
     pass
 
 
-def get_event_orga_groups() -> List[RDN]:
+#
+# groups
+#
+
+
+def list_assembly_presider_groups() -> List[RDN]:
     pass
 
 
-def get_ml_moderator_groups() -> List[RDN]:
+def list_event_orga_groups() -> List[RDN]:
     pass
 
 
-def get_ml_subscriber_groups() -> List[RDN]:
+def list_ml_moderator_groups() -> List[RDN]:
     pass
 
 
-def get_status_groups() -> List[RDN]:
+def list_ml_subscriber_groups() -> List[RDN]:
     pass
 
 
-def get_users() -> List[RDN]:
+def list_status_groups() -> List[RDN]:
     pass
 
 
@@ -151,55 +106,84 @@ def get_users() -> List[RDN]:
 # LDAP Tree #
 #############
 
-class LDAPTreeLeaf:
-    """A class to store the functions to handle leafs of the LDAP Tree.
+# contains all non-leaf ldap entries, mapping their DN to their attributes
+LDAP_BRANCHES: Dict[str, Attributes] = {
+    "dc=de": {
+        "objectClass": ["dcObject", "top"],
+    },
+    "dc=cde-ev,dc=de": {
+        "objectClass": ["dcObject", "organization"],
+        "o": ["CdE e.V."],
+    },
+    "ou=duas,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Directory User Agents"]
+    },
+    "ou=users,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Users"]
+    },
+    "ou=groups,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Groups"]
+    },
+    "ou=assembly-presiders,ou=groups,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Assembly Presiders"]
+    },
+    "ou=event-orgas,ou=groups,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Event Orgas"]
+    },
+    "ou=ml-moderators,ou=groups,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Mailinglists Moderators"]
+    },
+    "ou=ml-subscribers,ou=groups,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Mailinglists Subscribers"]
+    },
+    "ou=status,ou=groups,dc=cde-ev,dc=de": {
+        "objectClass": ["organizationalUnit"],
+        "o": ["Status"]
+    }
+}
 
-    Since there are n leafs of the same kind, this is a bit complicated.
-    """
-    def __init__(self, entities: ChildrenCallback, attributes: AttributesCallback):
-        """Create a new LDAPTreeLeaf.
 
-        :param entities: A callback returning _all_ entities of this kind of leaf.
-        :param attributes: A callback returning all attributes of _one_ given entity.
-        """
-        self.attributes = attributes
-        self.entities = entities
+class LdapLeaf(TypedDict):
+    get_entities: Callable[[List[DN]], Dict[DN, Attributes]]
+    list_entities: Callable[[], List[RDN]]
 
 
-DUAS = LDAPTreeLeaf(get_duas, get_dua_attributes)
-ASSEMBLY_PRESIDER_GROUPS = LDAPTreeLeaf(
-    get_assembly_presider_groups, get_assembly_presider_group_attributes)
-EVENT_ORGA_GROUPS = LDAPTreeLeaf(get_event_orga_groups, get_event_orga_group_attributes)
-ML_MODERATOR_GROUPS = LDAPTreeLeaf(
-    get_ml_moderator_groups, get_ml_moderator_group_attributes)
-ML_SUBSCRIBER_GROUPS = LDAPTreeLeaf(
-    get_ml_subscriber_groups, get_ml_subscriber_group_attributes)
-STATUS_GROUPS = LDAPTreeLeaf(get_status_groups, get_status_group_attributes)
-USERS = LDAPTreeLeaf(get_users, get_user_attributes)
-
-
-# The actual ldap tree, holding all structural information. This includes:
-# - What is the parent and are the children of an entry (from root to leaf)?
-# - What are the attributes of an entry (from root to leaf)?
-#
-# The Tree can be read per level:
-# Each level is a Dict, mapping the entry's RDN to a Tuple, containing
-# - the Attributes of the entry
-# - the next tree level (which is either another Tree, or a LDAPTreeLeaf)
-Tree = Dict[str, Tuple[Attributes, Union["Tree", LDAPTreeLeaf]]]
-
-LDAP_TREE: Tree = {
-    "dc=de": (DC_DE, {
-        "dc=cde-ev": (DC_CDE, {
-            "ou=dua": (OU_DUA, DUAS),
-            "ou=groups": (OU_GROUPS, {
-                "ou=assembly-presiders": (OU_ASSEMBLY_PRESIDERS, ASSEMBLY_PRESIDER_GROUPS),
-                "ou=event-orgas": (OU_EVENT_ORGAS, EVENT_ORGA_GROUPS),
-                "ou=ml-moderators": (OU_ML_MODERATORS, ML_MODERATOR_GROUPS),
-                "ou=ml-subscribers": (OU_ML_SUBSCRIBERS, ML_SUBSCRIBER_GROUPS),
-                "ou=status": (OU_STATUS, STATUS_GROUPS),
-            }),
-            "ou=users": (OU_USERS, USERS),
-        })
-    })
+# contains information about all ldap leaf entries. Maps their _parent_ DN to functions
+# for getting the attributes of a given list of entities and listing all entities.
+LDAP_LEAFS: Dict[str, LdapLeaf] = {
+    "ou=duas,dc=cde-ev,dc=de": {
+        "get_entities": get_duas,
+        "list_entities": list_duas,
+    },
+    "ou=users,dc=cde-ev,dc=de": {
+        "get_entities": get_users,
+        "list_entities": list_users,
+    },
+    "ou=assembly-presiders,ou=groups,dc=cde-ev,dc=de": {
+        "get_entities": get_assembly_presider_groups,
+        "list_entities": list_assembly_presider_groups,
+    },
+    "ou=event-orgas,ou=groups,dc=cde-ev,dc=de": {
+        "get_entities": get_event_orga_groups,
+        "list_entities": list_event_orga_groups,
+    },
+    "ou=ml-moderators,ou=groups,dc=cde-ev,dc=de": {
+        "get_entities": get_ml_moderator_groups,
+        "list_entities": list_ml_moderator_groups,
+    },
+    "ou=ml-subscribers,ou=groups,dc=cde-ev,dc=de": {
+        "get_entities": get_ml_subscriber_groups,
+        "list_entities": list_ml_subscriber_groups,
+    },
+    "ou=status,ou=groups,dc=cde-ev,dc=de": {
+        "get_entities": get_status_groups,
+        "list_entities": list_status_groups,
+    }
 }
