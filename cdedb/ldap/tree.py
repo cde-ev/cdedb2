d4395f2e08d3c6ef8f39a6805ae9782b4afe4514
Lars Esser <mail@lars-esser.de>
2022-02-01 01:54:35 +0100

    ldaptor/tree: implement all get_ and list_ methods beside those for users

--- cdedb/ldap/tree.py
+++ cdedb/ldap/tree.py
@@ -1,5 +1,7 @@
 import logging
-from typing import Callable, Dict, List, TypedDict
+import re
+from collections import defaultdict
+from typing import Callable, Dict, List, Optional, TypedDict
 
 from ldaptor.protocols.ldap.distinguishedname import (
     DistinguishedName as DN, LDAPAttributeTypeAndValue as ATV,
@@ -9,6 +11,7 @@ from ldaptor.protocols.ldap.distinguishedname import (
 from cdedb.common import unwrap
 from cdedb.config import Config, SecretsConfig
 from cdedb.database.connection import ConnectionContainer, connection_pool_factory
+from cdedb.database.constants import SubscriptionState
 from cdedb.database.query import QueryMixin
 
 LDAPObject = Dict[str, List[str]]
@@ -34,89 +37,267 @@ class LDAPsqlTree(QueryMixin):
         super().__init__(self.logger)
 
     @staticmethod
-    def _db_key(dn: DN):
+    def _dn_value(dn: DN, attribute: str) -> Optional[str]:
         rdn = dn.split()[0]
         attribute_value = unwrap(rdn.split())
-        return attribute_value.value
-
-    def _get_entities(self, query: str, dns: List[DN]) -> LDAPObjectMap:
-        """Retrieve all dns with the specified query and format them accordingly.
-
-        Each query result is converted in an attributes' dict, where the name of the
-        attribute is the column name of the query.
-        The attribute selecting the entities in the WHERE clause must be the same
-        attribute used in the DN of the entry.
-        """
-        # TODO check that this is equal for all dns
-        attribute_key = dns[0].split()[0].split()[0].attributeType
-        dn_to_key = {dn: self._db_key(dn) for dn in dns}
-        data = self.query_all(self.rs, query, (dn_to_key.values(),))
-        # the attributes of each entry have to be a list of strings
-        entries = {
-            e[attribute_key]: {
-                key: [str(value)] for key, value in e.items()
-            } for e in data
-        }
-        return {dn: entries.get(dn_to_key.get(dn)) for dn in dns}
+        if attribute_value.attributeType == attribute:
+            return attribute_value.value
+        else:
+            return None
 
     def get_duas(self, dns: List[DN]) -> LDAPObjectMap:
+        dn_to_cn = dict()
+        for dn in dns:
+            cn = self._dn_value(dn, attribute="cn")
+            if cn is None:
+                continue
+            dn_to_cn[dn] = cn
+
         query = "SELECT cn, password_hash FROM ldap.duas WHERE cn = ANY(%s)"
-        entities = self._get_entities(query, dns)
-        # add the object class to the entities
-        entities = {dn: dict(**entity, objectclass=["person"])
-                    for dn, entity in entities.items()}
-        return entities
+        data = self.query_all(self.rs, query, (dn_to_cn.values(),))
+        duas = {e["cn"]: e for e in data}
+
+        ret = dict()
+        for dn, cn in dn_to_cn.items():
+            if cn not in duas:
+                continue
+            group = {
+                "objectclass": ["person"],
+                "cn": [cn],
+                "userPassword": [duas[cn]["password_hash"]]
+            }
+            ret[dn] = group
+        return ret
 
     def get_users(self, dns: List[DN]) -> LDAPObjectMap:
         pass
 
+    STATUS_GROUPS = {
+        "is_active", "is_member", "is_searchable", "is_ml_realm", "is_event_realm",
+        "is_assembly_realm", "is_cde_realm", "is_ml_admin", "is_event_admin",
+        "is_assembly_admin", "is_cde_admin", "is_core_admin", "is_finance_admin",
+        "is_cdelokal_admin"
+    }
+
     def get_status_groups(self, dns: List[DN]) -> LDAPObjectMap:
-        pass
+        dn_to_cn = dict()
+        for dn in dns:
+            cn = self._dn_value(dn, attribute="cn")
+            if cn is None:
+                continue
+            dn_to_cn[dn] = cn
+
+        # since we have only a small group of status groups, we query them one by one
+        ret = dict()
+        for dn, cn in dn_to_cn.items():
+            if cn not in self.STATUS_GROUPS:
+                continue
+            group = {"cn": [cn], "objectclass": ["groupOfUniqueNames"]}
+            if cn == "is_searchable":
+                condition = "is_member AND is_searchable"
+            else:
+                condition = cn
+            query = f"SELECT id FROM core.personas WHERE {condition}"
+            members = self.query_all(self.rs, query, [])
+            # TODO can we avoid to hardcode the path to the users?
+            group["uniqueMember"] = [f"uid={e['id']},ou=users,dc=cde-ev,dc=de" for e in members]
+            ret[dn] = group
+        return ret
+
+    @staticmethod
+    def extract_id(cn: str, prefix: str) -> Optional[int]:
+        """Extract the id from a cn by stripping the prefix."""
+        if match := re.match(f"{prefix}-(?P<id>\d)", cn):
+            return int(match.group("id"))
+        else:
+            return None
 
     def get_assembly_presider_groups(self, dns: List[DN]) -> LDAPObjectMap:
-        pass
+        dn_to_assembly_id = dict()
+        for dn in dns:
+            cn = self._dn_value(dn, attribute="cn")
+            if cn is None:
+                continue
+            assembly_id = self.extract_id(cn, prefix="presiders")
+            if assembly_id is None:
+                continue
+            dn_to_assembly_id[dn] = assembly_id
+
+        query = ("SELECT persona_id, assembly_id FROM assembly.presiders"
+                 " WHERE assembly_id = ANY(%s)")
+        data = self.query_all(self.rs, query, (dn_to_assembly_id.values(),))
+        presiders = defaultdict(list)
+        for e in data:
+            presiders[e["assembly_id"]].append(e["persona_id"])
+
+        ret = dict()
+        for dn, assembly_id in dn_to_assembly_id.items():
+            if assembly_id not in presiders:
+                continue
+            group = {"objectclass": ["groupOfUniqueNames"]}
+            group["cn"] = [f"presiders-{assembly_id}"]
+            group["uniqueMember"] = [f"uid={presider},ou=users,dc=cde-ev,dc=de" for presider in presiders[assembly_id]]
+            ret[dn] = group
+        return ret
 
     def get_event_orga_groups(self, dns: List[DN]) -> LDAPObjectMap:
-        pass
+        dn_to_event_id = dict()
+        for dn in dns:
+            cn = self._dn_value(dn, attribute="cn")
+            if cn is None:
+                continue
+            event_id = self.extract_id(cn, prefix="orgas")
+            if event_id is None:
+                continue
+            dn_to_event_id[dn] = event_id
+
+        query = "SELECT persona_id, event_id FROM event.orgas WHERE event_id = ANY(%s)"
+        data = self.query_all(self.rs, query, (dn_to_event_id.values(),))
+        orgas = defaultdict(list)
+        for e in data:
+            orgas[e["event_id"]].append(e["persona_id"])
+        orgas = {e["event_id"]: e for e in data}
+
+        ret = dict()
+        for dn, event_id in dn_to_event_id.items():
+            if event_id not in orgas:
+                continue
+            group = {"objectclass": ["groupOfUniqueNames"]}
+            group["cn"] = [f"orgas-{event_id}"]
+            group["uniqueMember"] = [f"uid={orga},ou=users,dc=cde-ev,dc=de" for orga in orgas[event_id]]
+            ret[dn] = group
+        return ret
+
+    @staticmethod
+    def extract_address(owner_address: str) -> Optional[str]:
+        expression = r"(?P<local_part>[\w.-]*)-owner@(?P<domain>[\w.-]*)"
+        if match := re.match(expression, owner_address):
+            return f"{match.group('local_part')}@{match.group('domain')}"
+        else:
+            return None
 
     def get_ml_moderator_groups(self, dns: List[DN]) -> LDAPObjectMap:
-        pass
+        dn_to_address = dict()
+        for dn in dns:
+            cn = self._dn_value(dn, attribute="cn")
+            if cn is None:
+                continue
+            address = self.extract_address(cn)
+            if address is None:
+                continue
+            dn_to_address[dn] = address
+
+        query = ("SELECT persona_id, address FROM ml.moderators, ml.mailinglists"
+                 " WHERE ml.mailinglists.id = ml.moderators.mailinglist_id"
+                 " AND address = ANY(%s)")
+        data = self.query_all(self.rs, query, (dn_to_address.values(),))
+        moderators = defaultdict(list)
+        for e in data:
+            moderators[e["address"]].append(e["persona_id"])
+
+        ret = dict()
+        for dn, address in dn_to_address.items():
+            if address not in moderators:
+                continue
+            group = {"objectclass": ["groupOfUniqueNames"]}
+            group["cn"] = [address]
+            group["uniqueMember"] = [f"uid={moderator},ou=users,dc=cde-ev,dc=de" for moderator in moderators[address]]
+            ret[dn] = group
+        return ret
 
     def get_ml_subscriber_groups(self, dns: List[DN]) -> LDAPObjectMap:
-        pass
+        dn_to_address = dict()
+        for dn in dns:
+            cn = self._dn_value(dn, attribute="cn")
+            if cn is None:
+                continue
+            dn_to_address[dn] = cn
 
-    def _list_entities(self, query: str, attribute_key: str) -> List[RDN]:
-        """Construct the RDN of all entities returned by the given query."""
+        query = ("SELECT persona_id, address FROM ml.subscription_states, ml.mailinglists"
+                 " WHERE ml.mailinglists.id = ml.subscription_states.mailinglist_id"
+                 " AND subscription_state = ANY(%s) AND address = ANY(%s)")
+        states = SubscriptionState.subscribing_states()
+        data = self.query_all(self.rs, query, (states, dn_to_address.values(),))
+        subscribers = defaultdict(list)
+        for e in data:
+            subscribers[e["address"]].append(e["persona_id"])
+
+        ret = dict()
+        for dn, address in dn_to_address.items():
+            if address not in subscribers:
+                continue
+            group = {"objectclass": ["groupOfUniqueNames"]}
+            group["cn"] = [address]
+            group["uniqueMember"] = [f"uid={subscriber},ou=users,dc=cde-ev,dc=de" for subscriber in subscribers[address]]
+            ret[dn] = group
+        return ret
+
+    def list_duas(self) -> List[RDN]:
+        query = "SELECT cn FROM ldap.duas"
         data = self.query_all(self.rs, query, [])
         return [
             RDN(
                 attributeTypesAndValues=[
-                    ATV(attributeType=attribute_key, value=e[attribute_key])
+                    ATV(attributeType="cn", value=e["cn"])
                 ]
             ) for e in data
         ]
-
-    def list_duas(self) -> List[RDN]:
-        query = "SELECT cn FROM ldap.duas"
-        return self._list_entities(query, "cn")
 
     def list_users(self) -> List[RDN]:
         pass
 
     def list_assembly_presider_groups(self) -> List[RDN]:
-        pass
+        query = "SELECT assembly_id FROM assembly.presiders"
+        data = self.query_all(self.rs, query, [])
+        return [
+            RDN(
+                attributeTypesAndValues=[
+                    ATV(attributeType="cn", value=f"presider-{e['assembly_id']}")
+                ]
+            ) for e in data
+        ]
 
     def list_event_orga_groups(self) -> List[RDN]:
-        pass
+        query = "SELECT event_id FROM event.orgas"
+        data = self.query_all(self.rs, query, [])
+        return [
+            RDN(
+                attributeTypesAndValues=[
+                    ATV(attributeType="cn", value=f"orgas-{e['event_id']}")
+                ]
+            ) for e in data
+        ]
 
     def list_ml_moderator_groups(self) -> List[RDN]:
-        pass
+        query = "SELECT address FROM ml.mailinglists"
+        data = self.query_all(self.rs, query, [])
+        return [
+            RDN(
+                attributeTypesAndValues=[
+                    ATV(attributeType="cn", value=e["address"])
+                ]
+            ) for e in data
+        ]
 
     def list_ml_subscriber_groups(self) -> List[RDN]:
-        pass
+        query = "SELECT address FROM ml.mailinglists"
+        data = self.query_all(self.rs, query, [])
+        return [
+            RDN(
+                attributeTypesAndValues=[
+                    ATV(attributeType="cn", value=e["address"].replace("@", "-owner@"))
+                ]
+            ) for e in data
+        ]
 
     def list_status_groups(self) -> List[RDN]:
-        pass
+        return [
+            RDN(
+                attributeTypesAndValues=[
+                    ATV(attributeType="cn", value=group)
+                ]
+            ) for group in self.STATUS_GROUPS
+        ]
 
     @property
     def branches(self) -> Dict[str, LDAPObject]:
