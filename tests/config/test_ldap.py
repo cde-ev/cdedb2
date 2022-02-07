ff639977818ba0fbe06e8e2a5bf01ab4cddaeb72
Lars Esser <mail@lars-esser.de>
2022-02-07 13:51:06 +0100

    tests/config: fix copy-paste leftover

--- tests/config/test_ldap.py
+++ tests/config/test_ldap.py
@@ -5,7 +5,7 @@ import pathlib
 from tests.config.base import *  # type: ignore[import]
 
 # temporary directory created during the test run for this test thread
-_TMP_DIR = pathlib.Path("/tmp/cdedb-test-1")
+_TMP_DIR = pathlib.Path("/tmp/cdedb-test-ldap")
 
 LOG_DIR = _TMP_DIR / "logs"  # May not be inside STORAGE_DIR
 STORAGE_DIR = _TMP_DIR / "storage"
