fbe650666cdefaaa5b8b0d8e9573216ffb7eb0c5
Johannes Jung <jungj+gitea@posteo.de>
2022-02-12 23:11:01 +0100

    dev: reload werkzeug debugger on translation change
    closes #2426

--- cdedb/__main__.py
+++ cdedb/__main__.py
@@ -1,6 +1,7 @@
 """interactive werkzeug debugger for the CdEDB2 application"""
 
 import os
+import subprocess
 from pathlib import Path
 
 from werkzeug.serving import run_simple
@@ -13,8 +14,13 @@ static_directory = repopath / "static"
 configpath = Path("/etc/cdedb-application-config.py")
 
 if __name__ == "__main__":
+    if os.environ.get("INTERACTIVE_DEBUGGER"):
+        # otherwise, this would be executed twice on startup
+        subprocess.run(["make", "i18n-compile"], check=True)
     os.environ["INTERACTIVE_DEBUGGER"] = "1"
     application = Application(configpath if configpath.is_file() else None)
+    i18n_files = (repopath / "i18n" / lang / "LC_MESSAGES" / "cdedb.po"
+                  for lang in application.conf["I18N_LANGUAGES"])
     run_simple(
         "0.0.0.0",
         5000,
@@ -22,5 +28,6 @@ if __name__ == "__main__":
         use_debugger=True,
         use_evalex=True,
         use_reloader=True,
+        extra_files=i18n_files,
         static_files={"/static": str(static_directory), "/doc": str(docs_directory)}
     )
