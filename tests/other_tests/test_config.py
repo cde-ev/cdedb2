780469f267b10a3d533df26855393a1ce7b9fe9c
Lars Esser <mail@lars-esser.de>
2022-02-26 19:07:50 +0100

    tests/config: fix test_override

--- tests/other_tests/test_config.py
+++ tests/other_tests/test_config.py
@@ -6,25 +6,24 @@ import unittest
 
 import pytz
 
-from cdedb.setup.config import Config, SecretsConfig
+from cdedb.setup.config import Config, SecretsConfig, get_configpath, set_configpath
 
 
 class TestConfig(unittest.TestCase):
     def test_override(self) -> None:
         # save the actual config path, so we can use this after the test finishes
-        current_configpath = os.environ.get("CDEDB_CONFIGPATH", "")
+        current_configpath = get_configpath()
 
         # check config default values
         config = Config()
         self.assertIn(config["DB_PORT"], {6432, 5432})
-        self.assertEqual(config["CDB_DATABASE_NAME"], os.environ['CDEDB_TEST_DATABASE'])
 
         # check secret config default values
         secret = SecretsConfig()
         self.assertEqual(secret["URL_PARAMETER_SALT"], "aoeuidhtns9KT6AOR2kNjq2zO")
 
         # override default values by providing a config path
-        os.environ["CDEDB_CONFIGPATH"] = "tests/ancillary_files/extra_config.py"
+        set_configpath("tests/ancillary_files/extra_config.py")
 
         # check config override
         extraconfig = Config()
@@ -36,7 +35,7 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(extrasecret["URL_PARAMETER_SALT"], "matrix")
 
         # restore old config path for further tests
-        os.environ["CDEDB_CONFIGPATH"] = current_configpath
+        set_configpath(current_configpath)
 
     def test_caching(self) -> None:
         # this is a regression test
