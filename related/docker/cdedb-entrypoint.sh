5f6a3ba8299fca0409f5f74b5a246542f8ffa7d9
Lars Esser <mail@lars-esser.de>
2022-02-27 18:05:03 +0100

    docker: provide dummy-secrets-config already during build
    This is needed if there are actions performed on the container
    before the first startup, like f.e. done in our CI.

--- related/docker/cdedb-entrypoint.sh
+++ related/docker/cdedb-entrypoint.sh
@@ -17,12 +17,6 @@ fi
 if [ ! -e /etc/cdedb/container_already_initalized ]; then
     # TODO check whether it is sensible to lower privileges to cdedb user in general
 
-    # create a world-readable version of the secrets config with no content.
-    # This needs to be adjusted for productive use.
-    touch "$(python3 -m cdedb.setup secrets-configpath)"
-    chown www-data:www-data "$(python3 -m cdedb.setup secrets-configpath)"
-    chmod 644 "$(python3 -m cdedb.setup secrets-configpath)"
-
     # Create and populate the storage and log dirs
     python3 -m cdedb.setup create-storage --owner www-data
     python3 -m cdedb.setup populate-storage --owner www-data
