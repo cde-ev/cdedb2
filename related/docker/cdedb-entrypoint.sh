84b720d69438ca37f58f39adc92ec9df6d091529
Lars Esser <mail@lars-esser.de>
2022-02-27 14:22:14 +0100

    docker: make default config not configurable, move setup of container first run script
    so, this is directly usable for development or potential offline instances,
    since the default config exists at the correct place and is also used
    in the docker-compose file.
    
    For test instances, the initialization needs to be done manualy.

--- related/docker/cdedb-entrypoint.sh
+++ related/docker/cdedb-entrypoint.sh
@@ -13,12 +13,22 @@ if [ ! -d /etc/ssl/apache2 ]; then
         -subj "/C=DE/O=CdE e.V./CN=cdedb.local/emailAddress=cdedb@lists.cde-ev.de"
 fi
 
-if ! python3 bin/execute_sql_script.py -f /dev/null 2> /dev/null; then
-    # Apparently we cannot connect with the cdb user so we assume no sample-data is present.
+# If this is the first run of the container, perform some initialization
+if [ ! -e /etc/cdedb/container_already_initalized ]; then
+    # Create and populate the storage and log dirs
+    python3 -m cdedb.setup create-storage --owner www-data
+    python3 -m cdedb.setup populate-storage --owner www-data
+    python3 -m cdedb.setup create-log --owner www-data
+
+    # Compile the translations
     make i18n-compile
-    python3 -m cdedb.setup populate-storage
+
+    # create and populate the database
     python3 -m cdedb.setup create-database
     python3 -m cdedb.setup populate-database
+
+    # Touch the firstrun file, so we perform the initialization only once.
+    touch /etc/cdedb/container_already_initalized
 fi
 
 exec "$@"
