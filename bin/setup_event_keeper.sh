4bfc6ad16afd2f752dcefb53dc98ab1f65ba3516
Maarten Grothus <mail@mgrothus.de>
2022-02-28 03:19:12 +0100

    bin: Add deployment scripts for event keeper
    Deploy: Run bin/setup_event_keeper.py

--- bin/setup_event_keeper.sh
+++ bin/setup_event_keeper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Event keeper deployment script.
+# Event keeper apache deployment script.
 cd /etc/apache2/mods-enabled
 sudo ln -s ../mods-available/ldap.conf
 sudo ln -s ../mods-available/ldap.load
 sudo ln -s ../mods-available/authnz_ldap.load
 sudo cdedb-update.sh
 
-# Then run as root SCRIPT_DRY_RUN="" python3 /cdedb2/ldap/update-ldap.py
+# Then run make ldap-update
 # Then adjust /etc/apache2/sites-available/cdedb-site.conf according to /cdedb2/auto-build/files/stage3/cdedb-site.conf
 # Then run cdedb-update.sh
