5c18e4038faa3e0e1d5baff2419e0f8f4ab1a387
Lars Esser <mail@lars-esser.de>
2022-03-02 02:04:31 +0100

    setup/__main__: provide consistent naming

--- cdedb_setup/__main__.py
+++ cdedb_setup/__main__.py
@@ -7,17 +7,12 @@ from typing import Generator, Optional
 
 import click
 from cdedb_setup.config import (
-    DEFAULT_CONFIGPATH, SecretsConfig, TestConfig, set_configpath as _set_configpath,
+    DEFAULT_CONFIGPATH, SecretsConfig, TestConfig, set_configpath,
 )
 from cdedb_setup.database import (
-    compile_sample_data as _compile_sample_data, create_database as _create_database,
-    create_database_users as _create_database_users,
-    populate_database as _populate_database,
+    compile_sample_data, create_database, create_database_users, populate_database,
 )
-from cdedb_setup.storage import (
-    create_log as _create_log, create_storage as _create_storage,
-    populate_storage as _populate_storage,
-)
+from cdedb_setup.storage import create_log, create_storage, populate_storage
 
 
 @contextlib.contextmanager
@@ -39,122 +34,135 @@ def switch_user(user: str) -> Generator[None, None, None]:
               type=pathlib.Path, show_default=True, help="Or set via CDEDB_CONFIGPATH environment variable.")
 def cli(configpath: pathlib.Path) -> None:
     """Command line interface for setup of CdEDB."""
-    _set_configpath(configpath)
+    set_configpath(configpath)
 
 
-@cli.group()
-def config() -> None:
+@cli.group(name="config")
+def _config() -> None:
     """Interact with the config file."""
     pass
 
 
-@config.command()
+@_config.command(name="get")
 @click.argument("variable")
-def get(variable: str) -> None:
+def _get_config_var(variable: str) -> None:
     """Retrieve the given variable from the current config."""
     config = TestConfig()
-    print(config[variable])
+    click.echo(config[variable])
 
 
-@cli.group()
-def filesystem() -> None:
+@cli.group(name="filesystem")
+def _filesystem() -> None:
     """Preparations regarding the file system."""
     pass
 
 
-@filesystem.command()
+@_filesystem.group(name="storage")
+def _storage() -> None:
+    """Storage"""
+    pass
+
+
+@_storage.command(name="create")
 @click.option("--user", help="Use this user as the owner.")
-def create_storage(user: str) -> None:
+def _create_storage(user: str) -> None:
     """Create the file storage."""
     config = TestConfig()
     if user:
         with switch_user(user):
-            _create_storage(config)
+            create_storage(config)
     else:
-        _create_storage(config)
+        create_storage(config)
 
 
-@filesystem.command()
+@_storage.command(name="populate")
 @click.option("--user", help="Use this user as the owner.")
-def populate_storage(user: str) -> None:
+def _populate_storage(user: str) -> None:
     """Populate the file storage with sample data."""
     config = TestConfig()
     if user:
         with switch_user(user):
-            _populate_storage(config)
+            populate_storage(config)
     else:
-        _populate_storage(config)
+        populate_storage(config)
 
 
-@filesystem.command()
+@_filesystem.group(name="log")
+def _log() -> None:
+    """Log stuff."""
+    pass
+
+
+@_log.command(name="create")
 @click.option("--user", help="Use this user as the owner.")
-def create_log(user: str) -> None:
+def _create_log(user: str) -> None:
     """Create the log storage."""
     config = TestConfig()
     if user:
         with switch_user(user):
-            _create_log(config)
+            create_log(config)
     else:
-        _create_log(config)
+        create_log(config)
 
 
-@cli.group()
-def database() -> None:
+@cli.group(name="database")
+def _database() -> None:
     """Preparations regarding the database."""
     pass
 
 
-@database.command()
-def create_database_users() -> None:
+@_database.command("create-users")
+def _create_database_users() -> None:
     """Creates the database users."""
     config = TestConfig()
-    _create_database_users(config)
+    create_database_users(config)
 
 
-@database.command()
-def create_database() -> None:
+@_database.command(name="create")
+def _create_database() -> None:
     """Create the tables of the database from the config."""
     config = TestConfig()
     secrets = SecretsConfig()
-    _create_database(config, secrets)
+    create_database(config, secrets)
 
 
-@database.command()
+@_database.command(name="populate")
 @click.option("--xss", default=False, help="prepare the database for xss checks")
-def populate_database(xss: bool) -> None:
+def _populate_database(xss: bool) -> None:
     """Populate the database tables with sample data."""
     config = TestConfig()
     secrets = SecretsConfig()
-    _populate_database(config, secrets, xss)
+    populate_database(config, secrets, xss)
 
 
-@cli.group()
-def development() -> None:
+@cli.group(name="dev")
+def _development() -> None:
     """High-level helpers for development."""
     pass
 
 
-@cli.command()
+# TODO in which category should we do this?
+@cli.command(name="compile-sample-data")
 @click.option("--infile", default="/cdedb2/tests/ancillary_files/sample_data.json",
               help="the json file containing the sample data")
 @click.option("--outfile", default="/tmp/sample_data.sql",
               help="the place to store the sql file")
 @click.option("--xss", default=False, help="prepare sample data for xss checks")
-def compile_sample_data(infile: str, outfile: str, xss: bool) -> None:
+def _compile_sample_data(infile: str, outfile: str, xss: bool) -> None:
     """Parse sample data from a .json to a .sql file."""
     config = TestConfig()
-    _compile_sample_data(config, pathlib.Path(infile), pathlib.Path(outfile), xss=xss)
+    compile_sample_data(config, pathlib.Path(infile), pathlib.Path(outfile), xss=xss)
 
 
-@development.command()
+@_development.command(name="make-sample-data")
 @click.option("--user", help="Use this as the owner of the storage and log.")
 @click.pass_context
-def make_sample_data(context: click.Context, user: Optional[str]) -> None:
+def _make_sample_data(context: click.Context, user: Optional[str]) -> None:
     """Repopulates the application with sample data."""
-    context.invoke(create_storage, user=user)
-    context.invoke(populate_storage, user=user)
-    context.invoke(create_database)
-    context.invoke(populate_database)
+    context.invoke(_create_storage, user=user)
+    context.invoke(_populate_storage, user=user)
+    context.invoke(_create_database)
+    context.invoke(_populate_database)
 
 
 if __name__ == "__main__":
