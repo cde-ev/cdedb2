3149ba609bdbb2668ce2b7c103de51e7972fcad9
Lars Esser <mail@lars-esser.de>
2022-05-11 11:51:43 +0200

    common: strip after duplication

--- cdedb/common/__init__.py
+++ cdedb/common/__init__.py
@@ -32,10 +32,10 @@ import werkzeug.routing
 from schulze_condorcet.types import Candidate
 
 from cdedb.common.exceptions import PrivilegeError, ValidationWarning
+from cdedb.common.i18n import n_
 from cdedb.common.roles import roles_to_admin_views
 from cdedb.common.sorting import xsorted
 from cdedb.database.connection import ConnectionContainer
-from cdedb.validationdata import COUNTRY_CODES
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -92,15 +92,6 @@ PathLike = Union[pathlib.Path, str]
 Path = pathlib.Path
 
 T = TypeVar("T")
-
-
-def n_(x: str) -> str:
-    """
-    Alias of the identity for i18n.
-    Identity function that shadows the gettext alias to trick pybabel into
-    adding string to the translated strings.
-    """
-    return x
 
 
 class User:
@@ -523,50 +514,6 @@ def nearly_now(delta: datetime.timedelta = _NEARLY_DELTA_DEFAULT) -> NearlyNow:
     return NearlyNow(
         year=now.year, month=now.month, day=now.day, hour=now.hour,
         minute=now.minute, second=now.second, tzinfo=pytz.utc, delta=delta)
-
-
-def format_country_code(code: str) -> str:
-    """Helper to make string hidden to pybabel.
-
-    All possible combined strings are given for translation
-    in `i18n_additional.py`
-    """
-    return f'CountryCodes.{code}'
-
-
-def get_localized_country_codes(rs: RequestState, lang: str = None
-                                ) -> List[Tuple[str, str]]:
-    """Generate a list of country code - name tuples in current language."""
-
-    if not hasattr(get_localized_country_codes, "localized_country_codes"):
-        localized_country_codes = {
-            lang: xsorted(
-                ((cc, rs.translations[lang].gettext(format_country_code(cc)))
-                 for cc in COUNTRY_CODES),
-                key=lambda x: x[1]
-            )
-            for lang in rs.translations
-        }
-        get_localized_country_codes.localized_country_codes = localized_country_codes  # type: ignore[attr-defined]
-    return get_localized_country_codes.localized_country_codes[lang or rs.lang]  # type: ignore[attr-defined]
-
-
-def get_country_code_from_country(rs: RequestState, country: str) -> str:
-    """Match a country to its country code."""
-
-    if not hasattr(get_country_code_from_country, "reverse_country_code_map"):
-        reverse_map = {
-            lang: {
-                rs.translations[lang].gettext(format_country_code(cc)): cc
-                for cc in COUNTRY_CODES
-            }
-            for lang in rs.translations
-        }
-        get_country_code_from_country.reverse_map = reverse_map  # type: ignore[attr-defined]
-    for lang, v in get_country_code_from_country.reverse_map.items():  # type: ignore[attr-defined]
-        if ret := v.get(country):
-            return ret
-    return country
 
 
 def make_persona_forename(persona: CdEDBObject,
