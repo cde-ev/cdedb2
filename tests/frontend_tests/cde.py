19a887879c47347e797c4be085561e72d7506a52
Lars Esser <mail@lars-esser.de>
2022-05-12 13:18:19 +0200

    tests/lastschrift: fix after change of translation

--- tests/frontend_tests/cde.py
+++ tests/frontend_tests/cde.py
@@ -968,8 +968,7 @@ class TestCdEFrontend(FrontendTest):
         self.traverse({'description': 'Status ändern'})
         f = self.response.forms['modifymembershipform']
         self.submit(f)
-        self.assertPresence("Lastschriftmandate widerrufen.",
-                            div="notifications")
+        self.assertPresence("Lastschriftmandat wurde widerrufen.", div="notifications")
         self.assertNonPresence("Einzugsermächtigung", div="balance")
         self.logout()
         self.login(USER_DICT["farin"])
