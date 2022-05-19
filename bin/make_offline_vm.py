da47a2d35d8079416acbfd75e996848f97718375
Maarten Grothus <mail@mgrothus.de>
2022-05-19 23:06:14 +0200

    event: Fix tables in offline vm export and import
    This has been partially broken for over 2 years.
    I'd call that a rather embaradding oversight, especially
    regarding the fee modifiers which lead to inconsistent
    data regarding amount owed.

--- bin/make_offline_vm.py
+++ bin/make_offline_vm.py
@@ -122,7 +122,7 @@ def work(data_path: pathlib.Path, conf: Config, is_interactive: bool = True,
     with open(data_path, encoding='UTF-8') as infile:
         data = json.load(infile)
 
-    if data.get("EVENT_SCHEMA_VERSION") != [15, 5]:
+    if data.get("EVENT_SCHEMA_VERSION") != [15, 6]:
         raise RuntimeError("Version mismatch -- aborting.")
     if data["kind"] != "full":
         raise RuntimeError("Not a full export -- aborting.")
@@ -179,11 +179,14 @@ def work(data_path: pathlib.Path, conf: Config, is_interactive: bool = True,
 
     tables = (
         'core.personas', 'event.events', 'event.event_parts',
+        'event.part_groups', 'event.part_group_parts',
         'event.courses', 'event.course_tracks', 'event.course_segments',
-        'event.orgas', 'event.field_definitions', 'event.lodgement_groups',
-        'event.lodgements', 'event.registrations',
+        'event.orgas', 'event.field_definitions', 'event.fee_modifiers',
+        'event.lodgement_groups', 'event.lodgements', 'event.registrations',
         'event.registration_parts', 'event.registration_tracks',
-        'event.course_choices', 'event.questionnaire_rows', 'event.log')
+        'event.course_choices', 'event.questionnaire_rows', 'event.log',
+        'event.stored_queries',
+    )
 
     print("Connect to database")
     with connection as conn:
