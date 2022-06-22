b3cb23f3e713396e1467f2bc1844a8f313173a3c
Lars Esser <mail@lars-esser.de>
2022-06-22 22:49:48 +0200

    event/event-keeper: adopt evolution-trial

--- cdedb/database/evolutions/2022-06-15_add_event_keeper_log_id.sql
+++ cdedb/database/evolutions/2022-06-15_add_event_keeper_log_id.sql
@@ -1,6 +1,10 @@
 BEGIN;
-    CREATE TABLE event.keeper;
-    ALTER TABLE event.keeper ADD COLUMN event_id integer UNIQUE REFERENCES event.events(id);
-    ALTER TABLE event.keeper ADD COLUMN log_id integer NOT NULL DEFAULT 0;
+    CREATE TABLE event.keeper (
+        id                      serial PRIMARY KEY,
+        event_id                integer UNIQUE REFERENCES event.events(id),
+        log_id                  integer NOT NULL DEFAULT 0
+    );
+    GRANT SELECT, INSERT, UPDATE, DELETE ON event.keeper TO cdb_persona;
+    GRANT SELECT, UPDATE ON event.keeper_id_seq TO cdb_persona;
     -- TODO: add entries for existing events
 COMMIT;
