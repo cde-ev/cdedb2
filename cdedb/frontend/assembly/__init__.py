e55068de3bcbb50ec4cf72b03ee2230237ee74d5
Tobias Udtke <tobias.udtke@gmail.com>
2022-10-02 01:03:34 +0200

    assembly: add comments and notification to source error handling.

--- cdedb/frontend/assembly/__init__.py
+++ cdedb/frontend/assembly/__init__.py
@@ -727,14 +727,21 @@ class AssemblyFrontend(AbstractUserFrontend):
             rs.notify("warning", n_("Assembly already concluded."))
             return self.redirect(rs, "assembly/show_assembly")
 
+        # Use inspect validation to avoid showing a validation error for this.
+        # If the given source ID is not a valid ID at all, simply ignore it.
         if (source_id := inspect_validation(vtypes.ID, source_id)[0]):
-            source_ballot = self.assemblyproxy.get_ballot(rs, source_id)
+            # If the ballot does not exist, get_ballot would throw a key error.
+            source_ballot = unwrap(
+                self.assemblyproxy.get_ballots(rs, (source_id,)) or None)
             if source_ballot:
                 merge_dicts(rs.values, source_ballot)
                 # Multiselects work differently from multiple checkboxes, so
                 #  merge_dicts does the wrong thing here (setlist).
                 rs.values['linked_attachments'] = self.assemblyproxy.list_attachments(
                     rs, ballot_id=source_id)
+            # If the ballot does not exist or is not accessible, show a warning instead.
+            else:
+                rs.notify("warning", rs.gettext("Unknown ballot."))
 
         attachment_ids = self.assemblyproxy.list_attachments(
             rs, assembly_id=assembly_id)
