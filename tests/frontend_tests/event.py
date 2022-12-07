3c695625969cdffd3f9bdb0fa0f3d64a11636db1
Lars Esser <mail@lars-esser.de>
2022-12-07 19:41:58 +0100

    tests/event: fix log test

--- tests/frontend_tests/event.py
+++ tests/frontend_tests/event.py
@@ -1284,6 +1284,10 @@ etc;anything else""", f['entries_2'].value)
                 'change_note': "Alternative Akademie",
                 'code': const.EventLogCodes.lodgement_group_created,
                 'event_id': 1002,
+            },
+            {
+                'code': const.EventLogCodes.event_changed,
+                'event_id': 1002,
             }
         ])
         self.assertLogEqual(
