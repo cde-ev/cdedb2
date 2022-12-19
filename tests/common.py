c6d5a07aea8068e92e3bb17e63947ec46798fa56
Markus Walter <houseofsuns@houseofsuns.org>
2022-12-19 19:42:01 +0100

    test: Remove redundant code.

--- tests/common.py
+++ tests/common.py
@@ -517,9 +517,6 @@ class BrowserTest(CdEDBTest):
     """
     serverProcess = None
 
-    def __init__(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
-        super().__init__(*args, **kwargs)
-
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
