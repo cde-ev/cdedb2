f54653ee0dfd1b601bca5c6f3ef407e62eb0f794
Tobias Udtke <tobias.udtke@gmail.com>
2022-12-21 04:26:32 +0100

    fix deploy.

--- related/deploy/2022-11-08_finance_log_transaction_date.sh
+++ related/deploy/2022-11-08_finance_log_transaction_date.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u cdb psql -U cdb -d cdb -f /cdedb2/cdedb/database/evolutions/2022-11-08.sql
+sudo -u cdb psql -U cdb -d cdb -f /cdedb2/cdedb/database/evolutions/2022-11-08_finance_log_transaction_date.sql
