f62e8153e54ab39a6ef37fb111d2e1607b4ab02e
Maarten Grothus <mail@mgrothus.de>
2023-02-17 20:59:19 +0100

    Update AttachmentPolicy translations
    This has not reflected reality for quite some time.

--- cdedb/database/evolutions/2023-02-17_ml_convert_html.sql
+++ cdedb/database/evolutions/2023-02-17_ml_convert_html.sql
@@ -1,3 +1,3 @@
 BEGIN;
-    ALTER TABLE ml.mailinglists ADD COLUMN convert_html assembly.assemblies ADD COLUMN convert_html boolean NOT NULL DEFAULT TRUE;
+    ALTER TABLE ml.mailinglists ADD COLUMN convert_html boolean NOT NULL DEFAULT TRUE;
 COMMIT;
