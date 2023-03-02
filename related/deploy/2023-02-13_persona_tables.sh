96cffcdf87d4a24b3f577cb61eb910e0e1cdaffd
Tobias Udtke <tobias.udtke@gmail.com>
2023-03-02 19:29:44 +0100

    bin: fix deploy scripts.

--- related/deploy/2023-02-13_persona_tables.sh
+++ related/deploy/2023-02-13_persona_tables.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u cdb psql -U cdb -d cdb -f /cdedb2/cdedb/database/evolutions/2022-02-13_persona_tables.sql
+sudo -u cdb psql -U cdb -d cdb -f /cdedb2/cdedb/database/evolutions/2023-02-13_persona_tables.sql
