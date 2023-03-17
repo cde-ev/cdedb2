3e705a05d41fe174b750ab25ff78c8a2688a401b
Tobias Udtke <tobias.udtke@gmail.com>
2023-03-17 09:52:10 +0100

    fcp: Allow part substitutions when serializing parse result.

--- cdedb/fee_condition_parser/roundtrip.py
+++ cdedb/fee_condition_parser/roundtrip.py
@@ -4,27 +4,44 @@ from typing import Callable, Dict, List, Optional, Tuple
 import pyparsing as pp
 
 
-def serialize(result: pp.ParseResults, parenthesis_if_not: Optional[str] = None) -> str:
+def serialize(result: pp.ParseResults, *, part_substitutions: Dict[str, str] = None) -> str:
+    """Public serialization interface, to get a normalized condition string.
+
+    :param part_substitutions: Replace each part name in the dict with it's value.
+    """
+    return _serialize(result, outer_operator=None, ps=part_substitutions or {})
+
+
+def _serialize(result: pp.ParseResults, outer_operator: Optional[str], ps: Dict[str, str]) -> str:
+    """Internal recursive normalizer.
+
+    :param outer_operator: If given, put parentheses around current operation, if
+        it is not this operator. Pass the name of the current operator to the recursive
+        call for it's operands. For example `AND(a, AND(b, c))` is normalized to
+        'a AND b AND c', while `AND(a, OR(b, c))` is normalized to 'a AND (b OR c)`,
+        because the AND-operand `OR(b, c)` is not also a AND-operation.
+    :param: ps: A dict of part substitutions. Substitute part names for these values.
+    """
     functions = {
-        'and': lambda x: f"{serialize(x[0], 'and')} and {serialize(x[1], 'and')}",
-        'or': lambda x: f"{serialize(x[0], 'or')} or {serialize(x[1], 'or')}",
-        'xor': lambda x: f"{serialize(x[0], 'xor')} xor {serialize(x[1], 'xor')}",
-        'not': lambda x: f"not {serialize(x[0], '')}",
+        'and': lambda x: f"{_serialize(x[0], 'and', ps)} and {_serialize(x[1], 'and', ps)}",
+        'or': lambda x: f"{_serialize(x[0], 'or', ps)} or {_serialize(x[1], 'or', ps)}",
+        'xor': lambda x: f"{_serialize(x[0], 'xor', ps)} xor {_serialize(x[1], 'xor', ps)}",
+        'not': lambda x: f"not {_serialize(x[0], '', ps)}",
         'true': lambda x_: "true",
         'false': lambda x_: "false",
         'field': lambda x: f"field.{x[0]}",
-        'part': lambda x: f"part.{x[0]}",
+        'part': lambda x: f"part.{ps.get(x[0], x[0])}",
         'bool': lambda x: f"{x[0]}",
     }
     name = result.get_name()
-    if name in ('and', 'or', 'xor') and parenthesis_if_not is not None and name != parenthesis_if_not:
+    if name in ('and', 'or', 'xor') and outer_operator is not None and name != outer_operator:
         return f"({functions[name](result)})"
     else:
         return functions[name](result)
 
 
 def visual_debug(result: pp.ParseResults, field_values: Dict[str, bool], part_values: Dict[str, bool],
-                 other_values: Dict[str, bool], parenthesis_if_not: Optional[str] = None, top_level: bool = True
+                 other_values: Dict[str, bool], outer_operator: Optional[str] = None, top_level: bool = True
                  ) -> Tuple[bool, str]:
     functions: Dict[str, Callable[[List[Tuple[bool, str]]], Tuple[bool, str]]] = {
         'and': lambda sr: (sub_results[0][0] and sub_results[1][0], f"{sub_results[0][1]} <b>and</b> {sub_results[1][1]}"),
@@ -36,7 +53,7 @@ def visual_debug(result: pp.ParseResults, field_values: Dict[str, bool], part_va
     }
 
     name = result.get_name()
-    sub_parenthesis_if_not = name if name in ('and', 'or', 'xor') else '' if name == 'not' else None
+    operator = name if name in ('and', 'or', 'xor') else ('' if name == 'not' else None)
 
     if name == "field":
         value, text = field_values[result[0]], f"field.{result[0]}"
@@ -45,12 +62,12 @@ def visual_debug(result: pp.ParseResults, field_values: Dict[str, bool], part_va
     elif name == "bool":
         value, text = other_values[result[0]], f"{result[0]}"
     else:
-        sub_results = [visual_debug(token, field_values, part_values, other_values, sub_parenthesis_if_not, False)
+        sub_results = [visual_debug(token, field_values, part_values, other_values, operator, False)
                        for token in result]
         value, text = functions[name](sub_results)
 
     if name in ('and', 'or', 'xor'):
-        if parenthesis_if_not is not None and name != parenthesis_if_not:
+        if outer_operator is not None and name != outer_operator:
             return value, f"<span class=\"block {'true' if value else 'false'}\"><b>(</b>{text}<b>)</b></span>"
         elif top_level:
             return value, f"<span class=\"block {'true' if value else 'false'}\">{text}</span>"
