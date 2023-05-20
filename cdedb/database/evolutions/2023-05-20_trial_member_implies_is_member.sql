c0f18baa45753bad5999b64bf1e1f80e7d5cb544
Lars Esser <mail@lars-esser.de>
2023-05-20 18:05:37 +0200

    database/evolution: make sure the state of the database conforms with the new check

--- cdedb/database/evolutions/2023-05-20_trial_member_implies_is_member.sql
+++ cdedb/database/evolutions/2023-05-20_trial_member_implies_is_member.sql
@@ -1,1 +1,4 @@
-ALTER TABLE core.personas ADD CONSTRAINT personas_trial_member_implicits CHECK (NOT trial_member OR is_member);
+BEGIN;
+    UPDATE core.personas SET trial_member = FALSE WHERE is_member = FALSE;
+    ALTER TABLE core.personas ADD CONSTRAINT personas_trial_member_implicits CHECK (NOT trial_member OR is_member);
+COMMIT;
