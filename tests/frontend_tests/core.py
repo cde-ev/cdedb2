344862e6dd241543230cc4c3be512be7ff40587d
Lars Esser <mail@lars-esser.de>
2023-06-17 17:08:32 +0200

    tests/test_user-archived: remove unnecessary self.follow()

--- tests/frontend_tests/core.py
+++ tests/frontend_tests/core.py
@@ -214,7 +214,6 @@ class TestCoreFrontend(FrontendTest):
         _check_redirected_profile()
         # The history is available
         self.get('/core/persona/8/history')
-        self.follow()
         self.assertTitle("Änderungshistorie von Hades Hell")
         self.assertPresence("Benutzer ist archiviert.", div='static-notifications')
         self.get('/core/persona/8/adminchange')
