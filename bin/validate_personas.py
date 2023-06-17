e32a0482d5b07d1b84d501f55750ffbcf55a1355
Lars Esser <mail@lars-esser.de>
2023-06-17 19:36:41 +0200

    bin/validate_persona: check changelog for all personas, improve error printing

--- bin/validate_personas.py
+++ bin/validate_personas.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import sys
-from typing import Optional
+from typing import List, Optional, Tuple
 
 import cdedb.common.validation.types as vtypes
 import cdedb.database.constants as const
-from cdedb.backend.common import affirm_validation as affirm
 from cdedb.common.sorting import xsorted
+from cdedb.common.validation.validate import validate_check as check
 from cdedb.script import Script
 
 # Prepare stuff
@@ -14,20 +14,15 @@ script = Script(dbuser="cdb_persona")
 rs = script.rs()
 core = script.make_backend("core")
 
-def _print_error(persona_id: int, e: Exception) -> None:
-    print("-" * 80, file=sys.stderr)
-    print(f"Error for persona {persona_id}:", file=sys.stderr)
-    print(e.__class__, file=sys.stderr)
-    print(e, file=sys.stderr)
-
 # Execution
 
 with script:
     persona_id: Optional[int] = -1
-    errors = 0
+    total_errors = 0
     while True:
         persona_id = core.next_persona(
             rs, persona_id=persona_id, is_member=None, is_archived=False)
+        errors: List[Tuple[Optional[str], Exception]] = []
 
         if not persona_id:
             break
@@ -35,57 +30,45 @@ with script:
         # Validate ml data
         persona = core.get_ml_user(rs, persona_id)
 
-        try:
-            affirm(vtypes.Persona, persona)
-        except Exception as e:
-            _print_error(persona_id, e)
-            errors += 1
-            continue
+        _, errs = check(vtypes.Persona, persona, ignore_warnings=True)
+        errors.extend(errs)
 
         # Validate event data if applicable
-        if not persona['is_event_realm']:
-            continue
-
-        persona = core.get_event_user(rs, persona_id)
-        try:
-            affirm(vtypes.Persona, persona)
-        except Exception as e:
-            _print_error(persona_id, e)
-            errors += 1
-            continue
+        if persona['is_event_realm']:
+            persona = core.get_event_user(rs, persona_id)
+            _, errs = check(vtypes.Persona, persona, ignore_warnings=True)
+            errors.extend(errs)
 
         # Validate cde/total data if applicable
-        if not persona['is_cde_realm']:
-            continue
-
         persona = core.get_total_persona(rs, persona_id)
-        try:
-            affirm(vtypes.Persona, persona)
-        except Exception as e:
-            _print_error(persona_id, e)
-            errors += 1
-            continue
+        if persona['is_cde_realm']:
+            _, errs = check(vtypes.Persona, persona, ignore_warnings=True)
+            errors.extend(errs)
 
         # Validate consistency of changelog with persona state
         history = core.changelog_get_history(rs, persona_id, generations=None)
-
         for generation in xsorted(history.values(), key=lambda x: x['generation'],
                                   reverse=True):
             if generation['code'] == const.MemberChangeStati.committed:
                 if diff := [key for key in persona if persona[key] != generation[key]]:
-                    print("-" * 80, file=sys.stderr)
-                    print(f"Error for persona {persona_id}:", file=sys.stderr)
-                    errors += 1
                     for key in diff:
-                        print(f"Changelog inconsistent for field {key}: {persona[key]}"
-                              f" != {generation[key]}", file=sys.stderr)
+                        errors.append(("Changelog", RuntimeError(
+                            f"Changelog inconsistent for field {key}: {persona[key]}"
+                            f" != {generation[key]}")))
                 break
         else:
-            print("-" * 80, file=sys.stderr)
-            print(f"No commited state found for persona {persona_id}.", file=sys.stderr)
-            errors += 1
+            errors.append(("Changelog", RuntimeError(
+                f"No committed state found for persona {persona_id}.")))
 
-    if errors:
-        print(f"There were {errors} errors.", file=sys.stderr)
+        # Print all errors for this persona
+        if errors:
+            print("-" * 80, file=sys.stderr)
+            print(f"Error for persona {persona_id}:", file=sys.stderr)
+            for field, error in errors:
+                print(f"{error.__class__}: {error} ({field})", file=sys.stderr)
+            total_errors += 1
+
+    if total_errors:
+        print(f"\nThere were {total_errors} errors.", file=sys.stderr)
     else:
         print("All personas were validated successfully.")
