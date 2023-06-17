a720ccda073a75b9cdf59ae446e589a4c6cf0e8a
Lars Esser <mail@lars-esser.de>
2023-06-17 19:54:10 +0200

    core/changelog_submit_change: base check on committed_state, not on current_state

--- cdedb/backend/core/base.py
+++ cdedb/backend/core/base.py
@@ -326,7 +326,7 @@ class CoreBaseBackend(AbstractBackend):
             for gen in xsorted(history.values(), key=lambda x: x['generation'],
                                reverse=True):
                 if gen['code'] == const.MemberChangeStati.committed:
-                    if any(current_state[k] != gen[k] for k in current_state):
+                    if any(committed_state[k] != gen[k] for k in committed_state):
                         raise RuntimeError(n_("Persona and Changelog inconsistent."))
                     break
             else:
