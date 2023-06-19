237799cdf8d35eaf54d0774fc9770ea5881ed9b8
Tobias Udtke <tobias.udtke@gmail.com>
2023-06-20 01:36:06 +0200

    bin: add some additional output and error checks.

--- bin/fix_changelog_finances.py
+++ bin/fix_changelog_finances.py
@@ -1,5 +1,6 @@
 import datetime
 import decimal
+from pprint import pprint
 
 import cdedb.database.constants as const
 from cdedb.backend.core import CoreBackend
@@ -14,13 +15,20 @@ affected_count = 351
 error_time = datetime.datetime.fromisoformat("2023-04-12 10:28:35+02:00")
 persona_balances = {}
 
-def get_clean_history(persona_id: int, old_generation: int) -> CdEDBObject:
+def get_clean_history(persona_id: int, generation: int) -> CdEDBObject:
     generation_data = unwrap(
-        core.changelog_get_history(s.rs(), persona_id, [old_generation]))
+        core.changelog_get_history(s.rs(), persona_id, [generation]))
 
-    generation_data['persona_id'] = persona_id
+    if generation_data['code'] != const.MemberChangeStati.committed:
+        raise ValueError(
+            f"Unexpectedly encountered non-committed generation"
+            f" ({generation}, {generation_data['generation_data']['code']}"
+            f" for user {persona_id}.")
 
+    # Persona id is under the key 'id'.
+    generation_data['persona_id'] = generation_data['id']
     del generation_data['id']
+
     generation_data['submitted_by'] = s.persona_id
     generation_data['reviewed_by'] = None
     generation_data['ctime'] = None
@@ -36,7 +44,7 @@ def get_clean_history(persona_id: int, old_generation: int) -> CdEDBObject:
 
 with s:
     # 1.a Find those users where nothing has changed since the error. (337 users)
-    q = """
+    q_1a = """
         SELECT p.id, generation
         FROM core.personas AS p
             JOIN core.changelog AS cl ON p.id = cl.persona_id
@@ -47,8 +55,7 @@ with s:
             ) AS clmax ON p.id = clmax.persona_id
         WHERE p.balance != cl.balance AND cl.generation = clmax.max_gen
     """
-    p = ()
-    data = core.query_all(s.rs(), q, p)
+    data = core.query_all(s.rs(), q_1a, ())
     easy_affected = len(data)
 
     print(f"Fixing {easy_affected} easy personas, by appending changelog entries.")
@@ -61,11 +68,16 @@ with s:
         persona_balances[persona_id] = generation_data['balance']
         generation_data['balance'] = decimal.Decimal("0.00")
 
+        print(f"Inserting changelog generation {generation_data['generation']}"
+              f" for user {persona_id}.")
         core.sql_insert(s.rs(), "core.changelog", generation_data)
+
+    if core.query_all(s.rs(), q_1a, ()):
+        raise ValueError("Not all personas were fixed during 1a.")
 
     # 1.c Add skipped transactions from the finance log to changelog and persona
     # (17 users (subset of 337))
-    q = """
+    q_1c = """
         SELECT fl.ctime AS fl_ctime, fl.persona_id, fl.submitted_by, fl.transaction_date, fl.delta, clmax.max_gen, cl2.ctime AS max_gen_ctime 
         FROM cde.finance_log fl
             LEFT JOIN core.changelog cl ON fl.persona_id = cl.persona_id AND fl.ctime = cl.ctime
@@ -77,14 +89,14 @@ with s:
             JOIN core.changelog cl2 ON fl.persona_id = cl2.persona_id AND clmax.max_gen = cl2.generation
         WHERE fl.code = %s AND cl.persona_id IS NULL AND fl.ctime > %s;
     """
-    p = (
+    p_1c = (
         const.FinanceLogCodes.increase_balance,
         datetime.datetime.fromisoformat("2023-01-01"),
     )
-    data = core.query_all(s.rs(), q, p)
+    data = core.query_all(s.rs(), q_1c, p_1c)
 
     print(f"Fixing changelog and persona for {len(data)} missing transactions,"
-          f" by appending skipped transactions and updateing persona.")
+          f" by appending skipped transactions and updating persona.")
 
     note_template = ("Guthabenänderung um {amount} auf {new_balance} "
                      "(Überwiesen am {date})")
@@ -92,6 +104,7 @@ with s:
     for d in data:
         if d['max_gen_ctime'] > d['fl_ctime']:
             raise RuntimeError
+        persona_id = d['persona_id']
         generation_data = get_clean_history(d['persona_id'], d['max_gen'])
         generation_data['balance'] = d['delta']
 
@@ -100,15 +113,19 @@ with s:
         generation_data['change_note'] = note_template.format(
             amount=d['delta'], new_balance=d['delta'], date=d['transaction_date'])
 
+        print(f"Inserting transfer changelog entry"
+              f" (gen {generation_data['generation']}) for user {persona_id}.")
         core.sql_insert(s.rs(), "core.changelog", generation_data)
 
+        print(f"Updating persona table accordingly for user {persona_id}.")
         persona_data = {
             'id': d['persona_id'],
             'balance': d['delta'],
         }
         core.sql_update(s.rs(), "core.personas", persona_data)
 
-    # TODO add consistency check for 1.c here?
+    if core.query_all(s.rs(), q_1c, p_1c):
+        raise ValueError("Not all personas were fixed during 1c.")
 
     # 2.a Find those users where there were changes since the error. (14 users)
     # It is hard to determine these algorithmically.
@@ -120,33 +137,33 @@ with s:
     print(f"Fixing {len(affected_changed)} complicated personas.")
 
     # 2.b Increase changelog generation by one for each entry after the error
-    q = """
+    q_2b_1 = """
         ALTER TABLE core.changelog DROP CONSTRAINT changelog_persona_id_generation_key;
     """
-    core.query_exec(s.rs(), q, ())
+    core.query_exec(s.rs(), q_2b_1, ())
 
-    q = """
+    q_2b_2 = """
         UPDATE core.changelog SET generation = generation + 1
         WHERE persona_id = ANY(%s) AND ctime >= %s;
     """
-    p = (affected_changed, error_time)
-    rowcount = core.query_exec(s.rs(), q, p)
+    p_2b_2 = (affected_changed, error_time)
+    rowcount = core.query_exec(s.rs(), q_2b_2, p_2b_2)
     print(f"Moved {rowcount} changelog entries.")
 
-    q = """
+    q_2b_3 = """
         ALTER TABLE core.changelog ADD UNIQUE (persona_id, generation);
     """
-    core.query_exec(s.rs(), q, ())
+    core.query_exec(s.rs(), q_2b_3, ())
 
     # 2.c Determine number of missing changelog entry
-    q = """
+    q_2c = """
         SELECT persona_id, MAX(generation) AS before_gen
         FROM core.changelog
         WHERE persona_id = ANY(%s) AND ctime < %s
         GROUP BY persona_id
     """
-    params = (affected_changed, error_time,)
-    data = core.query_all(s.rs(), q, params)
+    p_2c = (affected_changed, error_time,)
+    data = core.query_all(s.rs(), q_2c, p_2c)
 
     print(f"Inserting {len(data)} new changelog entries.")
 
@@ -156,7 +173,12 @@ with s:
         generation_data = get_clean_history(persona_id, d['before_gen'])
         persona_balances[persona_id] = generation_data['balance']
         generation_data['balance'] = decimal.Decimal("0.00")
+
+        print(f"Inserting changelog generation into freed up position"
+              f" ({generation_data['generation']}) for user {persona_id}.")
         core.sql_insert(s.rs(), "core.changelog", generation_data)
 
-    from pprint import pprint
+    print("Summary of removed balances:")
     pprint(persona_balances)
+
+    print(f"Total: {sum(persona_balances.values(), start=decimal.Decimal(0))}")
