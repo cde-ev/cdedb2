25a60e60991922b023d97164f2dd6dbf59557533
Lars Esser <mail@lars-esser.de>
2023-07-20 19:16:46 +0200

    deploy: fix deploy script

--- related/deploy/2023-07-06_period_exmember_balance.sh
+++ related/deploy/2023-07-06_period_exmember_balance.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u www-data python3 /cdedb2/cdedb/database/evolutions/2023-07-06_period_balance_exmembers.sql
+sudo -u cdb psql -U cdb -d cdb -f /cdedb2/cdedb/database/evolutions/2023-07-06_period_balance_exmembers.sql
