b9f249d0930061a3acaf3e33041ff2c0e204ecbf
Maarten Grothus <mail@mgrothus.de>
2023-08-27 18:08:13 +0200

    deploy: Add migration

--- cdedb/database/evolutions/2023-08-27_trickle_down.sql
+++ cdedb/database/evolutions/2023-08-27_trickle_down.sql
@@ -1,7 +1,11 @@
 BEGIN;
-    ALTER TABLE event.event_parts ADD COLUMN camping_mat_field;
-    ALTER TABLE event.course_tracks ADD COLUMN course_room_field;
-    -- some magic migration
+    ALTER TABLE event.event_parts ADD COLUMN camping_mat_field VARCHAR;
+    ALTER TABLE event.course_tracks ADD COLUMN course_room_field VARCHAR;
+    UPDATE event.event_parts SET camping_mat_field = e.camping_mat_field
+        FROM event.events AS e WHERE event_id = e.id;
+    UPDATE event.course_tracks SET course_room_field = e.course_room_field
+        FROM event.events AS e, event.event_parts AS ep
+        WHERE part_id = ep.id AND ep.event_id = e.id;
     ALTER TABLE event.events DROP COLUMN camping_mat_field;
     ALTER TABLE event.events DROP COLUMN course_room_field;
 COMMIT;
