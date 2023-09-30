258a113841bba8c8fbc6aa9545da58975cc1007a
Lars Esser <mail@lars-esser.de>
2023-09-30 22:03:27 +0200

    models: add custom add_dict method
    This is basically the same implementation as dataclasses.asdict().
    However, since we have backreferences in our objects, we need to
    exclude those from being included in the dict representation.

--- cdedb/models/event.py
+++ cdedb/models/event.py
@@ -30,6 +30,7 @@ from typing import (
     TYPE_CHECKING, Any, ClassVar, Collection, Mapping, Optional, TypeVar, get_args,
     get_origin,
 )
+import copy
 
 import cdedb.common.validation.types as vtypes
 import cdedb.database.constants as const
@@ -91,6 +92,67 @@ class EventDataclass(CdEDataclass):
         return {
             obj.id: obj for obj in map(cls.from_database, list_of_data)
         }
+
+    def as_dict(self, *, dict_factory=dict):
+        """Return the fields of a dataclass instance as a new dictionary mapping
+        field names to field values.
+
+        This is an almost 1:1 copy of dataclasses.asdict. However, we need to exclude
+        the backward references to avoid infinit recursion, so we need to dig into
+        the implementation details here...
+        """
+        return self._asdict_inner(self, dict_factory)
+
+    def _asdict_inner(self, obj, dict_factory):
+        if dataclasses._is_dataclass_instance(obj):
+            result = []
+            for f in dataclasses.fields(obj):
+                #######################################################
+                # the following two lines are the only differences to #
+                # dataclasses._as_dict_inner                          #
+                #######################################################
+                if not self._include_in_dict(f):
+                    continue
+                value = self._asdict_inner(getattr(obj, f.name), dict_factory)
+                result.append((f.name, value))
+            return dict_factory(result)
+        elif isinstance(obj, tuple) and hasattr(obj, '_fields'):
+            # obj is a namedtuple.  Recurse into it, but the returned
+            # object is another namedtuple of the same type.  This is
+            # similar to how other list- or tuple-derived classes are
+            # treated (see below), but we just need to create them
+            # differently because a namedtuple's __init__ needs to be
+            # called differently (see bpo-34363).
+
+            # I'm not using namedtuple's _asdict()
+            # method, because:
+            # - it does not recurse in to the namedtuple fields and
+            #   convert them to dicts (using dict_factory).
+            # - I don't actually want to return a dict here.  The main
+            #   use case here is json.dumps, and it handles converting
+            #   namedtuples to lists.  Admittedly we're losing some
+            #   information here when we produce a json list instead of a
+            #   dict.  Note that if we returned dicts here instead of
+            #   namedtuples, we could no longer call asdict() on a data
+            #   structure where a namedtuple was used as a dict key.
+
+            return type(obj)(*[self._asdict_inner(v, dict_factory) for v in obj])
+        elif isinstance(obj, (list, tuple)):
+            # Assume we can create an object of this type by passing in a
+            # generator (which is not true for namedtuples, handled
+            # above).
+            return type(obj)(self._asdict_inner(v, dict_factory) for v in obj)
+        elif isinstance(obj, dict):
+            return type(obj)((self._asdict_inner(k, dict_factory),
+                              self._asdict_inner(v, dict_factory))
+                             for k, v in obj.items())
+        else:
+            return copy.deepcopy(obj)
+
+    @staticmethod
+    def _include_in_dict(field: dataclasses.Field) -> bool:
+        """Should this field be part of the dict representation of this object?"""
+        return field.repr
 
 
 #
