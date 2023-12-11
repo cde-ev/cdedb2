dde7166de04b0e44f1387d87750c6eb0f96dc3d8
Tobias Udtke <tobias.udtke@gmail.com>
2023-12-11 20:39:34 +0000

    models.event: add type annotation.

--- cdedb/models/event.py
+++ cdedb/models/event.py
@@ -35,7 +35,7 @@ from typing import TYPE_CHECKING, Any, ClassVar, Optional, get_args, get_origin
 import cdedb.common.validation.types as vtypes
 import cdedb.database.constants as const
 from cdedb.common import User, cast_fields, now
-from cdedb.common.query import make_registration_query_spec
+from cdedb.common.query import QuerySpec, make_registration_query_spec
 from cdedb.common.sorting import Sortkey
 from cdedb.models.common import CdEDataclass, CdEDataclassMap
 
@@ -205,7 +205,7 @@ class Event(EventDataclass):
         return self.begin, self.end, self.title
 
     @functools.cached_property
-    def basic_registration_query_spec(self):
+    def basic_registration_query_spec(self) -> QuerySpec:
         return make_registration_query_spec(self)
 
 
