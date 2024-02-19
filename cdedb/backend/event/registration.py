fa435effd2c8b65491dab708d3e40283638ccf9a
Tobias Udtke <tobias.udtke@gmail.com>
2024-02-19 20:27:57 +0000

    event: simplify fee calculation by removing is_member and is_orga overrides. Simply put overrides into the registration.

--- cdedb/backend/event/registration.py
+++ cdedb/backend/event/registration.py
@@ -1016,8 +1016,7 @@ class EventRegistrationBackend(EventBaseBackend):
             current = self.get_registration(rs, data['id'])
             update = {
                 'id': data['id'],
-                'amount_owed': self._calculate_single_fee(
-                    rs, current, event=event),
+                'amount_owed': self._calculate_single_fee(rs, current, event=event),
             }
             ret *= self.sql_update(rs, "event.registrations", update)
             self.event_log(
@@ -1057,10 +1056,9 @@ class EventRegistrationBackend(EventBaseBackend):
             self.assert_offline_lock(rs, event_id=data['event_id'])
             persona = self.core.get_persona(rs, data['persona_id'])
             data['fields'] = fdata
-            data['amount_owed'] = self._calculate_single_fee(
-                rs, data, event=event, is_member=persona['is_member'])
-            data['fields'] = PsycoJson(fdata)
             data['is_member'] = persona['is_member']
+            data['amount_owed'] = self._calculate_single_fee(rs, data, event=event)
+            data['fields'] = PsycoJson(fdata)
             part_ids = {e['id'] for e in self.sql_select(
                 rs, "event.event_parts", ("id",), (data['event_id'],),
                 entity_key="event_id")}
@@ -1264,16 +1262,14 @@ class EventRegistrationBackend(EventBaseBackend):
                                            visual_debug=visual_debug)
 
     def _calculate_single_fee(self, rs: RequestState, reg: CdEDBObject, *,
-                              event: models.Event, is_member: Optional[bool] = None,
-                              ) -> decimal.Decimal:
+                              event: models.Event) -> decimal.Decimal:
         """Helper to only calculate return the fee amount for a single registration."""
-        return self._calculate_complex_fee(
-            rs, reg, event=event, is_member=is_member).amount
+        return self._calculate_complex_fee(rs, reg, event=event).amount
 
-    def _calculate_complex_fee(self, rs: RequestState, reg: CdEDBObject, *,
-                               event: models.Event, is_member: Optional[bool] = None,
-                               is_orga: Optional[bool] = None,
-                               visual_debug: bool = False) -> RegistrationFeeData:
+    @staticmethod
+    def _calculate_complex_fee(rs: RequestState, reg: CdEDBObject, *,
+                               event: models.Event, visual_debug: bool = False,
+                               ) -> RegistrationFeeData:
         """Helper function to calculate the fee for one registration.
 
         This is used inside `create_registration` and `set_registration`,
@@ -1284,8 +1280,6 @@ class EventRegistrationBackend(EventBaseBackend):
         If the subset does not violate any MEP constraints, the total of all it's parts'
         fees is calculated. The final fee will be the maximum of all such totals.
 
-        :param is_member: If this is None, retrieve membership status here.
-        :param is_orga: If this is None, determine orga status regularly.
         :param visual_debug: If True, create a html representation of the
             evaluated condition.
         """
@@ -1304,8 +1298,7 @@ class EventRegistrationBackend(EventBaseBackend):
         for tmp_is_member in (True, False):
             # Other bools can be added here, but also require adjustment to the parser.
             other_bools = {
-                'is_orga':
-                    reg['persona_id'] in event.orgas if is_orga is None else is_orga,
+                'is_orga': reg.get('is_orga', reg['persona_id'] in event.orgas),
                 'is_member': tmp_is_member,
                 'any_part': any(reg_part_involvement.values()),
                 'all_parts': all(reg_part_involvement.values()),
@@ -1331,14 +1324,8 @@ class EventRegistrationBackend(EventBaseBackend):
             ret[tmp_is_member] = RegistrationFee(
                 amount, active_fees, visual_debug_data, fees_by_kind)
 
-        if is_member is None:
-            is_member = reg.get('is_member')
-            if is_member is None:
-                is_member = self.core.get_persona(rs, reg['persona_id'])['is_member']
-            assert is_member is not None
-
         return RegistrationFeeData(
-            member_fee=ret[True], nonmember_fee=ret[False], is_member=is_member,
+            member_fee=ret[True], nonmember_fee=ret[False], is_member=reg['is_member'],
         )
 
     @access("event")
@@ -1399,10 +1386,11 @@ class EventRegistrationBackend(EventBaseBackend):
                 for part_id in event.parts
             },
             'fields': fields,
+            'is_member': is_member,
+            'is_orga': is_orga,
         }
         return self._calculate_complex_fee(
-            rs, fake_registration, event=event, is_member=is_member, is_orga=is_orga,
-            visual_debug=True)
+            rs, fake_registration, event=event, visual_debug=True)
 
     @access("event")
     def calculate_fees(self, rs: RequestState, registration_ids: Collection[int],
