bd4f6a9ae65e14b8aa4f82b7620425678cc62bc7
Markus Walter <houseofsuns@houseofsuns.org>
2024-04-08 15:09:45 +0200

    docker: Fix environment leak for gunicorn.

--- related/docker/run-webserver.sh
+++ related/docker/run-webserver.sh
@@ -13,7 +13,8 @@ if [ ! -e /var/lib/cdedb ]; then
 fi
 
 export SCRIPT_NAME=/db
-sudo --preserve-env -u www-cde /usr/bin/gunicorn --forwarded-allow-ips="*" -w 4 --bind localhost:8998 wsgi.cdedb-app:application --daemon
+sudo --preserve-env=SCRIPT_NAME -u www-cde /usr/bin/gunicorn --forwarded-allow-ips="*" \
+     -w 4 --bind localhost:8998 wsgi.cdedb-app:application --daemon
 unset SCRIPT_NAME
 
 cd /
