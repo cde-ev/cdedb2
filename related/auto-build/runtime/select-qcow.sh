a19827787356855a8aa4323016665dc0219e10b0
Markus Walter <houseofsuns@houseofsuns.org>
2024-05-02 22:08:19 +0200

    auto-build: Add makefile for better discoverability.

---
+++ related/auto-build/runtime/select-qcow.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NUM=$(ls -1 ./*.qcow2 | wc -l)
+
+if (( NUM == 0 )); then
+    >&2 echo "No qcow image found."
+    exit 1
+elif (( NUM > 1 )); then
+    >&2 echo "More than one qcow image. Confused script cannot continue."
+    exit 2
+fi
+
+IMAGE=$(ls -1 ./*.qcow2)
+echo "$IMAGE"
