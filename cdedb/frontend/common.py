e5a5f80d86d12cf91d334fa35b80b781d4621ba5
Markus Walter <houseofsuns@houseofsuns.org>
2024-06-05 16:25:27 +0200

    frontend: Move notification code to a more sensible location.

--- cdedb/frontend/common.py
+++ cdedb/frontend/common.py
@@ -719,21 +719,6 @@ class AbstractFrontend(BaseApp, metaclass=abc.ABCMeta):
             else:
                 rs.notify("info", n_("The CdE database is currently under"
                                      " maintenance and is unavailable."))
-        defect_username = None
-        mls_with_defect_explicits = None
-        if rs.user.persona_id and (defect_addresses := self.coreproxy.get_defect_addresses(rs, [rs.user.persona_id])):
-            if rs.user.username in defect_addresses:
-                defect_username = rs.user.username
-            mls_with_defect_explicit_ids = {
-                e.address: e.ml_ids for e in defect_addresses.values()
-                if e.subscriber_id == rs.user.persona_id}
-            mls = self.mlproxy.get_mailinglists(
-                rs, set().union(*mls_with_defect_explicit_ids.values()))
-            mls_with_defect_explicits = {
-                address: [mls[ml_id] for ml_id in ml_ids]
-                for address, ml_ids in mls_with_defect_explicit_ids.items()}
-        params['defect_username'] = defect_username
-        params['mls_with_defect_explicits'] = mls_with_defect_explicits
 
         # A nonce to mark safe <script> tags in context of the CSP header
         csp_nonce = token_hex(12)
