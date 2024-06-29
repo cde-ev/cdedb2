c0e7a150c39845533d58fa8bbe7c62b86d02f08f
Maarten Grothus <mail@mgrothus.de>
2024-06-29 17:34:05 +0200

    bin: Add migration script to lock questionnaires
    Deploy: Run script

---
+++ bin/lock_archived_questionnaires.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Generic script to repurge all purged personas.
+
+Should not be archived after use.
+"""
+import cdedb.database.constants as const
+from cdedb.backend.common import Silencer
+from cdedb.script import Script
+
+# setup
+
+script = Script(persona_id=-1, dbuser="cdb_admin")
+rs = script.rs()
+event = script.make_backend("event")
+
+# work
+
+with script:
+    event_ids = event.list_events(rs, archived=True)
+    for event_id in event_ids:
+        with Silencer(rs):
+            aq = const.QuestionnaireUsages.additional
+            questionnaire = event.get_questionnaire(rs, event_id, [aq])[aq]
+            for entry in questionnaire:
+                entry['readonly'] = True
+            event.set_questionnaire(rs, event_id, {aq: questionnaire})
