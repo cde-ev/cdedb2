0d586b6cb24f402ad2ebe8047d8de44135e12e57
Tobias Udtke <tobias.udtke@gmail.com>
2024-07-20 07:46:47 +0200

    ruff: ignore interface import errors to appease Maarten.

--- tests/ldap_tests/__init__.py
+++ tests/ldap_tests/__init__.py
@@ -1,6 +1,4 @@
 """All LDAP tests."""
 
-from tests.ldap_tests.backend import (
-    AsyncLDAPBackendTest as AsyncLDAPBackendTest, LDAPBackendTest as LDAPBackendTest,
-)
-from tests.ldap_tests.frontend import TestLDAP as TestLDAP
+from tests.ldap_tests.backend import AsyncLDAPBackendTest, LDAPBackendTest
+from tests.ldap_tests.frontend import TestLDAP
--- tests/other_tests/__init__.py
+++ tests/other_tests/__init__.py
@@ -4,19 +4,15 @@ This contains tests for our database interface, our middleware (like our config 
 validation) and some special cases (like testing the offline instance, our scripting
 interface or the session handling).
 """
-from tests.other_tests.test_browser import TestBrowser as TestBrowser
-from tests.other_tests.test_common import TestCommon as TestCommon
-from tests.other_tests.test_config import TestConfig as TestConfig
-from tests.other_tests.test_database import TestDatabase as TestDatabase
-from tests.other_tests.test_huge_data import TestHugeData as TestHugeData
-from tests.other_tests.test_offline import TestOffline as TestOffline
-from tests.other_tests.test_script import TestScript as TestScript
+from tests.other_tests.test_browser import TestBrowser
+from tests.other_tests.test_common import TestCommon
+from tests.other_tests.test_config import TestConfig
+from tests.other_tests.test_database import TestDatabase
+from tests.other_tests.test_huge_data import TestHugeData
+from tests.other_tests.test_offline import TestOffline
+from tests.other_tests.test_script import TestScript
 from tests.other_tests.test_session import (
-    TestMultiSessionFrontend as TestMultiSessionFrontend,
-    TestSessionBackend as TestSessionBackend,
-    TestSessionFrontend as TestSessionFrontend,
+    TestMultiSessionFrontend, TestSessionBackend, TestSessionFrontend,
 )
-from tests.other_tests.test_validation import TestValidation as TestValidation
-from tests.other_tests.test_vote_verification_script import (
-    TestVerificationScript as TestVerificationScript,
-)
+from tests.other_tests.test_validation import TestValidation
+from tests.other_tests.test_vote_verification_script import TestVerificationScript
