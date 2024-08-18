47bf41f37e28664d067c5d3d458dab87707d2332
Markus Walter <houseofsuns@houseofsuns.org>
2024-08-18 16:30:15 +0200

    database: Add evolution.

---
+++ cdedb/database/evolutions/2024-08-18-email-states.sql
@@ -0,0 +1,12 @@
+BEGIN;
+    CREATE TABLE  core.email_states (
+            id                      serial PRIMARY KEY,
+            address                 varchar NOT NULL UNIQUE,
+            -- see cdedb.database.constants.EmailStatus
+            status                  integer NOT NULL,
+            notes                   varchar
+    );
+    GRANT SELECT on core.email_states TO cdb_persona;
+    GRANT SELECT, UPDATE ON core.email_states_id_seq TO cdb_admin;
+    GRANT INSERT, UPDATE, DELETE ON core.email_states TO cdb_admin;
+COMMIT;
