9e3b29cc1f58ef55ce18111d97b8b5da829b5ede
Johannes Jung <jungj+cde-gitea@posteo.de>
2024-11-25 10:27:36 +0100

    event/checkin periods: fix evolution

--- cdedb/database/evolutions/2024-11-09-checkin.sql
+++ cdedb/database/evolutions/2024-11-09-checkin.sql
@@ -6,9 +6,11 @@ BEGIN;
         checkout_time           timestamp WITH TIME ZONE DEFAULT NULL
     );
     CREATE INDEX checkin_periods_registration_id_idx ON event.checkin_periods(registration_id);
-    GRANT SELECT, INSERT, DELETE, UPDATE (checkin_time, checkout_time) ON event.checkin_periods TO cdb_persona;
+    GRANT SELECT, INSERT, DELETE, UPDATE ON event.checkin_periods TO cdb_persona;
+    GRANT SELECT, UPDATE ON event.checkin_periods_id_seq TO cdb_persona;
     INSERT INTO event.checkin_periods (registration_id, checkin_time)
         SELECT id, checkin
-        FROM event.registrations;
+        FROM event.registrations
+        WHERE checkin IS NOT NULL;
     ALTER TABLE event.registrations DROP COLUMN checkin;
 COMMIT;
