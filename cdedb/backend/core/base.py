f9cf5909466d5eadb205f906d0af709ed4709bc6
Tobias Udtke <tobias.udtke@gmail.com>
2025-02-22 14:12:32 +0100

    core: exclude lone moderators from automatic archival.

--- cdedb/backend/core/base.py
+++ cdedb/backend/core/base.py
@@ -1601,6 +1601,21 @@ class CoreBaseBackend(AbstractBackend):
             if self.query_all(rs, query, (persona_id, states)):
                 return False
 
+            # Check being the sole moderator of a mailinglist.
+            query = """
+                SELECT mailinglist_id, COUNT(persona_id)
+                FROM
+                    ml.moderators
+                WHERE mailinglist_id IN (
+                    SELECT mailinglist_id FROM ml.moderators WHERE persona_id = %s
+                )
+                GROUP BY mailinglist_id
+                HAVING COUNT(persona_id) = 1
+                ORDER BY mailinglist_id
+            """
+            if self.query_all(rs, query, (persona_id,)):
+                return False
+
         return True
 
     @access(*REALM_ADMINS)
