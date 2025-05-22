c8d007072ceab3271f07a35a357b47c227c349f8
Tobias Udtke <tobias.udtke@gmail.com>
2025-05-22 15:56:09 +0200

    event: Remove unneccessary retrieval of registrations in helper exclusively used by `get_constraint_violations`.

--- cdedb/frontend/event/base.py
+++ cdedb/frontend/event/base.py
@@ -531,7 +531,7 @@ class EventBaseFrontend(AbstractUserFrontend):
 
         choice_stats: "ChoiceStats"  # noqa: UP037
         attendee_stats: "AttendeeStats"  # noqa: UP037
-        choice_stats, attendee_stats = self.get_course_stats(rs, event)  # type: ignore[attr-defined]
+        choice_stats, attendee_stats = self.get_course_stats(rs, event, all_registrations)  # type: ignore[attr-defined]
 
         # Retrieve lodgements.
         all_lodgements = self.eventproxy.get_lodgements(
