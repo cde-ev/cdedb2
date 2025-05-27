8d0d0460651a59f773bfdcb8989aa444bb38505d
Tobias Udtke <tobias.udtke@gmail.com>
2025-05-27 15:40:02 +0200

    tests: test redirect to non default tab after tab form submit.

--- tests/other_tests/test_browser.py
+++ tests/other_tests/test_browser.py
@@ -177,8 +177,11 @@ class TestBrowser(BrowserTest):
 
         page.locator("#dynamicrow-delete-button-9").click()
 
+        page.get_by_role("tab", name="Kursfelder").click()
+        page.wait_for_url("http://localhost:5000/event/event/1/field/summary#tab_course")
+
         page.get_by_role("button", name="Speichern").click()
-        page.wait_for_url("http://localhost:5000/event/event/1/field/summary#tab_registration")
+        page.wait_for_url("http://localhost:5000/event/event/1/field/summary#tab_course")
 
         expect(page.locator('input[name="title_1001"]')).to_have_value('Lieblingsheld')
         expect(page.locator('input[name="field_name_1001"]')).to_have_value('held')
