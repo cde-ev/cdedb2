3659a988150d88a34f41f7c58d7a7eed1c29aa24
Tobias Udtke <tobias.udtke@gmail.com>
2025-05-29 02:58:26 +0200

    database: fix complaint case columns.

--- cdedb/database/cdedb-tables.sql
+++ cdedb/database/cdedb-tables.sql
@@ -532,9 +532,9 @@ GRANT INSERT, UPDATE, DELETE ON complaint.cases TO cdb_admin;
 CREATE TABLE complaint.entries (
     id            serial PRIMARY KEY,
     case_id       integer NOT NULL REFERENCES complaint.cases(id),
-    type          integer DEFAULT NULL, -- database.constants.ComplaintEntryType
+    entry_type    integer DEFAULT NULL, -- database.constants.ComplaintEntryType
     root_entry    integer REFERENCES complaint.entries(id) DEFAULT NULL, -- only for some types
-    concerned_id  integer NOT NULL REFERENCES core.personas(id) DEFAULT NULL  -- maybe reference involved_id instead
+    concerned_id  integer REFERENCES core.personas(id) DEFAULT NULL  -- maybe reference involved_id instead
 );
 GRANT SELECT, INSERT ON complaint.entries TO cdb_persona;
 
