4874e18834d4f7ee056816449cb85b72ed8196d6
Tobias Udtke <tobias.udtke@gmail.com>
2025-05-30 19:53:17 +0200

    tests: fix xss test

--- cdedb/cli/dev/json2sql.py
+++ cdedb/cli/dev/json2sql.py
@@ -78,7 +78,8 @@ def prepare_aux(data: CdEDBObject) -> AuxData:
         "vote_begin", "vote_end", "vote_extension_end", "secret", "vote", "salt",
         "hash", "filename", "file_hash", "address", "local_part", "new_balance",
         "modifier_name", "transaction_date", "condition", "donation", "payment_date",
-        'etime', 'rtime', 'secret_hash', 'member_total',
+        'etime', 'rtime', 'secret_hash', 'member_total', "start_date", "end_date",
+        "timestamp",
     }
     xss_table_excludes = {
         "cde.org_period", "cde.expuls_period",
