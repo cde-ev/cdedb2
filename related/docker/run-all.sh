7f3cecd1fa985c3c205b89da2c8000afe57dda3a
Lars Esser <mail@lars-esser.de>
2025-06-02 18:06:25 +0200

    related: remove calls to setup logdir

--- related/docker/run-all.sh
+++ related/docker/run-all.sh
@@ -5,9 +5,6 @@ cd /cdedb2
 
 # Be paranoid about filesystem locations. This should not be necessary, but
 # sadly is. Can probably be removed after #3427 is resolved.
-if [ ! -e /var/log/cdedb ]; then
-    python3 -m cdedb filesystem --owner www-cde --group www-data log create
-fi
 if [ ! -e /var/lib/cdedb ]; then
     python3 -m cdedb filesystem --owner www-cde --group www-data storage create
 fi
