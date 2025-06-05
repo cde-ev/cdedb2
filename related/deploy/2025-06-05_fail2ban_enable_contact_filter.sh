ac3f7e932e55f6dcdcfdeed74b273ad9276d2914
Lars Esser <mail@lars-esser.de>
2025-06-05 13:14:17 +0200

    fail2ban: active contact filter

---
+++ related/deploy/2025-06-05_fail2ban_enable_contact_filter.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+sudo cp /cdedb2/related/auto-build/files/stage3/fail2ban-filter-cdedb-contact.conf /etc/fail2ban/filter.d/cdedb-contact.conf
+sudo cp /cdedb2/related/auto-build/files/stage3/jail.local /etc/fail2ban/
