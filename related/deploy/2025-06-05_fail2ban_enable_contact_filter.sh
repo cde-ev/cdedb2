2915642628e6ab168393836c8621357c88c8126a
Lars Esser <mail@lars-esser.de>
2025-09-06 10:10:41 +0200

    fix typo, add missing dependency install to deploy

--- related/deploy/2025-06-05_fail2ban_enable_contact_filter.sh
+++ related/deploy/2025-06-05_fail2ban_enable_contact_filter.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+sudo apt install python3-systemd
+
 sudo cp /cdedb2/related/auto-build/files/stage3/fail2ban-filter-cdedb-apitoken.conf /etc/fail2ban/filter.d/cdedb-apitoken.conf
 sudo cp /cdedb2/related/auto-build/files/stage3/fail2ban-filter-cdedb-contact.conf /etc/fail2ban/filter.d/cdedb-contact.conf
 sudo cp /cdedb2/related/auto-build/files/stage3/fail2ban-filter-cdedb-login.conf /etc/fail2ban/filter.d/cdedb-login.conf
