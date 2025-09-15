ae657cce366b1baca24cc650e0083d9f6b5064da
Tobias Udtke <tobias.udtke@gmail.com>
2025-09-15 14:40:09 +0200

    complaint: sort entry versions correctly within their entry.

--- cdedb/models/complaint.py
+++ cdedb/models/complaint.py
@@ -377,7 +377,7 @@ class ComplaintEntryVersion(CdEDataclass):
         return Fernet(key).decrypt(description).decode("utf-8")
 
     def get_sortkey(self) -> Sortkey:
-        return (self.timestamp, self.ctime)
+        return (self.ctime,)
 
     @classmethod
     def from_database(cls, data: CdEDBObject) -> Self:
