b0bdf7fd539c680d70886322686315b0d63f9c79
Tobias Udtke <tobias.udtke@gmail.com>
2025-09-19 02:04:13 +0200

    tests: fix one more test failure

--- tests/frontend_tests/core.py
+++ tests/frontend_tests/core.py
@@ -3248,32 +3248,41 @@ class TestCoreFrontend(FrontendTest):
         # First: generate data
         # request two new accounts
         self._genesis_request(self.ML_GENESIS_DATA)
-        logs.append((1001, const.CoreLogCodes.genesis_request))
-        logs.append((1002, const.CoreLogCodes.genesis_verified))
+        logs.append(const.CoreLogCodes.genesis_request)
+        logs.append(const.CoreLogCodes.genesis_verified)
 
         event_genesis = self.EVENT_GENESIS_DATA.copy()
         event_genesis['username'] = "tester@example.cde"
         self._genesis_request(event_genesis)
-        logs.append((1003, const.CoreLogCodes.genesis_request))
-        logs.append((1004, const.CoreLogCodes.genesis_verified))
+        logs.append(const.CoreLogCodes.genesis_request)
+        logs.append(const.CoreLogCodes.genesis_verified)
 
         # approve the account requests
         self.login(user)
-        self.traverse("Accountanfragen", "Details")
+        self.traverse("Accountanfragen", "Details", "Accountanfrage bearbeiten")
+        f = self.response.forms['genesismodifyform']
+        f['pevent_id'] = 1
+        self.submit(f)
+        self.traverse("Accountanfrage bearbeiten")
+        f = self.response.forms['genesismodifyform']
+        f['pcourse_id'] = 1
+        self.submit(f)
         f = self.response.forms['genesisdecisionform']
         self.submit(f, button="decision", value=str(GenesisDecision.approve))
-        logs.append((1005, const.CoreLogCodes.genesis_change))
-        logs.append((1006, const.CoreLogCodes.persona_creation))
-        logs.append((1007, const.CoreLogCodes.genesis_approved))
-        logs.append((1008, const.CoreLogCodes.password_reset_cookie))
+        logs.append(const.CoreLogCodes.genesis_change)
+        logs.append(const.CoreLogCodes.genesis_change)
+        logs.append(const.CoreLogCodes.genesis_change)
+        logs.append(const.CoreLogCodes.persona_creation)
+        logs.append(const.CoreLogCodes.genesis_approved)
+        logs.append(const.CoreLogCodes.password_reset_cookie)
 
         self.traverse("Details")
         f = self.response.forms['genesisdecisionform']
         self.submit(f, button="decision", value=str(GenesisDecision.approve))
-        logs.append((1009, const.CoreLogCodes.genesis_change))
-        logs.append((1010, const.CoreLogCodes.persona_creation))
-        logs.append((1011, const.CoreLogCodes.genesis_approved))
-        logs.append((1012, const.CoreLogCodes.password_reset_cookie))
+        logs.append(const.CoreLogCodes.genesis_change)
+        logs.append(const.CoreLogCodes.persona_creation)
+        logs.append(const.CoreLogCodes.genesis_approved)
+        logs.append(const.CoreLogCodes.password_reset_cookie)
 
         # make janis assembly user
         self.admin_view_profile('janis')
@@ -3284,7 +3293,7 @@ class TestCoreFrontend(FrontendTest):
         f = self.response.forms['promotionform']
         f['change_note'] = promotion_change_note = "trivial promotion"
         self.submit(f)
-        logs.append((1013, const.CoreLogCodes.realm_change))
+        logs.append(const.CoreLogCodes.realm_change)
 
         # change berta's user name
         self.admin_view_profile('berta')
@@ -3292,12 +3301,12 @@ class TestCoreFrontend(FrontendTest):
         f = self.response.forms['usernamechangeform']
         f['new_username'] = "bertalotta@example.cde"
         self.submit(f)
-        logs.append((1014, const.CoreLogCodes.username_change))
+        logs.append(const.CoreLogCodes.username_change)
 
         # Now check it
         self.traverse({'description': 'Index'},
                       {'description': 'Account-Log'})
-        self.log_pagination("Account-Log", tuple(logs))
+        self.log_pagination("Account-Log", list(enumerate(logs, start=1001)))
         f = self.response.forms["logshowform"]
         f["codes"] = [const.CoreLogCodes.genesis_verified.value,
                       const.CoreLogCodes.realm_change.value,
