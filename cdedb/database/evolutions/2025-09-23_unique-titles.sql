532e7bfc95b2937efdcc848690103fa90b4a139d
Tobias Udtke <tobias.udtke@gmail.com>
2025-09-23 23:09:52 +0200

    database: fix evolution

--- cdedb/database/evolutions/2025-09-23_unique-titles.sql
+++ cdedb/database/evolutions/2025-09-23_unique-titles.sql
@@ -1,4 +1,4 @@
 BEGIN;
-    ALTER TABLE event.event_fees ADD UNIQUE event_fee_title_constraint (event_id, title);
-    ALTER TABLE assembly.candidates ADD UNIQUE candidate_title_constraint (ballot_id, title);
+    ALTER TABLE event.event_fees ADD CONSTRAINT event_fee_title_constraint UNIQUE (event_id, title);
+    ALTER TABLE assembly.candidates ADD CONSTRAINT candidate_title_constraint UNIQUE (ballot_id, title);
 COMMIT;
