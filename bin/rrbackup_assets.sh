d5a95d0cf3fcd028ad533c1daf69d64c1822f6e8
MaartenGrothus <mail@mgrothus.de>
2025-11-13 02:10:14 +0100

    bin: Improve and fix rrbackup_assets script
    In particular, this automatically reduces event keeper repo size daily.
    The oom issue we had before does no longer occur.

--- bin/rrbackup_assets.sh
+++ bin/rrbackup_assets.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# to be executed via crontab of the cdedb user
+# to be executed via crontab of the www-cde user
 # 42 5 * * * /cdedb2/bin/rrbackup_assets.sh
 
-sudo tar -C / -zcf //sic_temp/backups/cdedb-assets-backup-$(date +%u).tar.gz var/lib/cdedb
+cd /var/lib/cdedb/event_keeper/
+for D in */; do(cd "$D" && sudo -u www-cde git maintenance run); done
+tar -C / -zcf //sic_temp/backups/cdedb-assets-backup-$(date +%u).tar.gz var/lib/cdedb
