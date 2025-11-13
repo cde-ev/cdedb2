d03a22ffbd0fcfd1cdc21cc0c30f48add39f045d
MaartenGrothus <mail@mgrothus.de>
2025-11-13 15:03:52 +0100

    bin: remove unnecessary sudo

--- bin/rrbackup_assets.sh
+++ bin/rrbackup_assets.sh
@@ -4,5 +4,5 @@
 # 42 5 * * * /cdedb2/bin/rrbackup_assets.sh
 
 cd /var/lib/cdedb/event_keeper/
-for D in */; do(cd "$D" && sudo -u www-cde git maintenance run); done
+for D in */; do(cd "$D" && git maintenance run); done
 tar -C / -zcf //sic_temp/backups/cdedb-assets-backup-$(date +%u).tar.gz var/lib/cdedb
