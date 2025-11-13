997ab03a71f87f830919f9bf50ab8e4066b14a29
Tobias Udtke <tobias.udtke@gmail.com>
2025-11-13 01:53:44 +0100

    database: adjust some inconsistent indentations

--- cdedb/database/cdedb-tables.sql
+++ cdedb/database/cdedb-tables.sql
@@ -508,11 +508,11 @@ GRANT SELECT, UPDATE ON core.anonymous_messages_id_seq TO cdb_persona;
 -- Read-Only table translating german postal codes into coordinates for nearby search.
 DROP TABLE IF EXISTS core.postal_code_locations;
 CREATE TABLE core.postal_code_locations (
-        postal_code     varchar PRIMARY KEY,
-        name            varchar,
-        earth_location  earth,
-        lat             float8,
-        long            float8
+        postal_code             varchar PRIMARY KEY,
+        name                    varchar,
+        earth_location          earth,
+        lat                     float8,
+        long                    float8
 );
 GRANT SELECT ON core.postal_code_locations TO cdb_persona;
 
@@ -524,68 +524,68 @@ CREATE SCHEMA complaint;
 GRANT USAGE ON SCHEMA complaint TO cdb_persona;
 
 CREATE TABLE complaint.cases (
-    id         serial PRIMARY KEY,
-    kind       integer NOT NULL, -- database.constants.ComplaintKind
-    is_grave   boolean DEFAULT FALSE,
-    summary    varchar NOT NULL,
-    notes      varchar,
-    start_date date,
-    end_date   date
+        id                      serial PRIMARY KEY,
+        kind                    integer NOT NULL, -- database.constants.ComplaintKind
+        is_grave                boolean DEFAULT FALSE,
+        summary                 varchar NOT NULL,
+        notes                   varchar,
+        start_date              date,
+        end_date                date
 );
 GRANT SELECT ON complaint.cases TO cdb_persona;
 GRANT INSERT, UPDATE, DELETE ON complaint.cases TO cdb_admin;
 GRANT SELECT, UPDATE ON complaint.cases_id_seq TO cdb_admin;
 
 CREATE TABLE complaint.access_log (
-    id              bigserial PRIMARY KEY,
-    persona_id      integer NOT NULL REFERENCES core.personas(id),
-    case_id         integer NOT NULL REFERENCES complaint.cases(id),
-    ctime           timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
-    atime           timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
-    UNIQUE (persona_id, case_id)
+        id                      bigserial PRIMARY KEY,
+        persona_id              integer NOT NULL REFERENCES core.personas(id),
+        case_id                 integer NOT NULL REFERENCES complaint.cases(id),
+        ctime                   timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
+        atime                   timestamp WITH TIME ZONE NOT NULL DEFAULT now(),
+        UNIQUE (persona_id, case_id)
 );
 GRANT SELECT, INSERT, UPDATE(ctime, atime), DELETE ON complaint.access_log TO cdb_persona;
 GRANT SELECT, UPDATE ON complaint.access_log_id_seq TO cdb_persona;
 
 CREATE TABLE complaint.entries (
-    id            serial PRIMARY KEY,
-    case_id       integer NOT NULL REFERENCES complaint.cases(id),
-    entry_type    integer DEFAULT NULL, -- database.constants.ComplaintEntryType
-    parent_id     integer REFERENCES complaint.entries(id) DEFAULT NULL, -- only for some types
-    concerned_id  integer REFERENCES core.personas(id) DEFAULT NULL,  -- maybe reference involved_id instead
-    is_revoked    boolean NOT NULL DEFAULT FALSE
+        id                      serial PRIMARY KEY,
+        case_id                 integer NOT NULL REFERENCES complaint.cases(id),
+        entry_type              integer DEFAULT NULL, -- database.constants.ComplaintEntryType
+        parent_id               integer REFERENCES complaint.entries(id) DEFAULT NULL, -- only for some types
+        concerned_id            integer REFERENCES core.personas(id) DEFAULT NULL,  -- maybe reference involved_id instead
+        is_revoked              boolean NOT NULL DEFAULT FALSE
 );
 GRANT SELECT, INSERT, UPDATE (is_revoked) ON complaint.entries TO cdb_persona;
 GRANT SELECT, UPDATE ON complaint.entries_id_seq TO cdb_persona;
 
 CREATE TABLE complaint.entry_versions (
-    id            serial PRIMARY KEY,
-    entry_id      integer NOT NULL REFERENCES complaint.entries(id),
-    submitted_by  integer NOT NULL REFERENCES core.personas(id),
-    description   bytea, -- encrypted
-    length        integer,
-    CONSTRAINT complaint_entry_empty_description_length
-        CHECK ((description IS NULL) = (length IS NULL)),
-    ctime         timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    timestamp     timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    etime         timestamp WITH TIME ZONE DEFAULT NULL,
-    -- attachment contents are stored encrypted. Metadata is not.
-    attachment_hash     varchar DEFAULT NULL,
-    attachment_title        varchar DEFAULT NULL,
-    attachment_filename     varchar DEFAULT NULL,
-    CONSTRAINT complaint_entry_attachment_title
-        CHECK ((attachment_hash IS NULL) = (attachment_title IS NULL)),
-    CONSTRAINT complaint_entry_attachment_filename
-        CHECK ((attachment_hash IS NULL) = (attachment_filename IS NULL)),
-    -- is_shared     boolean NOT NULL DEFAULT TRUE, -- with companions with shared involved personas
-    dtime         timestamp WITH TIME ZONE DEFAULT NULL,  -- to be updated on deletion
-    dreason       varchar DEFAULT NULL,
-    deleted_by    integer REFERENCES core.personas(id) DEFAULT NULL,
-    UNIQUE(entry_id, dtime),
-    CONSTRAINT complaint_entry_deletion_reason
-        CHECK ((dtime IS NULL) = (dreason IS NULL)),
-    CONSTRAINT complaint_entry_deletion_by
-        CHECK ((dtime IS NULL) = (deleted_by IS NULL))
+        id                      serial PRIMARY KEY,
+        entry_id                integer NOT NULL REFERENCES complaint.entries(id),
+        submitted_by            integer NOT NULL REFERENCES core.personas(id),
+        description             bytea, -- encrypted
+        length                  integer,
+        CONSTRAINT complaint_entry_empty_description_length
+            CHECK ((description IS NULL) = (length IS NULL)),
+        ctime                   timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
+        timestamp               timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
+        etime                   timestamp WITH TIME ZONE DEFAULT NULL,
+        -- attachment contents are stored encrypted. Metadata is not.
+        attachment_hash         varchar DEFAULT NULL,
+        attachment_title        varchar DEFAULT NULL,
+        attachment_filename     varchar DEFAULT NULL,
+        CONSTRAINT complaint_entry_attachment_title
+            CHECK ((attachment_hash IS NULL) = (attachment_title IS NULL)),
+        CONSTRAINT complaint_entry_attachment_filename
+            CHECK ((attachment_hash IS NULL) = (attachment_filename IS NULL)),
+        -- is_shared     boolean NOT NULL DEFAULT TRUE, -- with companions with shared involved personas
+        dtime                   timestamp WITH TIME ZONE DEFAULT NULL,  -- to be updated on deletion
+        dreason                 varchar DEFAULT NULL,
+        deleted_by              integer REFERENCES core.personas(id) DEFAULT NULL,
+        UNIQUE(entry_id, dtime),
+        CONSTRAINT complaint_entry_deletion_reason
+            CHECK ((dtime IS NULL) = (dreason IS NULL)),
+        CONSTRAINT complaint_entry_deletion_by
+            CHECK ((dtime IS NULL) = (deleted_by IS NULL))
 );
 CREATE UNIQUE INDEX entry_versions_id_current ON complaint.entry_versions(entry_id) WHERE dtime IS NULL;
 CREATE INDEX entry_versions_attachment_hash ON complaint.entry_versions(attachment_hash);
@@ -593,21 +593,21 @@ GRANT SELECT, INSERT, UPDATE (dtime, dreason, deleted_by) ON complaint.entry_ver
 GRANT SELECT, UPDATE ON complaint.entry_versions_id_seq TO cdb_persona;
 
 CREATE TABLE complaint.authors (
-    id                      serial PRIMARY KEY,
-    entry_version_id        integer NOT NULL REFERENCES complaint.entry_versions(id),
-    persona_id              integer NOT NULL REFERENCES core.personas(id),
-    UNIQUE(entry_version_id, persona_id)
+        id                      serial PRIMARY KEY,
+        entry_version_id        integer NOT NULL REFERENCES complaint.entry_versions(id),
+        persona_id              integer NOT NULL REFERENCES core.personas(id),
+        UNIQUE(entry_version_id, persona_id)
 );
 GRANT SELECT, INSERT ON complaint.authors TO cdb_persona;
 GRANT SELECT, UPDATE ON complaint.authors_id_seq TO cdb_persona;
 
 CREATE TABLE complaint.involved (
-    id            serial PRIMARY KEY,
-    case_id       int NOT NULL REFERENCES complaint.cases(id),
-    persona_id    int NOT NULL REFERENCES core.personas(id),
-    UNIQUE(case_id, persona_id),
-    involved_type integer NOT NULL, -- database.constants.ComplaintInvolvementType
-    is_informed   boolean NOT NULL DEFAULT FALSE
+        id                      serial PRIMARY KEY,
+        case_id                 int NOT NULL REFERENCES complaint.cases(id),
+        persona_id              int NOT NULL REFERENCES core.personas(id),
+        UNIQUE(case_id, persona_id),
+        involved_type           integer NOT NULL, -- database.constants.ComplaintInvolvementType
+        is_informed             boolean NOT NULL DEFAULT FALSE
 );
 GRANT SELECT ON complaint.involved TO cdb_persona;
 GRANT INSERT, UPDATE (is_informed), DELETE ON complaint.involved TO cdb_admin;
@@ -615,16 +615,16 @@ GRANT SELECT, UPDATE ON complaint.involved_id_seq TO cdb_admin;
 
 -- very limited access per case and persona
 CREATE TABLE complaint.companions (
-    id                      serial PRIMARY KEY,
-    -- Who is being accompanied in which case.
-    case_id                 int NOT NULL REFERENCES complaint.cases(id),
-    involved_persona_id     int NOT NULL REFERENCES core.personas(id),
-    -- This is duplicating the info from above, but this ensures integrity to the other table.
-    involved_id             int NOT NULL REFERENCES complaint.involved(id) ON DELETE CASCADE,
-    -- Who is doing the accompanying.
-    companion_persona_id    int NOT NULL REFERENCES core.personas(id),
-    UNIQUE(involved_id, companion_persona_id),
-    is_withdrawn  boolean NOT NULL DEFAULT FALSE
+        id                      serial PRIMARY KEY,
+        -- Who is being accompanied in which case.
+        case_id                 int NOT NULL REFERENCES complaint.cases(id),
+        involved_persona_id     int NOT NULL REFERENCES core.personas(id),
+        -- This is duplicating the info from above, but this ensures integrity to the other table.
+        involved_id             int NOT NULL REFERENCES complaint.involved(id) ON DELETE CASCADE,
+        -- Who is doing the accompanying.
+        companion_persona_id    int NOT NULL REFERENCES core.personas(id),
+        UNIQUE(involved_id, companion_persona_id),
+        is_withdrawn            boolean NOT NULL DEFAULT FALSE
 );
 GRANT SELECT ON complaint.companions TO cdb_persona;
 GRANT INSERT, UPDATE (is_withdrawn), DELETE ON complaint.companions TO cdb_admin;
@@ -632,17 +632,17 @@ GRANT SELECT, UPDATE ON complaint.companions_id_seq TO cdb_admin;
 
 -- people, who are blocked from "meeting" within the complaint process
 CREATE TABLE complaint.companion_incompatibles (
-    id            serial PRIMARY KEY,
-    blocker_id    int NOT NULL REFERENCES core.personas(id),
-    blocked_id    int NOT NULL REFERENCES core.personas(id),
-    UNIQUE(blocker_id, blocked_id)
+        id                      serial PRIMARY KEY,
+        blocker_id              int NOT NULL REFERENCES core.personas(id),
+        blocked_id              int NOT NULL REFERENCES core.personas(id),
+        UNIQUE(blocker_id, blocked_id)
 );
 GRANT SELECT, INSERT, DELETE ON complaint.companion_incompatibles TO cdb_persona;
 
 -- like event helpers, may access limited information on measures
 CREATE TABLE complaint.enforcers (
-    id                      serial PRIMARY KEY,
-    persona_id              integer UNIQUE NOT NULL REFERENCES core.personas(id)
+        id                      serial PRIMARY KEY,
+        persona_id              integer UNIQUE NOT NULL REFERENCES core.personas(id)
 );
 GRANT SELECT ON complaint.enforcers TO cdb_persona;
 GRANT INSERT, DELETE ON complaint.enforcers TO cdb_admin;
