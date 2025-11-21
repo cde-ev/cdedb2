d82eb5d82b9faaeded4331523237b8ff596f077e
Tobias Udtke <tobias.udtke@gmail.com>
2025-11-21 15:18:46 +0100

    common:crypt: add some docstrings.

--- cdedb/common/crypt.py
+++ cdedb/common/crypt.py
@@ -14,18 +14,26 @@ from cdedb.common import BytesLike
 
 
 def verify_password(password: str, password_hash: str) -> bool:
+    """Constant time password verification."""
     return passlib.hash.sha512_crypt.verify(password, password_hash)
 
 
 def encrypt_password(password: str) -> str:
+    """Encrypt a password with a salt and a suitable number of hash rounds."""
     return passlib.hash.sha512_crypt.hash(password)
 
 
 def generate_encrytion_key() -> bytes:
+    """Generate an encryption key for symmetric encryption helpers below."""
     return cryptography.fernet.Fernet.generate_key()
 
 
 def _encrypt(data: str | bytes | None, *, key: bytes) -> bytes | None:
+    """
+    Wrapper for encrypting data symmetrically.
+
+    Allows None to passthrough and encodes strings.
+    """
     if data is None:
         return None
     if isinstance(data, str):
@@ -34,6 +42,8 @@ def _encrypt(data: str | bytes | None, *, key: bytes) -> bytes | None:
 
 
 class _EncryptProtocol(Protocol):
+    """Slightly beefed up annotation for type inferrence purposes."""
+
     @overload
     def __call__(self, data: None) -> None: ...
 
@@ -44,16 +54,25 @@ class _EncryptProtocol(Protocol):
 
 
 def get_encrypt(key: bytes) -> _EncryptProtocol:
+    """Create single argument encryption function based on the given key."""
     return cast(_EncryptProtocol, functools.partial(_encrypt, key=key))
 
 
 def _decrypt(data: BytesLike | None, *, key: bytes) -> bytes | None:
+    """Wrapper for decrypting data with a symmetric key.
+
+    Reverse of `_encrypt`. Allows None to passthrough, but does not decode to string.
+    """
     if data is None:
         return None
     return cryptography.fernet.Fernet(key).decrypt(bytes(data))
 
 
 def _decrypt_decode(data: BytesLike | None, key: bytes) -> str | None:
+    """Wrapper for decrypting data to a string with a symmetric key.
+
+    Reverse of `_encrypt`. Allows None to passthrough and decodes result to string.
+    """
     ret = _decrypt(data=data, key=key)
     if ret is None:
         return None
@@ -61,6 +80,8 @@ def _decrypt_decode(data: BytesLike | None, key: bytes) -> str | None:
 
 
 class _DecryptProtocol(Protocol):
+    """Slightly beefed up annotation for type inferrence purposes."""
+
     @overload
     def __call__(self, data: None) -> None: ...
 
@@ -71,10 +92,13 @@ class _DecryptProtocol(Protocol):
 
 
 def get_decrypt(key: bytes) -> _DecryptProtocol:
+    """Create a single argument decryption function based on the given key."""
     return cast(_DecryptProtocol, functools.partial(_decrypt, key=key))
 
 
 class _DecryptDecodeProtocol(Protocol):
+    """Slightly beefed up annotation for type inferrence purposes."""
+
     @overload
     def __call__(self, data: None) -> None: ...
 
@@ -85,4 +109,9 @@ class _DecryptDecodeProtocol(Protocol):
 
 
 def get_decrypt_decode(key: bytes) -> _DecryptDecodeProtocol:
+    """
+    Create a single argument decryption function based on the given key.
+
+    Decodes the result to string.
+    """
     return cast(_DecryptDecodeProtocol, functools.partial(_decrypt_decode, key=key))
