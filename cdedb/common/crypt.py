df019db138728ecf98da6e00f6a602ffd1047f27
Tobias Udtke <tobias.udtke@gmail.com>
2025-11-22 09:38:07 +0100

    common:crypt: Expand docstrings regarding expected key format.

--- cdedb/common/crypt.py
+++ cdedb/common/crypt.py
@@ -24,7 +24,11 @@ def encrypt_password(password: str) -> str:
 
 
 def generate_encrytion_key() -> bytes:
-    """Generate an encryption key for symmetric encryption helpers below."""
+    """
+    Generate an encryption key for symmetric encryption helpers below.
+
+    The returned key is 32 bytes base64-encoded (urlsafe).
+    """
     return cryptography.fernet.Fernet.generate_key()
 
 
@@ -33,6 +37,8 @@ def _encrypt(data: str | bytes | None, *, key: bytes) -> bytes | None:
     Wrapper for encrypting data symmetrically.
 
     Allows None to passthrough and encodes strings.
+
+    The key is of the same format as returned by `generate_encryption_key()`.
     """
     if data is None:
         return None
@@ -54,14 +60,21 @@ class _EncryptProtocol(Protocol):
 
 
 def get_encrypt(key: bytes) -> _EncryptProtocol:
-    """Create single argument encryption function based on the given key."""
+    """
+    Create single argument encryption function based on the given key.
+
+    The key is of the same format as returned by `generate_encryption_key()`.
+    """
     return cast(_EncryptProtocol, functools.partial(_encrypt, key=key))
 
 
 def _decrypt(data: BytesLike | None, *, key: bytes) -> bytes | None:
-    """Wrapper for decrypting data with a symmetric key.
+    """
+    Wrapper for decrypting data with a symmetric key.
 
     Reverse of `_encrypt`. Allows None to passthrough, but does not decode to string.
+
+    The key is of the same format as returned by `generate_encryption_key()`.
     """
     if data is None:
         return None
@@ -69,9 +82,12 @@ def _decrypt(data: BytesLike | None, *, key: bytes) -> bytes | None:
 
 
 def _decrypt_decode(data: BytesLike | None, key: bytes) -> str | None:
-    """Wrapper for decrypting data to a string with a symmetric key.
+    """
+    Wrapper for decrypting data to a string with a symmetric key.
 
     Reverse of `_encrypt`. Allows None to passthrough and decodes result to string.
+
+    The key is of the same format as returned by `generate_encryption_key()`.
     """
     ret = _decrypt(data=data, key=key)
     if ret is None:
@@ -92,7 +108,11 @@ class _DecryptProtocol(Protocol):
 
 
 def get_decrypt(key: bytes) -> _DecryptProtocol:
-    """Create a single argument decryption function based on the given key."""
+    """
+    Create a single argument decryption function based on the given key.
+
+    The key is of the same format as returned by `generate_encryption_key()`.
+    """
     return cast(_DecryptProtocol, functools.partial(_decrypt, key=key))
 
 
@@ -113,5 +133,7 @@ def get_decrypt_decode(key: bytes) -> _DecryptDecodeProtocol:
     Create a single argument decryption function based on the given key.
 
     Decodes the result to string.
+
+    The key is of the same format as returned by `generate_encryption_key()`.
     """
     return cast(_DecryptDecodeProtocol, functools.partial(_decrypt_decode, key=key))
