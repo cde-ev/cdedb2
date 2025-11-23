65513859bc5eaeb190b299193826ec174a2235f8
TobiasUdtke <tobias.udtke@gmail.com>
2025-11-23 20:35:01 +0100

    Update cdedb/database/evolutions/2025-11-23_ballot_log.sql

--- cdedb/database/evolutions/2025-11-23_ballot_log.sql
+++ cdedb/database/evolutions/2025-11-23_ballot_log.sql
@@ -1,3 +1,3 @@
 BEGIN;
-    ALTER TABLE assembly.log ADD COLUMN ballot_id integer REFERENCES assembly.ballots(id) ON DELETE SET NULL,
+    ALTER TABLE assembly.log ADD COLUMN ballot_id integer REFERENCES assembly.ballots(id) ON DELETE SET NULL;
 COMMIT;
