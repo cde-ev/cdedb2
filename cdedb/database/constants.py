9c29dddfa75b8ad2d6676ebdcd2ad3d2b47ce7ab
Tobias Udtke <tobias.udtke@gmail.com>
2025-12-18 15:40:42 +0100

    past-event: add institution "other".

--- cdedb/database/constants.py
+++ cdedb/database/constants.py
@@ -559,6 +559,7 @@ class PastInstitutions(CdEIntEnum):
     basf = 80  #:
     van = 200  #:
     eisenberg = 400  #:
+    other = 1000  #:
 
     @classmethod
     def main_insitution(cls) -> "PastInstitutions":
@@ -575,8 +576,9 @@ class PastInstitutions(CdEIntEnum):
             self.basf: "BASF",
             self.van: "VAN",
             self.eisenberg: "FV Eisenberg",
+            self.other: "Sonst.",
         }
-        return shortnames[self]
+        return shortnames.get(self, str(self))
 
 
 @enum.unique
