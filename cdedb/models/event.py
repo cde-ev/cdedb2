6c39ebb3a458914a9fb9fec4f94790c151f542de
Tobias Udtke <tobias.udtke@gmail.com>
2026-01-27 00:11:55 +0100

    mypy: assume python3.13

--- cdedb/models/event.py
+++ cdedb/models/event.py
@@ -926,7 +926,7 @@ class TrackGroup(EventDataclass):
         return self.constraint_type, self.sortkey, self.title
 
 
-class SyncTrackGroup(TrackGroup, CourseChoiceObject):
+class SyncTrackGroup(TrackGroup, CourseChoiceObject):  # type: ignore[misc]
     constraint_type = const.CourseTrackGroupType.course_choice_sync
 
     def is_complex(self) -> bool:
