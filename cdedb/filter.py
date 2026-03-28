be3a19e2b1a40998d54261d5828c9eec8fa22904
Tobias Udtke <tobias.udtke@gmail.com>
2026-03-29 00:50:50 +0100

    filters: disable sorting of enum entries by value.

--- cdedb/filter.py
+++ cdedb/filter.py
@@ -725,16 +725,11 @@ def enum_entries_filter(
         pre = lambda x: x
     else:
         pre = lambda x: x.display_str() if hasattr(x, "display_str") else str(x)
-    if intval:
-        sortkey = lambda x: x
-    else:
-        sortkey = lambda e: e[0].value
-    to_sort = (
+    ret = (
         (int(entry) if intval else entry, prefix + processing(pre(entry)))
         for entry in enum
         if entry not in exempt
     )
-    ret = xsorted(to_sort, key=sortkey)
     grouped = collections.defaultdict(list)
     for value, label in ret:
         group_label = value.optgroup_label() if hasattr(value, "optgroup_label") else ""
