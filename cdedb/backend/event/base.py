fb5d555208877f3a92392b137b1e172fc51b9128
Tobias Udtke <tobias.udtke@gmail.com>
2026-05-19 15:56:22 +0200

    event: add stored queries to partial export.

--- cdedb/backend/event/base.py
+++ cdedb/backend/event/base.py
@@ -1734,6 +1734,7 @@ class EventBaseBackend(EventLowLevelBackend):
                 )
             )
             questionnaire = self.get_all_questionnaires(rs, event_id).as_dict(full=True)
+            stored_queries = self.get_event_queries(rs, event_id)  # type: ignore[attr-defined]
             persona_ids = tuple(reg['persona_id'] for reg in registrations.values())
             personas = {
                 p.id: p.as_dict()
@@ -1923,6 +1924,14 @@ class EventBaseBackend(EventLowLevelBackend):
             field["entries"] = normalize_field_entries(
                 field["entries"], field["kind"], coalesce=""
             )
+        ret["event"]["stored_queries"] = {
+            query.query_name: {
+                "scope": query.scope,
+                "query_group": query.query_group,
+                "serialized_query": query.serialized_query,
+            }
+            for query in stored_queries.values()
+        }
         # personas
         for reg_id, registration in ret['registrations'].items():
             persona = personas[registration['persona_id']]
