7358a91e5af769c7b4040ceecad53d0e78402f7e
Tobias Udtke <tobias.udtke@gmail.com>
2026-08-24 21:30:03 +0200

    core: add comments about dependency checks for privilege change. Slightly rewrite the calculation of actually changed roles.

--- cdedb/backend/core/base.py
+++ cdedb/backend/core/base.py
@@ -1318,7 +1318,12 @@ class CoreBaseBackend(AbstractBackend):
                 "User does not fit the requirements for this admin privilege."
             )
             for admin_role in Roles.all_admin_roles():
+                # Check if this role is currently being granted or
+                #  (is already in effect and is not currently being revoked).
                 if data.get(admin_role.marker, admin_role in persona_roles):
+                    # If so, check that requirements are (still) met.
+                    #  Again: Consider (roles that are currently being granted) and
+                    #  (roles that are already in effect and are not being revoked).
                     if any(
                         not data.get(required.marker, required in persona_roles)
                         for required in admin_role.required_roles
--- cdedb/frontend/core/base.py
+++ cdedb/frontend/core/base.py
@@ -2156,12 +2156,23 @@ class CoreBaseFrontend(AbstractFrontend):
         data = {
             "persona_id": persona_id,
             "notes": notes,
+            **{
+                admin_role.marker: admin_role in roles
+                for admin_role in Roles.all_admin_roles()
+                # Check if this admin roles has changed.
+                #  Left side of the comparison is the new state, right is the old state.
+                #  Collect only the actually changed admin bits in 'data'.
+                if (admin_role in roles) != (admin_role in persona_roles)
+            },
         }
-        for admin_role in Roles.all_admin_roles():
-            if (admin_role in roles) != (admin_role in persona_roles):
-                data[admin_role.marker] = admin_role in roles
 
+        for admin_role in Roles.all_admin_roles():
+            # Check if this role is currently being granted or
+            #  (is already in effect and is not currently being revoked).
             if data.get(admin_role.marker, admin_role in persona_roles):
+                # If so, check that requirements are (still) met.
+                #  Again: Consider (roles that are currently being granted) and
+                #  (roles that are already in effect and are not being revoked).
                 if any(
                     not data.get(required.marker, required in persona_roles)
                     for required in admin_role.required_roles
